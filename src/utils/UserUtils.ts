f05732210a76b9d38d9c87658c3a882d78eab3f8
Aleksey Budkin <anibaka9@gmail.com>
2018-10-03 19:57:21 +0300

    removed unused utils

--- src/utils/UserUtils.ts
+++
@@ -1,5 +0,0 @@
-export function handleSelectChangePrepareForFormik (name: string, optionSelected: { label: string; value: string }) {
-  const { setFieldValue } = this.props
-  const value = optionSelected ? optionSelected.value : ''
-  setFieldValue(name, value);
-}
--- src/utils/dateUtils.ts
+++
@@ -1,9 +0,0 @@
-export const daysFromNow = (days: number) => {
-  const formatTimestamp = (timestamp: number) => Math.round(timestamp / 1000)
-
-  const date = new Date()
-  date.setDate(date.getDate() + days)
-  date.setHours(0, 0, 0, 0)
-
-  return formatTimestamp(date.getTime())
-}
