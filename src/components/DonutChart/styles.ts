0a50000729156398942709c379ebc6d393882884
Aleksey Budkin <anibaka9@gmail.com>
2018-10-16 12:46:30 +0300

    moved fixes from web dev

--- src/components/DonutChart/styles.ts
+++ src/components/DonutChart/styles.ts
@@ -1,23 +1,33 @@
 import styled from 'styled-components'
 
 export const ChartContainer = styled.div`
-  width: ${(props: {width: number}) =>
-      props.width + 'px'
-    };
+  min-height: 25rem;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+`
+
+export const ChartWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 90px);
 `
 
 export const ValueContainer = styled.div`
   margin: 0px;
   position: relative;
   top: -50%;
-  transform: translate(0,-50%);
+  transform: translate(0, -50%);
   text-align: center;
-  z-index: -1;
+  z-index: 1;
+  opacity: ${(props: { value: string }) => (props.value ? 1 : 0)};
+  transition: opacity 0.25s ease-in-out;
 `
 
 export const LabelContainer = styled.div`
   margin: 0px;
   position: relative;
-  text-align: center;
+  display: flex;
+  place-content: center;
+  place-items: center;
   height: 90px;
 `
--- src/components/DonutChart/types.ts
+++ src/components/DonutChart/types.ts
@@ -19,6 +19,7 @@ export interface Props {
   data: InputRecord[]
   width?: number
   height?: number
+  labelPlaceholder?: string
   radius?: number
   thickness?: number
   theme: Theme
