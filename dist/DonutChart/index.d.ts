026e952f0e9699b2776b92b101014477fa74e46e
Aleksey Budkin <anibaka9@gmail.com>
2018-10-19 22:42:45 +0300

    delited dist

--- dist/DonutChart/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Props } from './types';
-export declare const DonutChart: React.ComponentType<Pick<Props, "data" | "width" | "height" | "radius" | "thickness"> & Partial<import("@material-ui/core/styles/withTheme").WithTheme>>;
-export default DonutChart;
--- dist/DonutChart/styles.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-export declare const ChartContainer: import("styled-components").StyledComponentClass<import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & {
-    width: number;
-}, any, import("react").ClassAttributes<HTMLDivElement> & import("react").HTMLAttributes<HTMLDivElement> & {
-    width: number;
-}>;
-export declare const ValueContainer: import("styled-components").StyledComponentClass<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, any, import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>>;
-export declare const LabelContainer: import("styled-components").StyledComponentClass<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, any, import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>>;
