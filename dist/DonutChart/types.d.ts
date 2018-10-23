999b28b9d9c56726c0fd4c13247524976a9c9f31
anibaka <anibaka9@gmail.com>
2018-10-23 13:16:25 +0000

    release v0.0.4-beta-test-connect-to-web (autogenerated)

---
+++ dist/DonutChart/types.d.ts
@@ -0,0 +1,34 @@
+import { Theme } from '@material-ui/core';
+export interface DonutPiece {
+    angle: number;
+    label: string;
+    realValue: number;
+    gradientIndex: number;
+    opacity?: number;
+    color?: string | number;
+    style?: object;
+}
+export interface InputRecord {
+    label: string;
+    realValue: number;
+}
+export interface gradient {
+    [index: number]: string;
+}
+export interface Props {
+    data: InputRecord[];
+    labelPlaceholder: string;
+    radius: number;
+    thickness?: number;
+    gradients: gradient[];
+    theme: Theme;
+    colorLegend: boolean;
+    isSizeFlexible: boolean;
+    hightCoefficient: number;
+    widthCoefficient: number;
+    thicknessCoefficient: number;
+}
+export interface State {
+    data: DonutPiece[];
+    value: DonutPiece | null;
+}
