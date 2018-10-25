8dd1f93541cbadcee1a03c442d10fe3856655612
Aleksey Budkin <anibaka9@gmail.com>
2018-10-25 14:54:37 +0300

    made dark theme default

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,9 +7,9 @@ import { withInfo } from '@storybook/addon-info'
 import { muiTheme } from 'storybook-addon-material-ui'
 
 import { customThemes } from './customTheme'
-console.log(customThemes)
+
 addDecorator(
-  muiTheme([customThemes.light, customThemes.dark])
+  muiTheme([customThemes.dark, customThemes.light])
 )
 addDecorator((story) => (
   <div style={{ margin: 20 }}>
