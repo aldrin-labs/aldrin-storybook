38f0251a9bf0b15f7157fd7d1da6466ad2fabda8
addamove <empire.empo@gmail.com>
2018-11-09 11:11:02 +0300

    fixed config

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,27 +4,20 @@ import { configure, addDecorator } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
 import { withKnobs } from '@storybook/addon-knobs'
 import { withInfo } from '@storybook/addon-info'
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles'
 
 import { customThemes } from './customTheme'
-console.log(customThemes)
+
 //You need to change customThemes.dark to customThemes.light to change theme in storybook
 addDecorator((story) => (
-  <MuiThemeProvider theme={customThemes.dark}>
-    {story()}
-  </MuiThemeProvider>
+  <MuiThemeProvider theme={customThemes.dark}>{story()}</MuiThemeProvider>
 ))
-addDecorator((story) => (
-  <div style={{ margin: 20 }}>
-    {story()}
-  </div>
-))	
+addDecorator((story) => <div style={{ margin: 20 }}>{story()}</div>)
 addDecorator(withKnobs)
-
 
 setOptions({
   name: 'Storybook',
-  url: '#'
+  url: '#',
 })
 
 configure(() => require('../src/stories/index'), module)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,7 @@
 const webpackMerge = require('webpack-merge')
 const path = require('path')
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = (baseConfig, env) => {
   const config = {
@@ -43,6 +45,26 @@ module.exports = (baseConfig, env) => {
           ],
         },
       ],
+    },
+    plugins: [
+      new HardSourceWebpackPlugin(),
+      // Add module names to factory functions so they appear in browser profiler.
+      new webpack.NamedModulesPlugin(),
+    ],
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+      dgram: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      child_process: 'empty',
+    },
+    // Turn off performance hints during development because we don't do any
+    // splitting or minification in interest of speed. These warnings become
+    // cumbersome.
+    performance: {
+      hints: false,
     },
   }
 
