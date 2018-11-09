923718e811d09ab48b85b7181f961ba213b50d40
addamove <empire.empo@gmail.com>
2018-11-09 16:27:54 +0300

    qff

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -46,11 +46,11 @@ module.exports = (baseConfig, env) => {
         },
       ],
     },
-    plugins: [
-      new HardSourceWebpackPlugin(),
-      // Add module names to factory functions so they appear in browser profiler.
-      new webpack.NamedModulesPlugin(),
-    ],
+    // plugins: [
+    // new HardSourceWebpackPlugin(),
+    // Add module names to factory functions so they appear in browser profiler.
+    // new webpack.NamedModulesPlugin(),
+    // ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
     node: {
