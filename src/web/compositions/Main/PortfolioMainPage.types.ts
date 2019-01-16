3a9eb6d0be460691de7c630df8229e8ce785633c
avernikoz <avernikoz@gmail.com>
2019-01-17 01:13:41 +0300

    add PortfolioMainPage composition

---
+++ src/web/compositions/Main/PortfolioMainPage.types.ts
@@ -0,0 +1,19 @@
+import { Theme } from '@material-ui/core'
+
+export interface IState {
+  key: number
+}
+
+export interface IProps {
+  tab: string
+  theme: Theme
+  isShownMocks: boolean
+  toolTip: {
+    portfolioMain: boolean
+    portfolioIndustry: boolean
+    portfolioRebalance: boolean
+    portfolioCorrelation: boolean
+    portfolioOptimization: boolean
+  }
+  hideToolTip: (tab: string) => void
+}
