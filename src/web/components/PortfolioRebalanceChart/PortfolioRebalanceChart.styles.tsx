9624cb55fda2da364f206c91786876922a6b1d62
avernikoz <avernikoz@gmail.com>
2019-01-18 17:29:32 +0300

    added PortfolioRebalanaceChart to storybook

---
+++ src/web/components/PortfolioRebalanceChart/PortfolioRebalanceChart.styles.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card, CardContent } from '@material-ui/core'
+
+export const ChartContainer = styled(Card)`
+  && {
+    height: 100%;
+    width: 100%;
+  }
+`
+
+//  minus card header height
+export const Chart = styled(({background, ...rest}) => (
+  <CardContent {...rest} />
+))`
+  background: ${(props: { background: string }) => props.background};
+  width: 100%;
+  height: calc(100% - 68px);
+  && {
+    padding: 0.5rem 0.5rem 0 0.5rem;
+  }
+`
