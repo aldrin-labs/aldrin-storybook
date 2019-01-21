034cd3c0cae08e6495be1aa8842546dc399d5001
avernikoz <avernikoz@gmail.com>
2019-01-21 05:02:08 +0300

    updated RebalanceComposition in storybook

---
+++ src/web/compositions/Rebalance/PortfolioRebalancePage.styles.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Card, Grid, CardContent } from '@material-ui/core'
+
+
+export const ChartWrapper = styled(({ isEditModeEnabled, ...rest }) => (
+  <Grid {...rest} />
+))`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+
+  height: 45%;
+  && {
+    padding-bottom: 0;
+  }
+`
+
+export const ChartContainer = styled(Card)`
+  && {
+    height: 100%;
+    width: 100%;
+  }
+`
+
+//  minus card header height
+export const Chart = styled(({background, ...rest}) => (
+  <CardContent {...rest} />
+))`
+  background: ${(props: { background: string }) => props.background};
+  width: 100%;
+  height: calc(100% - 68px);
+  && {
+    padding: 0.5rem 0.5rem 0 0.5rem;
+  }
+`
+
+export const Container = styled(({ isEditModeEnabled, ...rest }) => (
+  <Grid {...rest} />
+))`
+  justify-content: ${(props: { isEditModeEnabled: boolean }) =>
+  props.isEditModeEnabled ? 'space-between' : 'left'};
+
+  max-height: 55%;
+`
--- src/web/compositions/Rebalance/PortfolioRebalancePage.tsx
+++ src/web/compositions/Rebalance/PortfolioRebalancePage.tsx
@@ -1,2 +1,210 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { compose } from 'recompose'
+import { connect } from 'react-redux'
 
+import Joyride from 'react-joyride'
+import * as actions from '@containers/User/actions'
+
+import { Container as Content } from '@storybook/styles/cssUtils'
+import { portfolioRebalanceSteps } from '@storybook/config/joyrideSteps'
+import DialogComponent from '@storybook/components/RebalanceDialog/RebalanceDialog'
+import EmptyTablePlaceholder from '@storybook/components/EmptyTablePlaceholder'
+
+import PortfolioRebalanceTableContainer from '@core/containers/PortfolioRebalanceTableContainer/PortfolioRebalanceTableContainer'
+import PortfolioRebalanceChart from '@core/containers/PortfolioRebalanceChart/PortfolioRebalanceChart'
+import { Container, ChartWrapper } from './PortfolioRebalancePage.styles'
+import { withTheme } from "@material-ui/styles"
+
+class PortfolioRebalancePage extends Component {
+  state = {
+    key: 0,
+  }
+
+  handleJoyrideCallback = (data: any) => {
+    if (
+      data.action === 'close' ||
+      data.action === 'skip' ||
+      data.status === 'finished'
+    ) {
+      this.props.hideToolTip('Rebalance')
+    }
+    if (data.status === 'finished') {
+      const oldKey = this.state.key
+      this.setState({ key: oldKey + 1 })
+    }
+  }
+
+  render() {
+    const {
+      selectedActive,
+      areAllActiveChecked,
+      totalStaticRows,
+      totalRows,
+      isEditModeEnabled,
+      undistributedMoney,
+      isPercentSumGood,
+      totalPercents,
+      totalTableRows,
+      totalSnapshotRows,
+      rows,
+      staticRows,
+      addMoneyInputValue,
+      leftBar,
+      rightBar,
+      loading,
+      staticRowsMap,
+      openWarning,
+      isSystemError,
+      warningMessage,
+      isSaveError,
+      isCurrentAssetsChangedError,
+      children,
+      isUSDCurrently,
+      theme,
+      theme: { palette, customPalette },
+      tab,
+      timestampSnapshot,
+      onDiscardChanges,
+      onSaveClick,
+      onReset,
+      onEditModeEnable,
+      updateState,
+      onNewSnapshot,
+      hideWarning,
+      createNewSnapshot,
+    } = this.props
+
+    const secondary = palette.secondary.main
+    const red = customPalette.red.main
+    const green = customPalette.green.main
+    const fontFamily = theme.typography.fontFamily
+    const saveButtonColor = isPercentSumGood ? green : red
+
+    const tableDataHasData = !staticRows.length || !rows.length
+
+    return (
+      <>
+        <Joyride
+          continuous={true}
+          showProgress={true}
+          showSkipButton={true}
+          steps={portfolioRebalanceSteps}
+          run={this.props.toolTip.portfolioRebalance && tab === 'rebalance'}
+          callback={this.handleJoyrideCallback}
+          key={this.state.key}
+          styles={{
+            options: {
+              backgroundColor: theme.palette.getContrastText(
+                theme.palette.primary.main
+              ),
+              primaryColor: theme.palette.secondary.main,
+              textColor: theme.palette.primary.main,
+            },
+            tooltip: {
+              fontFamily: theme.typography.fontFamily,
+              fontSize: theme.typography.fontSize,
+            },
+          }}
+        />
+
+        <EmptyTablePlaceholder isEmpty={tableDataHasData}>
+          {children}
+          <Content key={`content`} container spacing={16}>
+            <Container
+              key={`table-container`}
+              item
+              md={12}
+              isEditModeEnabled={isEditModeEnabled}
+            >
+              <PortfolioRebalanceTableContainer
+                {...{
+                  isEditModeEnabled,
+                  staticRows,
+                  staticRowsMap,
+                  totalStaticRows,
+                  rows,
+                  selectedActive,
+                  areAllActiveChecked,
+                  totalRows,
+                  totalPercents,
+                  totalTableRows,
+                  isPercentSumGood,
+                  undistributedMoney,
+                  isUSDCurrently,
+                  addMoneyInputValue,
+                  theme,
+                  loading,
+                  red,
+                  saveButtonColor,
+                  secondary,
+                  fontFamily,
+                  totalSnapshotRows,
+                  timestampSnapshot,
+                  onDiscardChanges,
+                  onSaveClick,
+                  onReset,
+                  onEditModeEnable,
+                  updateState,
+                  onNewSnapshot,
+                }}
+              />
+            </Container>
+
+            <ChartWrapper
+              key={`chart-container`}
+              item
+              md={12}
+              className="PortfolioDistributionChart"
+            >
+              <PortfolioRebalanceChart
+                title={`Portfolio Distribution`}
+                background={theme.palette.background.default}
+                staticRows={staticRows}
+                rows={rows}
+                bottomMargin={75}
+                theme={theme}
+                hideDashForToolTip={true}
+                xAxisVertical={true}
+                alwaysShowLegend={true}
+                leftBar={leftBar}
+                rightBar={rightBar}
+              />
+            </ChartWrapper>
+
+            {/* end of a grid */}
+
+            <DialogComponent
+              {...{
+                openWarning,
+                warningMessage,
+                isSaveError,
+                isSystemError,
+                isCurrentAssetsChangedError,
+                hideWarning,
+                onSaveClick,
+                onReset,
+                createNewSnapshot,
+              }}
+            />
+          </Content>
+        </EmptyTablePlaceholder>
+      </>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch: any) => ({
+  hideToolTip: (tab: string) => dispatch(actions.hideToolTip(tab)),
+})
+
+const mapStateToProps = (store: any) => ({
+  toolTip: store.user.toolTip,
+})
+
+export default compose(
+  withTheme(),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(PortfolioRebalancePage)
