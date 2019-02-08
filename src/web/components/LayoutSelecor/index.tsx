023b7660d049764e08e4be94304cfeb56294a140
anibaka <anibaka9@gmail.com>
2019-02-08 12:36:30 +0300

    change back to functional component

--- src/web/components/LayoutSelecor/index.tsx
+++ src/web/components/LayoutSelecor/index.tsx
@@ -5,12 +5,8 @@ import { withTheme } from '@material-ui/styles'
 import { SelectR, SelectContainer } from './styles'
 import { AddLayoutDialog } from '@sb/components/AddLayoutDialog'
 
-class LayoutSelecorComponent extends React.Component {
-  state = {
-    open: false,
-  }
-
-  handleChange = ({ value }) => {
+const LayoutSelecorComponent = ({...props}) => {
+  const handleChange = ({ value }) => {
     const {
       loadLayout
     } = this.props
@@ -21,47 +17,34 @@ class LayoutSelecorComponent extends React.Component {
     loadLayout(value)
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true })
-  }
+  const {
+    layouts,
+    saveLayout,
+    charts,
+    theme: {
+      palette: { divider },
+    },
+  } = props
 
-  handleClose = () => {
-    console.log('handleClose')
-    this.setState({ open: false })
-  }
-
-  render() {
-    const {
-      layouts,
-      saveLayout,
-      handleClose,
-      handleSubmit,
-      charts,
-      theme: {
-        palette: { divider },
-      },
-    } = this.props
-
-    return (
-      <>
-        <SelectContainer
-          border={divider}
-        >
-          <SelectR
-            value=""
-            placeholder="Select layout"
-            fullWidth={true}
-            options={layouts}
-            onChange={this.handleChange}
-          />
-        </SelectContainer>
-        <AddLayoutDialog
-          charts={charts}
-          saveLayout={saveLayout}
+  return (
+    <>
+      <SelectContainer
+        border={divider}
+      >
+        <SelectR
+          value=""
+          placeholder="Select layout"
+          fullWidth={true}
+          options={layouts}
+          onChange={handleChange}
         />
-      </>
-    )
-  }
+      </SelectContainer>
+      <AddLayoutDialog
+        charts={charts}
+        saveLayout={saveLayout}
+      />
+    </>
+  )
 }
 
 export default withTheme()(LayoutSelecorComponent)
