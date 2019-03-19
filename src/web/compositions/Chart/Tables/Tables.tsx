3db2988d0100d1094417b595ef5094eed0ab6ee0
avernikoz <avernikoz@gmail.com>
2019-03-19 13:44:54 +0300

    delete ExchangesTable from Tables/Tables.tsx file

--- src/web/compositions/Chart/Tables/Tables.tsx
+++ src/web/compositions/Chart/Tables/Tables.tsx
@@ -1,6 +1,5 @@
 import OrderBookTable from './OrderBookTable/OrderBookTableContainer'
 import Aggregation from './Aggregation/Aggregation'
 import TradeHistoryTable from './TradeHistoryTable/TableContainer'
-import ExchangesTable from './ExchangesTable/TableContainer'
 
-export { Aggregation, OrderBookTable, ExchangesTable, TradeHistoryTable }
+export { Aggregation, OrderBookTable, TradeHistoryTable }
