3f54211edc157a6da0bf81eb28a45acd2c63fd48
avernikoz <avernikoz@gmail.com>
2019-03-19 13:40:09 +0300

    delete ExchangesTable in Chart/Tables/ExchangesTable/

--- src/web/compositions/Chart/Tables/ExchangesTable/Table/ExchangesTable.types.ts
+++
@@ -1,9 +0,0 @@
-import { IActiveExchange, IExchange } from '@core/types/ChartTypes'
-
-export interface IProps {
-  onButtonClick: Function
-  changeExchange: Function
-  activeExchange: IActiveExchange
-  exchanges: IExchange[]
-  theme: any
-}
--- src/web/compositions/Chart/Tables/ExchangesTable/TableContainer.types.ts
+++
@@ -1,6 +0,0 @@
-import { IExchange } from '@core/types/ChartTypes'
-
-export interface IExchangesTable {
-  data: {marketByName: [{exchanges: IExchange[]}]}
-  isShownMocks: boolean
-}
