ee8966d3f9ddaa2ccbfb8aea6c886e2ec1a271b2
anibaka <anibaka9@gmail.com>
2019-03-20 18:22:31 +0300

    changed stiles

--- src/web/components/TraidingTerminal/index.tsx
+++ src/web/components/TraidingTerminal/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Grid, TextField } from '@material-ui/core'
+import { Grid, TextField, InputAdornment } from '@material-ui/core'
 
 import { TypographyWithCustomColor } from '@sb/styles/StyledComponents/TypographyWithCustomColor'
 
 import {
-  ChartContainer,
+  Container,
   NameHeader,
   InputContainer,
   TitleContainer,
+  PriceButton,
+  GridContainer,
 } from './styles'
 
 export const TraidingTerminal = (props) => {
   return (
-    <ChartContainer>
+    <Container>
       <div>
         <NameHeader>
         <TypographyWithCustomColor
@@ -29,46 +31,78 @@ export const TraidingTerminal = (props) => {
           0.00000000 USDT
         </TypographyWithCustomColor>
         </NameHeader>
+      <GridContainer>
       <Grid container spacing={0}>
-        <Grid item xs={3}>
+        <Grid item xs={4}>
           <TitleContainer>
           <TypographyWithCustomColor
             textColor
-            variant="subtitle1"
+            variant="subtitle2"
           >
             Price:
           </TypographyWithCustomColor>
           </TitleContainer>
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={8}>
         <InputContainer>
         <TextField
           type="number"
           defaultValue="12324"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">USDT</InputAdornment>,
+          }}
         />
         </InputContainer>
         </Grid>
-        <Grid item xs={3}>
+        <Grid item xs={4}>
           <TitleContainer>
           <TypographyWithCustomColor
             textColor
-            variant="subtitle1"
+            variant="subtitle2"
           >
             Price:
           </TypographyWithCustomColor>
           </TitleContainer>
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={8}>
         <InputContainer>
         <TextField
           type="number"
           defaultValue="12324"
+          style={{ fontSize: '63px' }}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">BTC</InputAdornment>,
+          }}
         />
         </InputContainer>
+        </Grid>
+        <Grid item xs={4}>
+          {''}
+        </Grid>
+        <Grid item xs={2}>
+        <PriceButton>
+          25%
+        </PriceButton>
+        </Grid>
+        <Grid item xs={2}>
+        <PriceButton >
+          50%
+        </PriceButton>
+        </Grid>
+        <Grid item xs={2}>
+        <PriceButton>
+          75%
+        </PriceButton>
+        </Grid>
+        <Grid item xs={2}>
+        <PriceButton>
+          100%
+        </PriceButton>
         </Grid>
       </Grid>
+      </GridContainer>
       </div>
-    </ChartContainer>
+    </Container>
   )
 }
 
--- src/web/components/TraidingTerminal/styles.tsx
+++ src/web/components/TraidingTerminal/styles.tsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Card, withStyles } from '@material-ui/core'
-import InputLabel from '@material-ui/core/InputLabel'
-import TextField from '@material-ui/core/TextField'
-import Switch from '@material-ui/core/Switch'
+import { Card, Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles';
+
 
 import { CardHeader } from '@sb/components/index'
 
+const styles = {
+  root: {
+    borderRadius: 3,
+    color: 'white',
+    minWidth: 45,
+    height: 10,
+    padding: '0px',
+  },
+};
 
+export const PriceInput = styled(withStyles(styles)((props) =>
+<TextField
+type="number"
+defaultValue="12324"
+style={{ fontSize: '63px' }}
+InputProps={{
+  endAdornment: <InputAdornment position="end">BTC</InputAdornment>,
+}}
+/>))`
+margin: 5px;
+padding: 2px;
+`
+
+export const PriceButton = styled(withStyles(styles)((props) =>
+  <div>
+    <Button
+      className={props.classes.root}
+      variant="outlined"
+      size="small"
+      >
+        {props.children}
+    </Button>
+  </div>))`
+`
 
 export const StyledCardHeader = styled(CardHeader)`
   margin-bottom: 15px;
@@ -29,15 +61,13 @@ export const HighlightStyles = `
   box-shadow: 1px 3px 9px 0px rgb(255, 129, 0), 0px 2px 3px 0px rgb(195, 171, 4), 2px 2px 6px 1px rgb(212, 107, 17);
 `
 
-export const ChartContainer = styled(({ minHeight, margin, hide, ...other }) => (
+export const Container = styled(({ minHeight, margin, hide, ...other }) => (
   <Card {...other} />
 ))`
   max-width: 100%;
-  min-height: ${(props: { minHeight: string }) =>
-    props.minHeight ? props.minHeight : ''};
-  margin: ${(props: { margin: string }) => (props.margin ? props.margin : '')};
-
-  ${(props: { hide: boolean }) => (props.hide ? HidingStyles : '')};
+`
+export const GridContainer = styled.div`
+  padding-right: 5px;
 `
 
 export const NameHeader = styled.div`
@@ -50,10 +80,10 @@ padding: 16px;
 `
 
 export const TitleContainer = styled.div`
-padding: 7px;
+padding: 8px;
 `
 
 export const InputContainer = styled.div`
-padding: 5px;
+padding: 2px;
 `
 
