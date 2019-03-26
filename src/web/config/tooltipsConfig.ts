c6e6e9f68a39a57f761d81acd790b922a2525d35
avernikoz <avernikoz@gmail.com>
2019-03-26 12:15:20 +0300

    add tooltipsConfig.ts

---
+++ src/web/config/tooltipsConfig.ts
@@ -0,0 +1,21 @@
+
+export const tooltipsConfig = {
+  enabled: {
+    portfolioMain: true,
+    portfolioIndustry: true,
+    portfolioRebalance: true,
+    portfolioCorrelation: true,
+    portfolioOptimization: true,
+    chartPage: true,
+    multiChartPage: true,
+  },
+  disabled: {
+    portfolioMain: false,
+    portfolioIndustry: false,
+    portfolioRebalance: false,
+    portfolioCorrelation: false,
+    portfolioOptimization: false,
+    chartPage: false,
+    multiChartPage: false,
+  },
+}
