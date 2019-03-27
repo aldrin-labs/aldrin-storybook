b6356f5f0b96d5be1fdebf39ba029f5b98fd5dc5
avernikoz <avernikoz@gmail.com>
2019-03-27 10:45:31 +0300

    add Tabs

--- src/web/components/TradingTable/TradingTable.tsx
+++ src/web/components/TradingTable/TradingTable.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import MoreVertIcon from '@material-ui/icons/NetworkCellSharp'
+import { Button, Tabs, Tab } from '@material-ui/core'
 
 import { Table } from '@sb/components'
+import { StyledWrapperForDateRangePicker } from '@sb/styles/cssUtils'
 import { IProps, IState } from './TradingTable.types'
 import {
   tradingTableTabConfig,
@@ -39,24 +41,48 @@ const getTableHead = (tab: string) =>
 
 export default class TradingTable extends React.PureComponent<IProps, IState> {
   state: IState = {
+    tabValue: 0,
     tab: 'openOrders',
   }
 
   onTradingTableTabChange = (i: number) => {
     this.setState({ tab: tradingTableTabConfig[i] })
   }
+  handleChange = (e, tabValue) => {
+    this.setState({
+      tabValue,
+      tab: tradingTableTabConfig[tabValue],
+    })
+  }
 
   render() {
     return (
       <Table
         withCheckboxes={false}
-        title={new Array(4).fill(undefined).map((el, i) => (
-          <button onClick={() => this.onTradingTableTabChange(i)}>{`${
-            tradingTableTabConfig[i]
-          }`}</button>
-        ))}
+        title={(
+          <Tabs
+            value={this.state.tabValue}
+            onChange={this.handleChange}
+            indicatorColor="secondary"
+            textColor="primary"
+          >
+            <Tab label="Open orders" />
+            <Tab label="Order history" />
+            <Tab label="Trade history" />
+            <Tab label="Funds" />
+          </Tabs>
+        )}
         data={{ body: getTableBody(this.state.tab) }}
         columnNames={getTableHead(this.state.tab)}
+
+        // actions={[
+        //     {
+        //       id: '1',
+        //       icon: <MoreVertIcon />,
+        //       onClick: action('1'),
+        //       color: 'primary',
+        //     },
+        // ]}
         // actions={[
         //   {
         //     id: '1',
