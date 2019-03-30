946a77d8333e49f3e052a51ec6015cce7fc3e419
avernikoz <avernikoz@gmail.com>
2019-03-30 15:09:10 +0300

    update TradingTableUtils

--- src/web/components/TradingTable/TradingTable.utils.ts
+++ src/web/components/TradingTable/TradingTable.utils.ts
@@ -8,3 +8,14 @@ export const getEndDate = (stringDate: string) =>
     : stringDate === '1Month'
     ? moment().subtract(1, 'months')
     : moment().subtract(3, 'months')
+
+export const getEmptyTextPlaceholder = (tab: string) =>
+  tab === 'openOrders'
+    ? 'You have no open orders.'
+    : tab === 'orderHistory'
+    ? 'You have no order history'
+    : tab === 'tradeHistory'
+      ? 'You have no trades.'
+      : tab === 'funds'
+        ? 'You have no Funds'
+        : []
