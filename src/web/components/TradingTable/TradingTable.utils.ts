3551463fe68db1e117c09611c342ef2138dc4870
avernikoz <avernikoz@gmail.com>
2019-04-01 19:51:08 +0300

    processedOrderHistoryData & combineTradeHistoryTable functions added

--- src/web/components/TradingTable/TradingTable.utils.ts
+++ src/web/components/TradingTable/TradingTable.utils.ts
@@ -1,5 +1,5 @@
 import moment from 'moment'
-
+import { OrderType, TradeType } from '@core/types/ChartTypes'
 export const getEndDate = (stringDate: string) =>
   stringDate === '1Day'
     ? moment().subtract(1, 'days')
@@ -15,7 +15,57 @@ export const getEmptyTextPlaceholder = (tab: string) =>
     : tab === 'orderHistory'
     ? 'You have no order history'
     : tab === 'tradeHistory'
-      ? 'You have no trades.'
-      : tab === 'funds'
-        ? 'You have no Funds'
-        : []
+    ? 'You have no trades.'
+    : tab === 'funds'
+    ? 'You have no Funds'
+    : []
+
+export const combineOrderHistoryTable = (orderData: OrderType[]) => {
+
+  const processedOrderHistoryData = orderData.map((el: OrderType) => {
+    const { symbol, orderId, timestamp, type, side, price, stopPrice, status, origQty, executedQty } = el
+
+    return {
+      id: orderId,
+      date: moment(timestamp).format('MM-DD-YYYY h:mm:ss A'),
+      pair: symbol,
+      type: type,
+      side: side,
+      // TODO: We should change average "price" to average param from backend when it will be ready
+      average: price,
+      price: price,
+      filled: { render: executedQty, isNumber: true },
+      amount: origQty,
+      // TODO: We should change "total" to total param from backend when it will be ready
+      total: executedQty * price,
+      triggerConditions: stopPrice || '-',
+      status: status,
+    }
+  })
+
+  return processedOrderHistoryData
+}
+
+export const combineTradeHistoryTable = (tradeData: TradeType[]) => {
+
+  const processedTradeHistoryData = tradeData.map((el: TradeType) => {
+
+    const { id, timestamp, symbol, side, price, amount, fee: { cost, currency } } = el
+
+    return {
+      id: id,
+      time: moment(timestamp).format('MM-DD-YYYY h:mm:ss A'),
+      pair: symbol,
+      type: side,
+      price: price,
+      filled: amount,
+      fee: { render: `${cost} ${currency}`, isNumber: true },
+      // TODO: We should change "total" to total param from backend when it will be ready
+      total: 0,
+    }
+
+  })
+
+  return processedTradeHistoryData
+
+}
