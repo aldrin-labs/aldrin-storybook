556bfebf0abd3d8a8ff3e2623f0c1596085eea6c
avernikoz <avernikoz@gmail.com>
2019-04-08 15:29:01 +0300

    update headings & utils for Trading table

--- src/web/components/TradingTable/TradingTable.mocks.tsx
+++ src/web/components/TradingTable/TradingTable.mocks.tsx
@@ -19,15 +19,24 @@ export const openOrdersColumnNames = [
   { label: 'Pair', id: 'pair' },
   { label: 'Type', id: 'type' },
   { label: 'Side', id: 'side' },
-  { label: 'Price', id: 'price', isNumber: true },
-  { label: 'Amount', id: 'amount', isNumber: true },
-  { label: 'Filled %', id: 'filled', isNumber: true },
-  { label: 'Total', id: 'total', isNumber: true },
+  { label: 'Price', id: 'price' },
+  { label: 'Amount', id: 'amount' },
+  { label: 'Filled %', id: 'filled' },
+  { label: 'Total', id: 'total' },
   { label: 'Trigger', id: 'triggerConditions' },
-  { label: <TableButton size="small" variant="outlined">Cancel all</TableButton>, id: 'cancel', isSortable: false },
+  {
+    label: (
+      <TableButton size="small" variant="outlined">
+        Cancel all
+      </TableButton>
+    ),
+    id: 'cancel',
+    isSortable: false,
+  },
 ]
 
 export const openOrdersBody = new Array(13).fill(undefined).map((el, i) => ({
+  id: i,
   date: new Date().toDateString(),
   pair: `BTC_${String.fromCharCode(getRandomInt(65, 80)) +
     String.fromCharCode(getRandomInt(65, 80)) +
@@ -39,7 +48,13 @@ export const openOrdersBody = new Array(13).fill(undefined).map((el, i) => ({
   filled: { render: '100%', isNumber: true },
   total: 100,
   triggerConditions: '-',
-  cancel: {render: <TableButton key={i} variant="outlined" size={`small`}>Cancel</TableButton>},
+  cancel: {
+    render: (
+      <TableButton key={i} variant="outlined" size={`small`}>
+        Cancel
+      </TableButton>
+    ),
+  },
 }))
 
 export const orderHistoryColumnNames = [
@@ -47,11 +62,11 @@ export const orderHistoryColumnNames = [
   { label: 'Pair', id: 'pair' },
   { label: 'Type', id: 'type' },
   { label: 'Side', id: 'side' },
-  { label: 'Average', id: 'average', isNumber: true },
-  { label: 'Price', id: 'price', isNumber: true },
-  { label: 'Filled %', id: 'filled', isNumber: true },
-  { label: 'Amount', id: 'amount' , isNumber: true},
-  { label: 'Total', id: 'total', isNumber: true },
+  { label: 'Average', id: 'average' },
+  { label: 'Price', id: 'price' },
+  { label: 'Filled %', id: 'filled' },
+  { label: 'Amount', id: 'amount' },
+  { label: 'Total', id: 'total' },
   { label: 'Trigger', id: 'triggerConditions' },
   { label: 'Status', id: 'status' },
 ]
@@ -59,8 +74,8 @@ export const orderHistoryColumnNames = [
 export const orderHistoryBody = new Array(13).fill(undefined).map((el, i) => ({
   date: new Date().toDateString(),
   pair: `BTC_${String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80))}`,
+    String.fromCharCode(getRandomInt(65, 80)) +
+    String.fromCharCode(getRandomInt(65, 80))}`,
   type: arrayOfOrdersType[getRandomInt(0, 3)],
   side: arrayOfSides[getRandomInt(0, 2)],
   average: getRandomInt(100, 50000),
@@ -69,24 +84,24 @@ export const orderHistoryBody = new Array(13).fill(undefined).map((el, i) => ({
   amount: getRandomInt(100, 8000),
   total: 100,
   triggerConditions: '-',
-  status: arrayOforderStatus[getRandomInt(0,2)],
+  status: arrayOforderStatus[getRandomInt(0, 2)],
 }))
 
 export const tradeHistoryColumnNames = [
   { label: 'Time', id: 'time' },
   { label: 'Pair', id: 'pair' },
   { label: 'Type', id: 'type' },
-  { label: 'Price', id: 'price', isNumber: true },
-  { label: 'Filled', id: 'filled', isNumber: true },
-  { label: 'Fee', id: 'fee', isNumber: true },
-  { label: 'Total', id: 'total', isNumber: true },
+  { label: 'Price', id: 'price' },
+  { label: 'Filled', id: 'filled' },
+  { label: 'Fee', id: 'fee' },
+  { label: 'Total', id: 'total' },
 ]
 
 export const tradeHistoryBody = new Array(13).fill(undefined).map((el, i) => ({
   time: new Date().toDateString(),
   pair: `BTC_${String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80))}`,
+    String.fromCharCode(getRandomInt(65, 80)) +
+    String.fromCharCode(getRandomInt(65, 80))}`,
   type: arrayOfOrdersType[getRandomInt(0, 3)],
   price: getRandomInt(100, 3000),
   filled: { render: '100%', isNumber: true },
@@ -104,8 +119,8 @@ export const fundsColumnNames = [
 
 export const fundsBody = new Array(13).fill(undefined).map((el, i) => ({
   coin: `${String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80)) +
-  String.fromCharCode(getRandomInt(65, 80))}`,
+    String.fromCharCode(getRandomInt(65, 80)) +
+    String.fromCharCode(getRandomInt(65, 80))}`,
   totalBalance: getRandomInt(100, 300000),
   availableBalance: getRandomInt(100, 3000),
   inOrder: getRandomInt(1, 100),
