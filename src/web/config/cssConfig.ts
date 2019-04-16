2f1bec9f5d382376f8402837e3674e74d397541f
avernikoz <avernikoz@gmail.com>
2019-04-16 18:47:32 +0300

    add chart config to cssConfig

--- src/web/config/cssConfig.ts
+++ src/web/config/cssConfig.ts
@@ -1,4 +1,18 @@
 export const CSS_CONFIG = {
   navBarHeight: 48,
   colors: ['#00C853', '#ceabff', '#0091EA', '#FF9833', '#EF5D28'],
+  chart: {
+    title: {
+      fontSize: '0.875rem'
+    },
+    headCell: {
+      fontSize: '0.75rem',
+    },
+    bodyCell: {
+      fontSize: '0.75rem',
+    },
+    content: {
+      fontSize: '0.75rem',
+    },
+  }
 }
