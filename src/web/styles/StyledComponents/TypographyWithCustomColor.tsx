74de542624f21e066c439e119d60ff0971b44182
avernikoz <avernikoz@gmail.com>
2019-04-16 18:47:51 +0300

    add fontSize to typohraphy prop

--- src/web/styles/StyledComponents/TypographyWithCustomColor.tsx
+++ src/web/styles/StyledComponents/TypographyWithCustomColor.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import { Typography } from '@material-ui/core'
 
 export const TypographyWithCustomColor = styled(
-  ({ textColor, ...otherProps }) => <Typography {...otherProps} />
+  ({ textColor, fontSize, ...otherProps }) => <Typography {...otherProps} />
 )`
+  font-size: ${(props: { fontSize?: string }) => props.fontSize ? props.fontSize : ''};
+  
   color: ${(props: { textColor?: string }) => props.textColor};
 `
