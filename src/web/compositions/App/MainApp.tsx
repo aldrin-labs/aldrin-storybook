57d58ed62b7df157bb7aa91d76c8d88779bcc6c3
anibaka <anibaka9@gmail.com>
2019-04-29 20:28:24 +0300

    add main app

--- src/web/compositions/App/MainApp.tsx
+++ src/web/compositions/App/MainApp.tsx
@@ -3,24 +3,19 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 // https://material-ui.com/customization/css-in-js/#other-html-element
-import JssProvider from 'react-jss/lib/JssProvider'
 import { create } from 'jss'
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
+import { jssPreset } from '@material-ui/core/styles'
 
-const generateClassName = createGenerateClassName()
 const jss = create(jssPreset())
 // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
 jss.options.insertionPoint = document.getElementById('jss-insertion-point')
 //
 
-import CssBaseline from '@material-ui/core/CssBaseline'
 import Footer from '@sb/components/Footer'
 
 import AnimatedNavBar from '@sb/components/NavBar/AnimatedNavBar'
-import ThemeWrapper from './ThemeWrapper/ThemeWrapper'
 import { AppGridLayout } from './App.styles'
 import ShowWarningOnMoblieDevice from '@sb/components/ShowWarningOnMoblieDevice'
-import { GlobalStyle } from '@sb/styles/cssUtils'
 import 'react-dates/initialize'
 import 'react-dates/lib/css/_datepicker.css'
 
@@ -41,18 +36,19 @@ const AppRaw = ({
     currentPage === '/chart' && chartPageView !== 'default'
 
   return (
-    <JssProvider jss={jss} generateClassName={generateClassName}>
-      <ThemeWrapper themeMode={themeMode}>
-        <CssBaseline />
+    <>
+      <AppGridLayout>
+        <AnimatedNavBar pathname={currentPage} hide={fullscreen} />
         {children}
-      </ThemeWrapper>
-    </JssProvider>
+      </AppGridLayout>
+      <Footer fullscreenMode={fullscreen} />
+      <ShowWarningOnMoblieDevice />
+    </>
   )
 }
 
 const mapStateToProps = (store: any) => ({
-  themeMode: store.ui.theme,
   chartPageView: store.chart.view,
 })
 
-export const App = withRouter(connect(mapStateToProps)(AppRaw))
+export default MainApp = withRouter(connect(mapStateToProps)(AppRaw))
