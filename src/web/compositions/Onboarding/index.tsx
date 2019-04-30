ba2819590b8d0693b1003b2b73028be64604f538
anibaka <anibaka9@gmail.com>
2019-04-30 13:16:18 +0300

    add styles

--- src/web/compositions/Onboarding/index.tsx
+++ src/web/compositions/Onboarding/index.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
+
+import { Grid, Paper } from '@material-ui/core'
+
 import {
-  MainContainer
+  MainContainer,
+  StepContainer,
 } from './styles'
+
+
 const Onboarding = () => (
   <MainContainer>
-    aaaa
+    <Grid container>
+      <Grid item xs={6}>
+        <StepContainer>xs=6</StepContainer>
+      </Grid>
+      <Grid item xs={2}>
+        <Paper>xs=6</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper>xs=6</Paper>
+      </Grid>
+    </Grid>
   </MainContainer>
 )
 
--- src/web/compositions/Onboarding/styles.ts
+++ src/web/compositions/Onboarding/styles.ts
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
 
+export const StepContainer = styled.div`
+  background: #fefcea; /* Для старых браузров */
+  background: linear-gradient(to top, #2EC7CC, #34DCBF);
+  height: 646px;
+`
+
 export const MainContainer = styled.div`
-  height: 100%;
+  height: 646px;
   width: 1054px;
   background: red;
   margin: 0px auto;
