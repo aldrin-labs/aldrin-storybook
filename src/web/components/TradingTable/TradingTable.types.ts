bde4c4d664c6cdea14cf12e22f8d7d8502f53422
avernikoz <avernikoz@gmail.com>
2019-05-07 18:05:55 +0300

    update TradingTable -- add get current key query

--- src/web/components/TradingTable/TradingTable.types.ts
+++ src/web/components/TradingTable/TradingTable.types.ts
@@ -1,5 +1,8 @@
-export interface IProps {
+import { Key } from '@core/types/ChartTypes'
 
+export interface IProps {
+  showCancelResult: ({ status, message }: { status: string, message: string }) => void
+  getSelectedKeyQuery: { chart: { selectedKey: Key }}
 }
 
 export interface IState {
