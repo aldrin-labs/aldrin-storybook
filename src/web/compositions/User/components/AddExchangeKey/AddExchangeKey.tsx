1b8608b2a756ac8831f1154fd5e587c8f6eb67b6
avernikoz <avernikoz@gmail.com>
2019-05-20 13:14:09 +0300

    fix height

--- src/web/compositions/User/components/AddExchangeKey/AddExchangeKey.tsx
+++ src/web/compositions/User/components/AddExchangeKey/AddExchangeKey.tsx
@@ -203,7 +203,7 @@ const SExchangeSelect = styled.div`
   flex-direction: column;
   margin: 30px 5px 5px 5px;
   width: 80%;
-  min-height: 50px;
+  min-height: 52px;
 `
 
 const SPaper = styled(Paper)`
