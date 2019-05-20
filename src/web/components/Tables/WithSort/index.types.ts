5e0023c4a5f09acdf36a7858bf172d2fdcf217ed
avernikoz <avernikoz@gmail.com>
2019-05-20 18:57:23 +0300

    defaultSort logic in WithSort table wrapper

--- src/web/components/Tables/WithSort/index.types.ts
+++ src/web/components/Tables/WithSort/index.types.ts
@@ -3,4 +3,10 @@ import { SortDirection } from '@material-ui/core/TableCell'
 export interface SortState {
   sortDirection: SortDirection
   sortColumn: string
+  defaultSort: defaultSortTypes
+}
+
+export type defaultSortTypes = {
+  sortColumn: string
+  sortDirection: 'asc' | 'desc'
 }
