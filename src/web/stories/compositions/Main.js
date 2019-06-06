3529653e8428ea2fc02d37ed2db34438d3efab5d
avernikoz <avernikoz@gmail.com>
2019-06-06 13:28:53 +0300

    remove redux from package.json & storybook stories

--- src/web/stories/compositions/Main.js
+++ src/web/stories/compositions/Main.js
@@ -1,9 +1,6 @@
 import React, { Suspense, lazy } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { IntlProvider } from 'react-intl'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { ErrorBoundary, Loading } from '@sb/components'
 import { storiesOf } from '@storybook/react'
 import { radios } from '@storybook/addon-knobs'
@@ -13,36 +10,28 @@ import { App } from '@sb/compositions/App/App'
 import ThemeWrapper from '@sb/compositions/App/ThemeWrapper/ThemeWrapper'
 
 import { client } from '@core/graphql/apolloClient'
-import { persistor, store } from '@sb/mocks/configureStore'
-import createHistory from 'history/createBrowserHistory'
-
-const history = createHistory()
 
 storiesOf('Compositions/Main', module).add('Main', () => (
-    <ApolloProvider client={client}>
-        <IntlProvider locale="en">
-            <Provider store={store}>
-                <PersistGate loading={null} persistor={persistor}>
-                    <ErrorBoundary>
-                        <Suspense fallback={<Loading centerAligned />}>
-                            <ThemeWrapper
-                                themeMode={radios(
-                                    `Theme`,
-                                    { dark: 'dark', light: 'light' },
-                                    'dark'
-                                )}
-                            >
-                                <Main
-                                    tab={'main'}
-                                    baseCoin={`USDT`}
-                                    isUSDCurrently={true}
-                                    dustFilter={-100}
-                                />
-                            </ThemeWrapper>
-                        </Suspense>
-                    </ErrorBoundary>
-                </PersistGate>
-            </Provider>
-        </IntlProvider>
-    </ApolloProvider>
+  <ApolloProvider client={client}>
+    <IntlProvider locale="en">
+      <ErrorBoundary>
+        <Suspense fallback={<Loading centerAligned />}>
+          <ThemeWrapper
+            themeMode={radios(
+              `Theme`,
+              { dark: 'dark', light: 'light' },
+              'dark'
+            )}
+          >
+            <Main
+              tab={'main'}
+              baseCoin={`USDT`}
+              isUSDCurrently={true}
+              dustFilter={-100}
+            />
+          </ThemeWrapper>
+        </Suspense>
+      </ErrorBoundary>
+    </IntlProvider>
+  </ApolloProvider>
 ))
--- src/web/stories/compositions/Rebalance.js
+++ src/web/stories/compositions/Rebalance.js
@@ -1,9 +1,6 @@
 import React, { Suspense, lazy } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { IntlProvider } from 'react-intl'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { ErrorBoundary, Loading } from '@sb/components'
 import { radios } from '@storybook/addon-knobs'
 
@@ -13,7 +10,6 @@ import { App } from '@sb/compositions/App/App'
 import ThemeWrapper from '@sb/compositions/App/ThemeWrapper/ThemeWrapper'
 
 import { client } from '@core/graphql/apolloClient'
-import { persistor, store } from '@sb/mocks/configureStore'
 import createHistory from 'history/createBrowserHistory'
 
 const history = createHistory()
@@ -21,28 +17,24 @@ const history = createHistory()
 storiesOf('Compositions/Rebalance', module).add('Rebalance', () => (
   <ApolloProvider client={client}>
     <IntlProvider locale="en">
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <ErrorBoundary>
-            <Suspense fallback={<Loading centerAligned />}>
-              <ThemeWrapper
-                themeMode={radios(
-                  `Theme`,
-                  { dark: 'dark', light: 'light' },
-                  'dark'
-                )}
-              >
-                <Rebalance
-                  baseCoin={`USDT`}
-                  tab={'rebalance'}
-                  isUSDCurrently={true}
-                  dustFilter={-100}
-                />
-              </ThemeWrapper>
-            </Suspense>
-          </ErrorBoundary>
-        </PersistGate>
-      </Provider>
+      <ErrorBoundary>
+        <Suspense fallback={<Loading centerAligned />}>
+          <ThemeWrapper
+            themeMode={radios(
+              `Theme`,
+              { dark: 'dark', light: 'light' },
+              'dark'
+            )}
+          >
+            <Rebalance
+              baseCoin={`USDT`}
+              tab={'rebalance'}
+              isUSDCurrently={true}
+              dustFilter={-100}
+            />
+          </ThemeWrapper>
+        </Suspense>
+      </ErrorBoundary>
     </IntlProvider>
   </ApolloProvider>
 ))
