f385afa6dd70f7ad379775441ef3d1efb5f9d4c4
9243031@gmail.com <9243031@gmail.com>
2019-06-24 20:53:33 +0500

    Rebalance Accordion Index

--- src/web/components/RebalanceAccorionIndex/RebalanceAccordionIndex.tsx
+++ src/web/components/RebalanceAccorionIndex/RebalanceAccordionIndex.tsx
@@ -6,6 +6,11 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
+import PortfolioRebalanceTableContainer from '@core/containers/PortfolioRebalanceTableContainer/PortfolioRebalanceTableContainer'
+
+import Slider from '../Slider/Slider';
+import {TypographyHeading, StyledTypography, StyledSubTypography, GridItemHeadingCustom} from './RebalanceAccorionIndex.styles' 
 
 const styles = theme => ({
   root: {
@@ -34,60 +39,220 @@ class RebalanceAccordionIndex extends React.Component {
   };
 
   render() {
-    const { classes } = this.props;
+    const { 
+      classes, 
+      isEditModeEnabled,
+      staticRows,
+      staticRowsMap,
+      totalStaticRows,
+      rows,
+      totalRows,
+      totalPercents,
+      totalTableRows,
+      isPercentSumGood,
+      undistributedMoney,
+      isUSDCurrently,
+      addMoneyInputValue,
+      theme,
+      loading,
+      red,
+      saveButtonColor,
+      secondary,
+      fontFamily,
+      totalSnapshotRows,
+      timestampSnapshot,
+      onDiscardChanges,
+      onSaveClick,
+      onReset,
+      onEditModeEnable,
+      updateState,
+      onNewSnapshot,
+      dustFilter,
+      showWarning,
+    } = this.props;
     const { expanded } = this.state;
 
     return (
       <div className={classes.root}>
         <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>General settings</Typography>
-            <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+            <Grid container justify="center">
+              <GridItemHeadingCustom borderColor="#F29C38" lg={3}>
+                <TypographyHeading>Free Assets</TypographyHeading>
+              </GridItemHeadingCustom>
+              <Grid item lg={3}>
+                <StyledTypography>Current value</StyledTypography>
+                <StyledSubTypography>$56,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={3}>
+                <Slider thumbBackground='blue' trackAfterBackground='red' trackBeforeBackground='green' />
+              </Grid>
+              <Grid item lg={2}>
+                <StyledTypography>Target value</StyledTypography>
+                <StyledSubTypography>$56,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={1}>
+                <Typography>...</Typography>
+              </Grid>
+            </Grid>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography>
-              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-              maximus est, id dignissim quam.
-            </Typography>
+
+            <PortfolioRebalanceTableContainer
+              key={`PortfolioRebalanceTableContainer`}
+            {...{
+              isEditModeEnabled,
+              staticRows,
+              staticRowsMap,
+              totalStaticRows,
+              rows,
+              totalRows,
+              totalPercents,
+              totalTableRows,
+              isPercentSumGood,
+              undistributedMoney,
+              isUSDCurrently,
+              addMoneyInputValue,
+              theme,
+              loading,
+              red,
+              saveButtonColor,
+              secondary,
+              fontFamily,
+              totalSnapshotRows,
+              timestampSnapshot,
+              onDiscardChanges,
+              onSaveClick,
+              onReset,
+              onEditModeEnable,
+              updateState,
+              onNewSnapshot,
+              dustFilter,
+              showWarning,
+              }}
+            />
+
           </ExpansionPanelDetails>
         </ExpansionPanel>
+        
         <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
           <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Users</Typography>
-            <Typography className={classes.secondaryHeading}>
-              You are currently not an owner
-            </Typography>
+            <Grid container justify="center">
+              <GridItemHeadingCustom borderColor="#DEDB8E" lg={3}>
+                <TypographyHeading>Free Assets</TypographyHeading>
+              </GridItemHeadingCustom>
+              <Grid item lg={3}>
+                <StyledTypography>Current value</StyledTypography>
+                <StyledSubTypography>$74,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={3}>
+                <Typography>POLZUNOK</Typography>
+              </Grid>
+              <Grid item lg={2}>
+                <StyledTypography>Target value</StyledTypography>
+                <StyledSubTypography>$90,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={1}>
+                <Typography>...</Typography>
+              </Grid>
+            </Grid>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography>
-              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-              diam eros in elit. Pellentesque convallis laoreet laoreet.
-            </Typography>
+
+            <PortfolioRebalanceTableContainer
+              key={`PortfolioRebalanceTableContainer`}
+            {...{
+              isEditModeEnabled,
+              staticRows,
+              staticRowsMap,
+              totalStaticRows,
+              rows,
+              totalRows,
+              totalPercents,
+              totalTableRows,
+              isPercentSumGood,
+              undistributedMoney,
+              isUSDCurrently,
+              addMoneyInputValue,
+              theme,
+              loading,
+              red,
+              saveButtonColor,
+              secondary,
+              fontFamily,
+              totalSnapshotRows,
+              timestampSnapshot,
+              onDiscardChanges,
+              onSaveClick,
+              onReset,
+              onEditModeEnable,
+              updateState,
+              onNewSnapshot,
+              dustFilter,
+              showWarning,
+              }}
+            />
+
           </ExpansionPanelDetails>
         </ExpansionPanel>
         <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Advanced settings</Typography>
-            <Typography className={classes.secondaryHeading}>
-              Filtering has been entirely disabled for whole web server
-            </Typography>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Grid container justify="center">
+              <GridItemHeadingCustom borderColor="#4152AF" lg={3}>
+                <TypographyHeading>Free Assets</TypographyHeading>
+              </GridItemHeadingCustom>
+              <Grid item lg={3}>
+                <StyledTypography>Current value</StyledTypography>
+                <StyledSubTypography>$65,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={3}>
+                <Typography>POLZUNOK</Typography>
+              </Grid>
+              <Grid item lg={2}>
+                <StyledTypography>Target value</StyledTypography>
+                <StyledSubTypography>$45,500.00</StyledSubTypography>
+              </Grid>
+              <Grid item lg={1}>
+                <Typography>...</Typography>
+              </Grid>
+            </Grid>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
-              eros, vitae egestas augue. Duis vel est augue.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel expanded={expanded === 'panel4'} onChange={this.handleChange('panel4')}>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>Personal data</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas
-              eros, vitae egestas augue. Duis vel est augue.
-            </Typography>
+
+            <PortfolioRebalanceTableContainer
+              key={`PortfolioRebalanceTableContainer`}
+            {...{
+              isEditModeEnabled,
+              staticRows,
+              staticRowsMap,
+              totalStaticRows,
+              rows,
+              totalRows,
+              totalPercents,
+              totalTableRows,
+              isPercentSumGood,
+              undistributedMoney,
+              isUSDCurrently,
+              addMoneyInputValue,
+              theme,
+              loading,
+              red,
+              saveButtonColor,
+              secondary,
+              fontFamily,
+              totalSnapshotRows,
+              timestampSnapshot,
+              onDiscardChanges,
+              onSaveClick,
+              onReset,
+              onEditModeEnable,
+              updateState,
+              onNewSnapshot,
+              dustFilter,
+              showWarning,
+              }}
+            />
+
           </ExpansionPanelDetails>
         </ExpansionPanel>
       </div>
---
+++ src/web/components/RebalanceAccorionIndex/RebalanceAccorionIndex.styles.tsx
@@ -0,0 +1,36 @@
+
+import styled from 'styled-components'
+import { Grid, Typography } from '@material-ui/core'
+
+export {TypographyHeading, StyledTypography, StyledSubTypography, GridItemHeadingCustom }
+
+
+const TypographyHeading = styled(Typography)`
+`
+
+const GridItemHeadingCustom = styled(Grid)`
+    border-left:3px solid ${props => props.borderColor || '#fff'};
+    align-self: center;
+    padding: 5px 0 5px 20px;
+`
+
+const StyledTypography = styled(Typography)`
+  /* font-family: 'Aguafina Script', cursive; */
+  font-size: 65%;
+  /* font-size: 11px; */
+  line-height: 25px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${props => props.primaryColor || '#7284A0'};
+  text-align: ${props => props.position || 'none'};
+`
+
+const StyledSubTypography = styled(Typography)`
+  font-family: 'Avenir Next';
+  font-size: 18px;
+  line-height: 25px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${props => props.primaryColor ||'#97c15c'};
+  text-align: ${props => props.position || 'none'};
+`
