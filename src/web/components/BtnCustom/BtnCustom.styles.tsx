6c21f7f5689ef5ac2fc9077fdd6d00d3245296bc
9243031@gmail.com <9243031@gmail.com>
2019-06-24 20:58:13 +0500

    Btn Custom

---
+++ src/web/components/BtnCustom/BtnCustom.styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import { Button } from '@material-ui/core'
+
+export {
+    BtnCustom,
+}
+
+const BtnCustom = styled(Button)`
+  width: ${props => props.btnWidth || "150px"};
+  border: 2px solid ${props => props.btnColor || '#333'};
+  border-radius: 23px;
+  color: ${props => props.btnColor || '#333'}; 
+  font-size: 11px;
+  margin: 10px auto;
+`
