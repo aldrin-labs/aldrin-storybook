242a93e860dd951aee6376098396b474e180b295
9243031@gmail.com <9243031@gmail.com>
2019-07-10 21:25:20 +0500

    Portfolio chart updated

--- src/web/components/PortfolioRebalanceChart/PortfolioRebalanaceChart.tsx
+++ src/web/components/PortfolioRebalanceChart/PortfolioRebalanaceChart.tsx
@@ -4,18 +4,55 @@ import { BarChart, CardHeader } from '@sb/components/index'
 import { ChartContainer, Chart } from './PortfolioRebalanceChart.styles'
 import { IProps } from './PortfolioRebalanceChart.types'
 
+import ProgressBarWrapper from '@sb/components/ProgressBarCustom/ProgressBarWrapper.tsx'
+
 const PortfolioRebalanceChartComponent = ({
-  theme,
-  title,
-  rebalanceChartsData,
-  ...otherProps
+  // theme, //TODO: Я ЗАКОММЕНТИЛ
+  // title,
+  // rebalanceChartsData,
+  // ...otherProps
+  coinData,
+  otherCoinData,
+  otherCoinsPercentage,
+  isSectionChart,
+  sectionDataProgress,
 }: IProps) => (
   <ChartContainer>
-    <CardHeader title={title} />
-    <Chart background={theme.palette.background.default}>
-      <BarChart theme={theme} charts={rebalanceChartsData} {...otherProps} />
-    </Chart>
+    <ProgressBarWrapper
+      sectionDataProgress={sectionDataProgress}
+      coinData={coinData}
+      otherCoinData={otherCoinData}
+      otherCoinsPercentage={otherCoinsPercentage}
+      isSectionChart={isSectionChart}
+    />
   </ChartContainer>
 )
 
 export default PortfolioRebalanceChartComponent
+
+// import React from 'react'
+
+// import { BarChart, CardHeader } from '@sb/components/index'
+// import { ChartContainer, Chart } from './PortfolioRebalanceChart.styles'
+// import { IProps } from './PortfolioRebalanceChart.types'
+
+// import ProgressBarWrapper from '@sb/components/ProgressBarCustom/ProgressBarWrapper.tsx'
+
+// const PortfolioRebalanceChartComponent = ({
+//   // theme, //TODO: Я ЗАКОММЕНТИЛ
+//   // title,
+//   // rebalanceChartsData,
+//   // ...otherProps
+//   coinData,
+// }: IProps) => (
+//   <ChartContainer>
+//     <CardHeader title={title} />
+//     <Chart background={theme.palette.background.default}>
+//       <ProgressBarWrapper coinData={coinData} data={'1'} />
+
+//          <BarChart theme={theme} charts={rebalanceChartsData} {...otherProps} />
+//      </Chart>
+//    </ChartContainer>
+//  )
+
+//  export default PortfolioRebalanceChartComponent
