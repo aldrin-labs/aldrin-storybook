fedef694e418fc1d703202f56a17815145301eb2
9243031@gmail.com <9243031@gmail.com>
2019-07-25 21:37:22 +0500

    Transaction table update

--- src/web/components/TradeOrderHistoryTable/TraderOrderHistoryTable.tsx
+++ src/web/components/TradeOrderHistoryTable/TraderOrderHistoryTable.tsx
@@ -3,42 +3,75 @@ import { TableWithSort } from '@sb/components/index'
 import { IProps } from './TraderOrderHistoryTable.types'
 
 class TradeOrderHistoryTable extends React.Component<IProps> {
-  // state = {
-  //   activeSortArg: null,
-  //   page: 0,
-  //   rowsPerPage: 20,
-  // }
+  state = {
+    activeSortArg: null,
+    page: 0,
+    rowsPerPage: 6,
+  }
 
-  // handleChangePage = (
-  //   event: React.ChangeEvent<HTMLInputElement>,
-  //   page: number
-  // ) => {
-  //   this.setState({ page })
-  // }
+  handleChangePage = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    page: number
+  ) => {
+    this.setState({ page })
+  }
 
-  // handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   this.setState({ rowsPerPage: event.target.value })
-  // }
+  handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ rowsPerPage: event.target.value })
+  }
 
   render() {
     const { rows } = this.props
+
+    const tableStyles = {
+      heading: {
+        background: 'transparent',
+        color: '#ABBAD1',
+        fontFamily: `DM Sans`,
+        textTransform: 'uppercase',
+        fontWeight: '700',
+        fontSize: '0.75rem',
+        borderBottom: '1px solid #E0E5EC',
+        padding: '0 0 10px 10px',
+      },
+      title: {},
+      cell: {
+        letterSpacing: '1.5px',
+        textTransform: 'uppercase',
+        color: '#16253D',
+        fontFamily: 'DM Sans',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        fontSize: '0.75rem',
+        lineHeight: '31px',
+        background: 'transparent',
+        borderBottom: '1px solid #E0E5EC',
+        height: '48px',
+        //margin: '0 15px',
+        paddingLeft: '10px',
+        '&:first-child': {
+          color: 'green',
+        },
+      },
+    }
 
     return (
       <TableWithSort
         id="PortfolioActionsTable"
         padding="dense"
-        title="TRANSACTIONS"
+        // title="TRANSACTIONS"
         data={{ body: rows.body }}
         columnNames={rows.head}
         emptyTableText="No history"
-        // pagination={{
-        //   enabled: true,
-        //   page: this.state.page,
-        //   rowsPerPage: this.state.rowsPerPage,
-        //   rowsPerPageOptions: [20, 50, 100, 200],
-        //   handleChangeRowsPerPage: this.handleChangeRowsPerPage,
-        //   handleChangePage: this.handleChangePage,
-        // }}
+        tableStyles={tableStyles}
+        pagination={{
+          enabled: true,
+          page: this.state.page,
+          rowsPerPage: 6, // this.state.rowsPerPage,
+          // rowsPerPageOptions: [20, 50, 100, 200],
+          handleChangeRowsPerPage: this.handleChangeRowsPerPage,
+          handleChangePage: this.handleChangePage,
+        }}
       />
     )
   }
