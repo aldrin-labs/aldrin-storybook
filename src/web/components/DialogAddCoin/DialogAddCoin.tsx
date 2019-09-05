90b943ade39f34e0591403bf5f5771a87e0b8013
Likbes <likbes.js@gmail.com>
2019-09-05 17:49:42 +0300

    add coin in storybook

--- src/web/components/DialogAddCoin/DialogAddCoin.tsx
+++ src/web/components/DialogAddCoin/DialogAddCoin.tsx
@@ -13,6 +13,7 @@ import SelectCoinList from '@core/components/SelectCoinList/SelectCoinList'
 class DialogAddCoin extends React.Component {
   state = {
     open: false,
+    mouseInPopup: false,
   }
 
   handleClickOpen = () => {
@@ -20,7 +21,15 @@ class DialogAddCoin extends React.Component {
   }
 
   handleClose = () => {
-    this.setState({ open: false })
+    !this.state.mouseInPopup ? this.setState({ open: false }) : null
+  }
+
+  mouseEnter = () => {
+    this.setState({ mouseInPopup: true })
+  }
+
+  mouseLeave = () => {
+    this.setState({ mouseInPopup: false })
   }
 
   // handleSelectChange = (index, symbol, optionSelected) => {
@@ -32,30 +41,47 @@ class DialogAddCoin extends React.Component {
     const { handleSelectChange, onAddRowButtonClick } = this.props
 
     return (
-      <div>
+      <div style={{ position: 'relative', padding: '1rem 0' }}>
         <BtnCustom
           variant="outlined"
           color="#165BE0"
           btnWidth="100px"
-          onClick={this.handleClickOpen}
+          onFocus={this.handleClickOpen}
+          onBlur={this.handleClose}
           style={{
             position: 'relative',
             left: '75%',
             top: 0,
             transform: 'translateX(-50%)',
-            width: '10rem'
+            width: '10rem',
+            height: '2.5rem',
           }}
         >
           Add Coin
         </BtnCustom>
-        <Dialog
+        {/* <Dialog
           style={{ background: 'transparent' }}
           open={this.state.open}
           onClose={this.handleClose}
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description"
         >
-          <DialogContent style={{ minWidth: '400px', height: '220px' }}>
+          <DialogContent style={{ minWidth: '400px', height: '220px' }}> */}
+        {this.state.open && (
+          <div
+            onMouseEnter={this.mouseEnter}
+            onMouseLeave={this.mouseLeave}
+            onBlur={this.handleClose}
+            style={{
+              position: 'absolute',
+              right: '0',
+              top: '4.5rem',
+              background: 'white',
+              border: '1px solid #ABBAD1',
+              width: '24rem',
+              borderRadius: '1.5rem',
+            }}
+          >
             <SelectCoinList
               //ref={handleRef}
               key={`inputCoinSymbol${'index'}`}
@@ -63,36 +89,39 @@ class DialogAddCoin extends React.Component {
               isClearable={true}
               isSearchable={true}
               openMenuOnClick={false}
-              menuPortalTarget={document.body}
-              menuPortalStyles={{
-                zIndex: 11111,
-              }}
+              // menuPortalTarget={document.body}
+              // menuPortalStyles={{
+              //   zIndex: 11111,
+              // }}
               menuStyles={{
                 fontSize: '12px',
                 minWidth: '150px',
-                height: '140px',
-                width: '350px',
-                padding: '5px 8px',
-                borderRadius: '14px',
+                padding: '0 0 0 1.5rem',
+                borderRadius: '1.5rem',
                 textAlign: 'center',
-                border: '1px solid transparent',
-                boxShadow: '0 0 0 1px transparent',
                 background: 'white',
+                position: 'relative',
+                boxShadow: 'none',
+                border: 'none',
               }}
               menuListStyles={{
-                height: '140px',
+                height: '8rem',
               }}
               optionStyles={{
                 color: '#7284A0',
                 background: 'transparent',
                 textAlign: 'left',
                 fontSize: '12px',
+                position: 'relative',
 
                 '&:hover': {
                   borderRadius: '1rem',
                   color: '#16253D',
                   background: '#E7ECF3',
                 },
+              }}
+              controlStyles={{
+                padding: '1rem 1.5rem 0 1.5rem',
               }}
               clearIndicatorStyles={{
                 padding: '2px',
@@ -108,12 +137,15 @@ class DialogAddCoin extends React.Component {
               dropdownIndicatorStyles={{
                 display: 'none',
               }}
-              noOptionsMessage={() => `No such coin in our DB found`}
+              // noOptionsMessage={() => `No such coin in our DB found`}
               valueContainerStyles={{
                 border: '2px solid #E7ECF3',
                 borderRadius: '54px',
                 background: '#F2F4F6',
                 paddingLeft: '15px',
+              }}
+              noOptionsMessageStyles={{
+                textAlign: 'left',
               }}
               menuIsOpen={true}
               onChange={(
@@ -123,8 +155,8 @@ class DialogAddCoin extends React.Component {
                 } | null
               ) => onAddRowButtonClick(optionSelected.label)}
             />
-          </DialogContent>
-        </Dialog>
+          </div>
+        )}
       </div>
     )
   }
