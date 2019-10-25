e885db0d9e33b80c221debe79a0e65ab6ac56488
avernikoz <avernikoz@gmail.com>
2019-10-25 15:04:59 +0300

    fix trading terminal balances

--- src/web/components/TraidingTerminal/index.tsx
+++ src/web/components/TraidingTerminal/index.tsx
@@ -22,6 +22,8 @@ import { CSS_CONFIG } from '@sb/config/cssConfig'
 
 import { IProps, FormValues, IPropsWithFormik, priceType } from './types'
 
+import { formatNumberToUSFormat, stripDigitPlaces } from '@core/utils/PortfolioTableUtils'
+
 import {
   Container,
   NameHeader,
@@ -227,6 +229,9 @@ class TraidingTerminal extends PureComponent<IPropsWithFormik> {
       (percentage / 100)
     ).toFixed(4)
 
+    const firstValuePair = stripDigitPlaces(funds[0].value) === null ? funds[0].value : formatNumberToUSFormat(stripDigitPlaces(funds[0].value))
+    const secondValuePair = stripDigitPlaces(funds[1].value) === null ? funds[1].value : formatNumberToUSFormat(stripDigitPlaces(funds[1].value))
+
     return (
       <Container background={'transparent'}>
         <div>
@@ -240,9 +245,9 @@ class TraidingTerminal extends PureComponent<IPropsWithFormik> {
                 <BalanceItem item xs={4}>
                   <TradingItemTitle>{pair[0]}</TradingItemTitle>
                   <TradingItemValue>
-                    {funds[0].quantity.toFixed(4)}
+                    {funds[0].quantity}
                     <TradingItemSubValue>
-                      {`$${funds[0].value.toFixed(2)}`}
+                      {`$${firstValuePair}`}
                     </TradingItemSubValue>
                   </TradingItemValue>
                 </BalanceItem>
@@ -250,9 +255,9 @@ class TraidingTerminal extends PureComponent<IPropsWithFormik> {
                 <BalanceItem item xs={4} lastItem>
                   <TradingItemTitle>{pair[1]}</TradingItemTitle>
                   <TradingItemValue>
-                    {funds[1].quantity.toFixed(4)}
+                    {funds[1].quantity}
                     <TradingItemSubValue>
-                      {`$${funds[1].value.toFixed(2)}`}
+                      {`$${secondValuePair}`}
                     </TradingItemSubValue>
                   </TradingItemValue>
                 </BalanceItem>
