37f8689c05c97083145d5c5ff8908e751de801b5
Likbes <likbes.js@gmail.com>
2019-11-18 22:25:15 +0200

    delete console.logs

--- src/web/compositions/Chart/Tables/OrderBookTable/OrderBookTableContainer.tsx
+++ src/web/compositions/Chart/Tables/OrderBookTable/OrderBookTableContainer.tsx
@@ -95,7 +95,6 @@ class OrderBookTableContainer extends Component<IProps, IState> {
   }
 
   componentDidMount() {
-    console.log('this.props', this.props)
     if (this.props.subscribeToMore) {
       //  unsubscribe from old exchange when you first time change exchange
       unsubscribe && unsubscribe()
--- src/web/compositions/Chart/Tables/TradeHistoryTable/TableContainer.tsx
+++ src/web/compositions/Chart/Tables/TradeHistoryTable/TableContainer.tsx
@@ -69,8 +69,6 @@ class TableContainer extends Component<IProps, IState> {
   }
 
   componentDidMount() {
-    console.log('this.props', this.props)
-
     if (this.props.subscribeToMore) {
       //  unsubscribe from old exchange when you first time change exchange
       unsubscribe && unsubscribe()
