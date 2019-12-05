260132fbfff5b12c1af20c126119f3cd4d1122d7
avernikoz <avernikoz@gmail.com>
2019-12-05 21:25:41 +0300

    update types

--- src/web/compositions/Profile/compositions/DepositWithdrawalComponents/AccountBlock.types.ts
+++ src/web/compositions/Profile/compositions/DepositWithdrawalComponents/AccountBlock.types.ts
@@ -1,7 +1,7 @@
 export interface IProps {
-    isDepositPage?: boolean
-    selectedCoin: string
-    setSelectedCoin: (selectedCoin: string) => void
-    selectedAccount: string
-    setSelectedAccount: (selectedAccount: string) => void
+  isDepositPage?: boolean
+  selectedCoin: string
+  setSelectedCoin: (selectedCoin: string) => void
+  selectedAccount: { keyId: string; label: string; value: number }
+  setSelectedAccount: (selectedAccount: { keyId: string; label: string; value: number }) => void
 }
