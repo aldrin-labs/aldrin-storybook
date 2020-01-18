6afdfe32681faa8263459bacafd6d470013ec714
Likbes <likbes.js@gmail.com>
2020-01-18 02:21:04 +0200

    add wrapper for date and types

---
+++ src/web/components/TradingTable/StrategiesHistoryTable/StrategiesHistoryDataWrapper.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import moment from 'moment'
+
+import { getEndDate } from '../TradingTable.utils'
+import { IProps, IState } from './TradeHistoryDataWrapper.types'
+import StrategiesHistoryTable from './StrategiesHistoryTable'
+
+export default class StrategiesHistoryDataWrapper extends React.PureComponent<IProps, IState> {
+  state: IState = {
+    startDate: getEndDate('1Day'),
+    endDate: moment().endOf('day'),
+    focusedInput: null,
+    activeDateButton: '1Day',
+  }
+
+  onClearDateButtonClick = () => {
+    this.setState({
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+      activeDateButton: null,
+    })
+  }
+
+  onDateButtonClick = async (stringDate: string) => {
+    this.setState(
+      {
+        activeDateButton: stringDate,
+        startDate: getEndDate(stringDate),
+        endDate: moment().endOf('day'),
+      },
+      () => {
+        // TODO: there should be mutation for search:
+      }
+    )
+  }
+
+  onDatesChange = ({
+    startDate,
+    endDate,
+  }: {
+    startDate: moment.Moment | null
+    endDate: moment.Moment | null
+  }) => this.setState({ startDate, endDate })
+
+  onFocusChange = (focusedInput: string) => this.setState({ focusedInput })
+
+  render() {
+    const {
+      tab,
+      tabIndex,
+      show,
+      handleTabChange,
+      selectedKey,
+      currencyPair,
+      marketType,
+      canceledOrders,
+      arrayOfMarketIds,
+    } = this.props
+
+    const { focusedInput, endDate, activeDateButton, startDate } = this.state
+
+    const maximumDate = moment().endOf('day')
+    const minimumDate = moment().subtract(3, 'years')
+
+    return (
+      <StrategiesHistoryTable
+        {...{
+          tab,
+          tabIndex,
+          selectedKey,
+          show,
+          marketType,
+          canceledOrders,
+          currencyPair,
+          arrayOfMarketIds,
+          handleTabChange,
+          focusedInput,
+          endDate,
+          activeDateButton,
+          startDate,
+          maximumDate,
+          minimumDate,
+          onClearDateButtonClick: this.onClearDateButtonClick,
+          onDateButtonClick: this.onDateButtonClick,
+          onDatesChange: this.onDatesChange,
+          onFocusChange: this.onFocusChange,
+        }}
+      />
+    )
+  }
+}
