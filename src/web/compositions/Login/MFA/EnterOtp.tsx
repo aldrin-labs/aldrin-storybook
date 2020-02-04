45c8b0d2791876f630fbd416f542db531610359c
avernikoz <avernikoz@gmail.com>
2020-02-04 23:03:36 +0300

    add otp page

---
+++ src/web/compositions/Login/MFA/EnterOtp.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import OtpInput from 'react-otp-input'
+import { compose } from 'recompose'
+import { Grid, Typography, Theme } from '@material-ui/core'
+import { withTheme } from '@material-ui/styles'
+import SvgIcon from '@sb/components/SvgIcon'
+
+import GoogleAuthenticationLogo from '@icons/googleAuthentication.svg'
+
+const EnterOtp = ({
+  theme,
+  changeStep,
+  onChangeOtp,
+}: {
+  theme: Theme
+  changeStep: (step: string) => void
+  onChangeOtp: (otp: string) => void
+}) => {
+  return (
+    <Grid>
+      <Grid>
+        <Typography>Google Authentication</Typography>
+      </Grid>
+      <Grid>
+        <SvgIcon src={GoogleAuthenticationLogo} width="3.5rem" height="auto" />
+        <Typography>
+          Input the 6-digit code in your Google Authenticator app
+        </Typography>
+      </Grid>
+      <Grid>
+        <OtpInput
+          onChange={(otp: string) => onChangeOtp(otp)}
+          numInputs={6}
+          separator={<span>-</span>}
+        />
+      </Grid>
+      <Grid>
+        <Typography onClick={() => changeStep('recoveryCode')}>Can't access Google Authenticator?</Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default compose(withTheme)(EnterOtp)
