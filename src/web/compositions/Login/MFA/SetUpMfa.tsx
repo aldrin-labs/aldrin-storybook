7b01a1f268f5d496c5ff7e92eaf886823ac5b498
avernikoz <avernikoz@gmail.com>
2020-02-11 19:56:38 +0300

    add styles to SetUpMfa

--- src/web/compositions/Login/MFA/SetUpMfa.tsx
+++ src/web/compositions/Login/MFA/SetUpMfa.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 const QRCode = require('qrcode.react')
 import { compose } from 'recompose'
-import { Grid, Typography, Button, Theme } from '@material-ui/core'
+import { Grid, Theme } from '@material-ui/core'
 import { withTheme } from '@material-ui/styles'
+
+import {
+  MfaBackupCode,
+  MfaBackupText,
+  MfaHeading,
+  MfaStep,
+  MfaText,
+  MfaSubHeading,
+  SubmitLoginButton,
+} from '@sb/compositions/Login/Login.styles'
 
 const SetUpMfa = ({
   theme,
@@ -16,41 +26,57 @@ const SetUpMfa = ({
   recoveryCode: string
 }) => {
   return (
-    <Grid>
-      <Grid>
-        <Typography>Set 2 factor authentication</Typography>
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      direction="column"
+      style={{ width: '56%' }}
+    >
+      <Grid style={{ paddingBottom: '3rem' }}>
+        <MfaHeading>Set 2 factor authentication</MfaHeading>
       </Grid>
-      <Grid>
-        <Typography>step 2/2</Typography>
+      <Grid style={{ paddingBottom: '0.5rem' }}>
+        <MfaStep>step 2/2</MfaStep>
       </Grid>
-      <Grid>
-        <Typography>
+      <Grid style={{ paddingBottom: '5rem' }}>
+        <MfaSubHeading>
           Use your Authenticator app to scan the barcode below
-        </Typography>
+        </MfaSubHeading>
       </Grid>
-      <Grid>
+      <Grid
+        container
+        justify="space-around"
+        alignItems="center"
+        style={{ paddingBottom: '5rem' }}
+      >
         <Grid>
           <QRCode value={barcodeUri} />
         </Grid>
-        <Grid>
-          <Grid>
-            <Typography>
+        <Grid style={{ width: '56%' }} container direction="column">
+          <Grid style={{ paddingBottom: '0.5rem' }}>
+            <MfaText>
               Write down this backup key in a safe place in case you lose your
-              device It only shows up once here
-            </Typography>
+              device
+            </MfaText>
+            <MfaText>It only shows up once here</MfaText>
           </Grid>
           <Grid>
-            <Typography>Backup key</Typography>
+            <MfaBackupText>Backup key</MfaBackupText>
           </Grid>
           <Grid>
-            <Typography>{recoveryCode}</Typography>
+            <MfaBackupCode>{recoveryCode}</MfaBackupCode>
           </Grid>
         </Grid>
       </Grid>
       <Grid>
-        <Button onClick={() => changeStep('enterOtp')}>
+        <SubmitLoginButton
+          variant="contained"
+          color="secondary"
+          onClick={() => changeStep('enterOtp')}
+        >
           Save changes and enable 2FA
-        </Button>
+        </SubmitLoginButton>
       </Grid>
     </Grid>
   )
