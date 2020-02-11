869275455fd9b7262460ce9be3f1238ef555a595
avernikoz <avernikoz@gmail.com>
2020-02-11 20:55:18 +0300

    add styles for ChooseMfaProvider

--- src/web/compositions/Login/MFA/ChooseMfaProvider.tsx
+++ src/web/compositions/Login/MFA/ChooseMfaProvider.tsx
@@ -3,6 +3,16 @@ import { compose } from 'recompose'
 import { Grid, Typography, Button, Theme, Link } from '@material-ui/core'
 import { withTheme } from '@material-ui/styles'
 
+import {
+  MfaBackupCode,
+  MfaBackupText,
+  MfaHeading,
+  MfaStep,
+  MfaText,
+  MfaSubHeading,
+  SubmitLoginButton,
+} from '@sb/compositions/Login/Login.styles'
+
 import SvgIcon from '@sb/components/SvgIcon'
 import AppleLogo from '@icons/apple.svg'
 import AndroidLogo from '@icons/android.svg'
@@ -17,42 +27,47 @@ const ChooseMfaProvider = ({
   changeStep: (step: string) => void
 }) => {
   return (
-    <Grid>
-      <Grid>
-        <Typography>Set 2 factor authentication</Typography>
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      direction="column"
+      style={{ width: '56%' }}
+    >
+      <Grid style={{ paddingBottom: '3rem' }}>
+        <MfaHeading>Set 2 factor authentication</MfaHeading>
       </Grid>
-      <Grid>
-        <Typography>step 1/2</Typography>
+      <Grid style={{ paddingBottom: '0.5rem' }}>
+        <MfaStep>step 1/2</MfaStep>
       </Grid>
-      <Grid>
-        <Typography>
+      <Grid style={{ paddingBottom: '8rem' }}>
+        <MfaSubHeading>
           Download the Google Authenticator or any other 2fa App on your phone
-        </Typography>
+        </MfaSubHeading>
       </Grid>
-      <Grid>
-        <Grid>
-          <Grid>
-            <Grid>
-              <SvgIcon src={AppleLogo} width="3.5rem" height="auto" />
-            </Grid>
-            <Grid>
-              <SvgIcon src={AppleQrCodeAppLink} width="20%" height="auto" />
-            </Grid>
-          </Grid>
+      <Grid
+        container
+        justify="space-between"
+        alignItems="center"
+        style={{ paddingBottom: '5rem', flexWrap: 'nowrap' }}
+      >
+        <Grid container justify="center" alignItems="center" direction="column">
+          <SvgIcon src={AppleLogo} width="3.5rem" height="auto" />
+          <SvgIcon src={AppleQrCodeAppLink} width="160px" height="auto" />
         </Grid>
-        <Grid>
-          <Grid>
-            <SvgIcon src={AndroidLogo} width="3.5rem" height="auto" />
-          </Grid>
-          <Grid>
-            <SvgIcon src={AndroidQrCodeAppLink} width="20%" height="auto" />
-          </Grid>
+        <Grid container justify="center" alignItems="center" direction="column">
+          <SvgIcon src={AndroidLogo} width="4.5rem" height="auto" style={{ paddingBottom: '1.5rem' }} />
+          <SvgIcon src={AndroidQrCodeAppLink} width="144px" height="auto" />
         </Grid>
       </Grid>
       <Grid>
-        <Button onClick={() => changeStep('setupMfa')}>
+        <SubmitLoginButton
+          variant="contained"
+          color="secondary"
+          onClick={() => changeStep('setupMfa')}
+        >
           I already have the app
-        </Button>
+        </SubmitLoginButton>
       </Grid>
     </Grid>
   )
