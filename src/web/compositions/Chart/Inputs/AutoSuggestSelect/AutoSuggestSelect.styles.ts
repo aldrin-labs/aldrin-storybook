1071c26a0243aceb93f01af6718c98fe637bb269
Likbes <likbes.js@gmail.com>
2020-02-18 23:14:20 +0200

    fix selects for small screens

--- src/web/compositions/Chart/Inputs/AutoSuggestSelect/AutoSuggestSelect.styles.ts
+++ src/web/compositions/Chart/Inputs/AutoSuggestSelect/AutoSuggestSelect.styles.ts
@@ -6,6 +6,16 @@ export const SelectR = styled(ReactSelectComponent)`
   width: 100%;
   font-size: 1.28rem;
   display: flex;
+
+  @media (max-width: 1440px) {
+    & > div {
+      font-size: 0.9rem;
+    }
+
+    & svg {
+      height: 12px;
+    }
+  }
 `
 
 export const ExchangePair = styled.div`
