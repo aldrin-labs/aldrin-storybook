f1edf812de08a58ef29ae25fc043d25f88dadfa9
avernikoz <avernikoz@gmail.com>
2020-02-18 21:19:56 +0300

    change color -> background

--- src/web/components/Login/Login.tsx
+++ src/web/components/Login/Login.tsx
@@ -71,7 +71,7 @@ class LoginClassComponent extends React.Component<Props> {
               variant="contained"
               // onClick={this.hangleGoToSignupPage}
               className="loginButton"
-              style={{ padding: '1px 16px', margin: '0 1rem', color: '#97C15C' }}
+              style={{ padding: '1px 16px', margin: '0 1rem', backgroundColor: '#97C15C' }}
             >
               Sign Up
             </Button>
