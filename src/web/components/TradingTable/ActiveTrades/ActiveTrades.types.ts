8c51c37c0a334f61e3e5439a3bb3a119877627ae
Likbes <likbes.js@gmail.com>
2020-02-20 13:41:17 +0200

    add types for active sm table and fix flickering for sm

---
+++ src/web/components/TradingTable/ActiveTrades/ActiveTrades.types.ts
@@ -0,0 +1,93 @@
+import { Theme } from '@material-ui/core'
+
+type SmartOrderConditions = {
+  pair: string
+  marketType: number
+  leverage: number
+}
+
+type SmartOrderState = {}
+
+export type Price = {
+  pair: string
+  price: number
+}
+
+export type Fund = {
+  free: number
+  assetType: number
+  quantity: number
+  asset: {
+    symbol: string
+    priceUSD: number
+  }
+}
+
+export type SmartOrder = {
+  _id: string
+  enabled: boolean
+  createdAt: number | string
+  conditions: SmartOrderConditions
+  state: SmartOrderState
+}
+
+export interface IProps {
+  tab: string
+  show: boolean
+  marketType: 0 | 1
+  exchange: string
+  currencyPair: string
+  arrayOfMarketIds: string[]
+  priceFromOrderbook: number
+  quantityPrecision: number
+  pricePrecision: number
+  theme: Theme
+  getActiveStrategiesQuery: {
+    getActiveStrategies: SmartOrder[]
+  }
+  getFundsQuery: {
+    getFunds: Fund[]
+  }
+  selectedKey: {
+    keyId: string
+  }
+  canceledOrders: string[]
+
+  showCancelResult: ({
+    status,
+    message,
+  }: {
+    status: string
+    message: string
+  }) => void
+  showOrderResult: (
+    status: any,
+    cancelOrder: (id: string) => void,
+    marketType: number
+  ) => void
+  handleTabChange: (tab: string) => void
+  enqueueSnackbar: (message: string, variant: { variant: string }) => void
+  getActiveStrategiesQueryRefetch: () => void
+  disableStrategyMutation: (
+    variables: any
+  ) => { data: { disableStrategy: SmartOrder } }
+  subscribeToMore: () => () => void
+  updateEntryPointStrategyMutation: (
+    variables: any
+  ) => { data: { updateEntryPoint: SmartOrder } }
+  updateStopLossStrategyMutation: (
+    variables: any
+  ) => { data: { updateStopLossStrategy: SmartOrder } }
+  updateTakeProfitStrategyMutation: (
+    variables: any
+  ) => { data: { updateTakeProfitStrategy: SmartOrder } }
+}
+
+export interface IState {
+  prices: Price[]
+  editTrade: string | null
+  cachedOrder: any
+  expandedRows: string[]
+  activeStrategiesProcessedData: any[]
+  selectedTrade: SmartOrder
+}
