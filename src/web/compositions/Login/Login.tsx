d4ac0dcb382949493a43a6066ce35c06205f8258
avernikoz <avernikoz@gmail.com>
2020-02-20 22:08:48 +0300

    delete old login

--- src/web/compositions/Login/Login.tsx
+++ src/web/compositions/Login/Login.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 
-import YouNotLoginedCard from '@sb/components/YouNotLoginedCard'
-import LoginOld from '@core/containers/Login'
 import LoginCustom from '@core/containers/LoginCustom'
 
-export default ({ initialStep }: { initialStep?: 'signIn' | 'signUp' }) =>
-  initialStep ? (
-    <Grid
-      style={{ height: 'calc(100% - 5rem)' }}
-      container
-      justify="center"
-      alignItems="center"
-    >
-      <LoginCustom initialStep={initialStep} />
-    </Grid>
-  ) : (
-    <YouNotLoginedCard open={true}>
-      <Grid container justify="center" alignItems="center">
-        <LoginOld />
-      </Grid>
-    </YouNotLoginedCard>
-  )
+export default ({ initialStep }: { initialStep: 'signIn' | 'signUp' }) => (
+  <Grid
+    style={{ height: 'calc(100% - 5rem)' }}
+    container
+    justify="center"
+    alignItems="center"
+  >
+    <LoginCustom initialStep={initialStep} />
+  </Grid>
+)
