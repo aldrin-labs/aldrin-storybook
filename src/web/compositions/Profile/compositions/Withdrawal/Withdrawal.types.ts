98482986f2f56f822a92fc44622a7af068fd6ed8
avernikoz <avernikoz@gmail.com>
2020-02-21 18:01:04 +0300

    update withdrawal types

--- src/web/compositions/Profile/compositions/Withdrawal/Withdrawal.types.ts
+++ src/web/compositions/Profile/compositions/Withdrawal/Withdrawal.types.ts
@@ -1,13 +1,22 @@
 export interface IProps {
-    getProfileSettingsQuery: {
-      getProfileSettings: {
-        depositSettings: {
-          selectedKey: string
-        }
-        withdrawalSettings: {
-          selectedKey: string
-        }
+  getProfileSettingsQuery: {
+    getProfileSettings: {
+      depositSettings: {
+        selectedKey: string
+      }
+      withdrawalSettings: {
+        selectedKey: string
       }
     }
   }
-  
+  withdrawalMutation: (variablesObj: {
+    variables: {
+      input: {
+        keyId: string
+        symbol: string
+        address: string
+        amount: number
+      }
+    }
+  }) => Promise<{ data: string; status: 'ERR' | 'OK'; errorMessage: string }>
+}
