03f7adcc2ba74a0a2ba510eec64f5ac1a0080fbb
avernikoz <avernikoz@gmail.com>
2020-02-24 23:45:58 +0300

    update withdrawal

--- src/web/compositions/Profile/compositions/Withdrawal/Withdrawal.tsx
+++ src/web/compositions/Profile/compositions/Withdrawal/Withdrawal.tsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react'
 import { compose } from 'recompose'
 import { graphql } from 'react-apollo'
+import { withRouter } from 'react-router'
+import { withSnackbar } from 'notistack'
+
 import { Grid } from '@material-ui/core'
 
 import { queryRendererHoc } from '@core/components/QueryRenderer/index'
 import { getProfileSettings } from '@core/graphql/queries/user/getProfileSettings'
 import { withdrawal } from '@core/graphql/mutations/withdrawal/withdrawal'
 import { confirmWithdrawal } from '@core/graphql/mutations/withdrawal/confirmWithdrawal'
-
+import { getAccountSettings } from '@core/graphql/queries/user/getAccountSettings'
+import {
+  validateEnablingMfa,
+  getUserProfileFromAuthResult,
+  getToken,
+  getUserIdFromToken,
+} from '@core/utils/loginUtils'
+import { Loading } from '@sb/components/index'
 
 import PillowButton from '@sb/components/SwitchOnOff/PillowButton'
 import { BtnCustom } from '@sb/components/BtnCustom/BtnCustom.styles'
 import AccountBlock from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/AccountBlock'
 import RecentHistoryTable from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/RecentHistoryTable'
 import InputAmount from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/InputAmount'
+import WithdrawalEnableMfaPopup from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/WithdrawalEnableMfaPopup/WithdrawalEnableMfaPopup'
+import WithdrawalAuthentificatePopup from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/WithdrawalAuthentificatePopup/WithdrawalAuthentificatePopup'
+import WithdrawalRequestPopup from '@sb/compositions/Profile/compositions/DepositWithdrawalComponents/WithdrawalRequestPopup/WithdrawalRequestPopup'
+
 import {
   StyledInput,
   StyledTypography,
@@ -39,15 +53,184 @@ const Withdrawal = ({ ...props }: IProps) => {
   const [coinAmount, setCoinAmount] = useState('')
   const [amountError, setAmountError] = useState(false)
   const [addressError, setAddressError] = useState(false)
+  const [openEnableMfaPopup, toggleEnableMfaPopup] = useState(false)
+  const [
+    withdrawalAuthentificatePopup,
+    toggleWithdrawalAuthentificatePopup,
+  ] = useState(false)
+  const [withdrawalRequestPopup, toggleWithdrawalRequestPopup] = useState(false)
+  const [withdrawalRequestLoading, toggleWithdrawalRequestLoading] = useState(
+    false
+  )
+  const [withdrawalRequestId, setWithdrawalRequestId] = useState('')
 
-  const minimalWithdrawalAmount = 0.000001
-  const transactionFee = 0.000003241
+  const [loading, setLoading] = useState(false)
+
+  const minimalWithdrawalAmount = 0
+  const transactionFee = 0
   const actualAmountGet = +coinAmount - transactionFee
 
   const networkChange = () => {}
 
+  const showWithdrawalStatus = ({
+    status = 'ERR',
+    errorMessage = 'Something went wrong with your withdrawal',
+  }: {
+    status: 'ERR' | 'OK'
+    errorMessage: string
+  }) => {
+    if (status === 'OK') {
+      props.enqueueSnackbar(`Your withdrawal verification is completed `, {
+        variant: 'success',
+      })
+    } else {
+      props.enqueueSnackbar(`Error: ${errorMessage}`, { variant: 'error' })
+    }
+  }
+
+  const goToProfileSettingsHandler = async (): Promise<void> => {
+    const {
+      history: { push },
+    } = props
+    push('/profile/settings')
+  }
+
+  const goToConfirmWithdrawalHandler = async ({
+    withdrawalId,
+  }: {
+    withdrawalId: string
+  }): Promise<void> => {
+    const {
+      history: { push },
+    } = props
+    push(`/profile/confirmWithdrawal/:${withdrawalId}`)
+  }
+
+  const withdrawalMutationHandler = async (): Promise<{
+    status: 'ERR' | 'OK'
+    errorMessage: string
+    withdrawalId?: string
+  }> => {
+    let result: {
+      status: 'ERR' | 'OK'
+      errorMessage: string
+      withdrawalId?: string
+    }
+    try {
+      const res = await props.withdrawalMutation({
+        variables: {
+          input: {
+            address: coinAddress,
+            amount: +coinAmount,
+            symbol: selectedCoin.label,
+            keyId: selectedKey,
+          },
+        },
+      })
+
+      if (res.data.withdrawal.data && res.data.withdrawal.data.id) {
+        result = {
+          status: 'OK',
+          errorMessage: '',
+          withdrawalId: res.data.withdrawal.data.id,
+        }
+      } else {
+        result = {
+          status: 'ERR',
+          errorMessage: res.data.withdrawal.errorMessage,
+        }
+      }
+    } catch (e) {
+      result = {
+        status: 'ERR',
+        errorMessage: e.message,
+      }
+    }
+
+    return result
+  }
+
+  const processWithdrawalRequestHandler = async () => {
+    toggleWithdrawalAuthentificatePopup(false)
+    toggleWithdrawalRequestPopup(true)
+    toggleWithdrawalRequestLoading(true)
+    const resultOfRequest = await withdrawalMutationHandler()
+
+    if (resultOfRequest && resultOfRequest.withdrawalId) {
+      showWithdrawalStatus({
+        status: 'OK',
+        errorMessage: '',
+      })
+
+      setWithdrawalRequestId(resultOfRequest.withdrawalId)
+      toggleWithdrawalRequestLoading(false)
+      toggleWithdrawalRequestPopup(false)
+      goToConfirmWithdrawalHandler({
+        withdrawalId: resultOfRequest.withdrawalId,
+      })
+
+      return
+    }
+
+    showWithdrawalStatus({
+      status: 'ERR',
+      errorMessage: resultOfRequest.errorMessage,
+    })
+
+    toggleWithdrawalRequestPopup(false)
+    toggleWithdrawalRequestLoading(false)
+  }
+
+  const onLoginForWithdrawal = async (
+    rawProfile: any,
+    idToken: any,
+    accessToken = ''
+  ) => {
+    const processedProfile = getUserProfileFromAuthResult(rawProfile)
+    const mfaEnabled = processedProfile.mfaEnabled
+    const userId = processedProfile.sub
+
+    if (!mfaEnabled) {
+      showWithdrawalStatus({
+        status: 'ERR',
+        errorMessage: 'MFA is not enabled for your account',
+      })
+      return
+    }
+
+    const oldToken = await getToken()
+    const oldUserId = getUserIdFromToken(oldToken)
+
+    if (oldUserId !== userId) {
+      showWithdrawalStatus({
+        status: 'ERR',
+        errorMessage: 'Different accounts used for confirming withdrawal',
+      })
+      return
+    }
+
+    // TODO: maybe we should update accessToken & idToken in localstorage also here to prevent hacks
+
+    processWithdrawalRequestHandler()
+  }
+
   return (
     <>
+      <WithdrawalEnableMfaPopup
+        open={openEnableMfaPopup}
+        handleClose={() => toggleEnableMfaPopup(false)}
+        goToProfileSettingsHandler={goToProfileSettingsHandler}
+      />
+      <WithdrawalAuthentificatePopup
+        open={withdrawalAuthentificatePopup}
+        handleClose={() => toggleWithdrawalAuthentificatePopup(false)}
+        onLoginForWithdrawal={onLoginForWithdrawal}
+      />
+      <WithdrawalRequestPopup
+        withdrawalRequestLoading={withdrawalRequestLoading}
+        open={withdrawalRequestPopup}
+        handleClose={() => toggleWithdrawalRequestPopup(false)}
+      />
       <Grid
         container
         justify="center"
@@ -158,6 +341,7 @@ const Withdrawal = ({ ...props }: IProps) => {
               </Grid>
               <Grid style={{ paddingTop: '16px' }}>
                 <BtnCustom
+                  disabled={loading}
                   btnWidth={'80%'}
                   borderRadius={'8px'}
                   btnColor={'#165BE0'}
@@ -166,10 +350,12 @@ const Withdrawal = ({ ...props }: IProps) => {
                   margin={'0 2rem 0 0'}
                   height={'4rem'}
                   fontSize={'1.2rem'}
-                  onClick={() => {
+                  onClick={async () => {
+                    setLoading(true)
                     const isAddressIsNotEmpty = !!coinAddress.length
                     if (!isAddressIsNotEmpty) {
                       setAddressError(true)
+                      setLoading(false)
                       return
                     }
 
@@ -182,33 +368,36 @@ const Withdrawal = ({ ...props }: IProps) => {
                     )
 
                     if (!isCoinAmountIsEnoughToProcessTransaction) {
+                      setLoading(false)
                       setAmountError(true)
                       return
                     }
+
+                    const IsMfaEnabled = await validateEnablingMfa()
+
+                    if (!IsMfaEnabled) {
+                      showWithdrawalStatus({
+                        status: 'ERR',
+                        errorMessage:
+                          'MFA is disabled. Please enable mfa first to process withdrawal',
+                      })
+                      setLoading(false)
+                      toggleEnableMfaPopup(true)
+                      return
+                    }
+
+                    setLoading(false)
+                    toggleWithdrawalAuthentificatePopup(true)
                   }}
                 >
-                  Submit
+                  {loading ? (
+                    <Loading size={16} style={{ height: '16px' }} />
+                  ) : (
+                    `Submit`
+                  )}
                 </BtnCustom>
               </Grid>
             </Grid>
-            {/* <Grid item>
-              <Grid container justify="space-between" style={{ width: '80%' }}>
-                <Grid>
-                  <SvgIcon src={exclamationMark} width="9.5px" height="auto" />
-                </Grid>
-                <Grid style={{ width: '89%' }}>
-                  <Typography
-                    style={{ color: '#16253D', paddingBottom: '1rem' }}
-                  >
-                    Send only BTC to this deposit address.
-                  </Typography>
-                  <Typography>
-                    Sending coin or token other than BTC to this address may
-                    result in the loss of your deposit.
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid> */}
           </Grid>
         </Grid>
       </Grid>
@@ -218,11 +407,18 @@ const Withdrawal = ({ ...props }: IProps) => {
 }
 
 export default compose(
+  withSnackbar,
+  withRouter,
   graphql(withdrawal, { name: 'withdrawalMutation' }),
   graphql(confirmWithdrawal, { name: 'confirmWithdrawalMutation' }),
   queryRendererHoc({
     query: getProfileSettings,
     name: 'getProfileSettingsQuery',
+    fetchPolicy: 'cache-and-network',
+  }),
+  queryRendererHoc({
+    query: getAccountSettings,
+    name: 'getAccountSettingsQuery',
     fetchPolicy: 'cache-and-network',
   })
 )(Withdrawal)
