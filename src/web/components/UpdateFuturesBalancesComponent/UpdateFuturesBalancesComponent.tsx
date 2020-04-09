e1e6db1630022995f4be90c981bc6ccf8b7b922e
avernikoz <avernikoz@gmail.com>
2020-04-09 23:23:12 +0300

    added loader for updatefuturesbalances component

--- src/web/components/UpdateFuturesBalancesComponent/UpdateFuturesBalancesComponent.tsx
+++ src/web/components/UpdateFuturesBalancesComponent/UpdateFuturesBalancesComponent.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid } from '@material-ui/core'
-import SvgIcon from '@sb/components/SvgIcon'
-import { TooltipCustom } from '@sb/components/index'
+
+import { Loading, TooltipCustom, SvgIcon } from '@sb/components'
 
 import { IProps } from './UpdateFuturesBalancesComponent.types'
 
 import Reimport from '@icons/reimport.svg'
 
 export const UpdateFuturesBalancesComponent = ({ ...props }: IProps) => {
+  const [loading, setLoading] = useState(false)
   const { keyId, updateFuturesBalancesHandler } = props
 
   return (
-    <Grid style={{ cursor: 'pointer' }}>
-      <TooltipCustom
-        PopperProps={{ disablePortal: false }}
-        withSpan={true}
-        title="Update your futures balances"
-        component={
-          <SvgIcon
-            alt="Update your futures balances"
-            src={Reimport}
-            width="26px"
-            height="17px"
-            onClick={async () => await updateFuturesBalancesHandler(keyId)}
+    <>
+      {loading ? (
+        <Loading size={16} style={{ height: '16px' }} />
+      ) : (
+        <Grid style={{ cursor: 'pointer' }}>
+          <TooltipCustom
+            PopperProps={{ disablePortal: false }}
+            withSpan={true}
+            title="Update your futures balances"
+            component={
+              <SvgIcon
+                alt="Update your futures balances"
+                src={Reimport}
+                width="26px"
+                height="17px"
+                onClick={async () => {
+                  setLoading(true)
+                  await updateFuturesBalancesHandler(keyId)
+                  setLoading(false)
+                }}
+              />
+            }
           />
-        }
-      />
-    </Grid>
+        </Grid>
+      )}
+    </>
   )
 }
