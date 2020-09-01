14ea4120e6ddef486525e2790a38e90e2cf3e40e
avernikoz <avernikoz@gmail.com>
2020-09-01 15:56:35 +0300

    bump config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.API_ENDPOINT || 'develop.api.cryptocurrencies.ai'
+  process.env.API_ENDPOINT || 'develop.ccai.jp.ngrok.io'
 export const CHARTS_API_URL =
   process.env.CHARTS_API_ENDPOINT || 'chart.cryptocurrencies.ai'
