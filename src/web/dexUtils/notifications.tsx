88c5b9c2615a58d75c0713dc727e89563a57bd48
Likbes <likbes.js@gmail.com>
2020-09-30 19:24:45 +0300

    ts to tsx

---
+++ src/web/dexUtils/notifications.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSnackbar } from 'notistack'
+import { notification } from 'antd';
+import Link from '../components/Link';
+
+export const notify = ({
+  message,
+  description,
+  txid,
+  type = 'info',
+  placement = 'bottomLeft',
+}) => {
+  const { enqueueSnackbar } = useSnackbar()
+  console.log('enqueueSnackbar', enqueueSnackbar)
+  // if (txid) {
+  //   description = (
+  //     <Link
+  //       external
+  //       to={'https://explorer.solana.com/tx/' + txid}
+  //       style={{ color: '#0000ff' }}
+  //     >
+  //       View transaction {txid.slice(0, 8)}...{txid.slice(txid.length - 8)}
+  //     </Link>
+  //   );
+  // }
+
+  enqueueSnackbar(message, {
+    variant: type,
+  })
+
+  return null
+}
