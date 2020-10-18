964dd21ee05b5b9bd9c0d039d1457eaae56511c1
avernikoz <avernikoz@gmail.com>
2020-10-18 15:16:09 +0300

    remove FontStyle component & move it to css

--- src/web/compositions/App/App.styles.tsx
+++ src/web/compositions/App/App.styles.tsx
@@ -31,55 +31,55 @@ export const AppGridLayout = styled.div`
 //   max-height: ${(props) => (props.isPNL ? 'calc(100vh - 48px)' : '')};
 // }
 
-export const FontStyle = React.memo(createGlobalStyle`
-/* latin */
-@font-face {
-  font-family: 'DM Sans';
-  font-style: normal;
-  font-weight: 400;
-  src: local('DM Sans Regular'), local('DMSans-Regular'), url('${DMSans}') format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// export const FontStyle = React.memo(createGlobalStyle`
+// /* latin */
+// @font-face {
+//   font-family: 'DM Sans';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('DM Sans Regular'), local('DMSans-Regular'), url('${DMSans}') format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
-@font-face {
-  font-family: 'Monaco';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Monaco'), local('Monaco'), url('${Monaco}') format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// @font-face {
+//   font-family: 'Monaco';
+//   font-style: normal;
+//   font-weight: 400;
+//   src: local('Monaco'), local('Monaco'), url('${Monaco}') format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
-@font-face {
-  font-family: 'DM Sans Medium';
-  font-style: normal;
-  font-weight: 500;
-  src: local('DM Sans Medium'), local('DMSans-Medium'), url('${DMSansMedium}') format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// @font-face {
+//   font-family: 'DM Sans Medium';
+//   font-style: normal;
+//   font-weight: 500;
+//   src: local('DM Sans Medium'), local('DMSans-Medium'), url('${DMSansMedium}') format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
-@font-face {
-  font-family: 'Avenir Next Demi';
-  font-style: normal;
-  font-weight: 500;
-  src: local('Avenir Next Demi'), local('AvenirNextCyr-Demi'), url('${AvenirNextDemi}') format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// @font-face {
+//   font-family: 'Avenir Next Demi';
+//   font-style: normal;
+//   font-weight: 500;
+//   src: local('Avenir Next Demi'), local('AvenirNextCyr-Demi'), url('${AvenirNextDemi}') format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
-@font-face {
-  font-family: 'IBM Plex Sans Condensed';
-  font-style: normal;
-  font-weight: 400;
-  font-display: swap;
-  src: local('IBM Plex Sans Condensed'), local('IBMPlexSansCond'), url('${IBMPlexSansCondensed}') format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
+// @font-face {
+//   font-family: 'IBM Plex Sans Condensed';
+//   font-style: normal;
+//   font-weight: 400;
+//   font-display: swap;
+//   src: local('IBM Plex Sans Condensed'), local('IBMPlexSansCond'), url('${IBMPlexSansCondensed}') format('woff2');
+//   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+// }
 
-@font-face {
-  font-family: 'FontAwesome';
-  src: url('${FontAwesome}') format('woff2');
-  font-weight: normal;
-  font-style: normal;
-}
+// @font-face {
+//   font-family: 'FontAwesome';
+//   src: url('${FontAwesome}') format('woff2');
+//   font-weight: normal;
+//   font-style: normal;
+// }
 
-.fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.fa-circle:before{content:"\f111"}.fa-usd:before{content:"\f155"}.fa-btc:before{content:"\f15a"}.fa-arrow-left:before{content:"\f060"}.fa-arrow-right:before{content:"\f061"}.fa-ellipsis-h:before{content:"\f141"}
-`)
+// .fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.fa-circle:before{content:"\f111"}.fa-usd:before{content:"\f155"}.fa-btc:before{content:"\f15a"}.fa-arrow-left:before{content:"\f060"}.fa-arrow-right:before{content:"\f061"}.fa-ellipsis-h:before{content:"\f141"}
+// `)
