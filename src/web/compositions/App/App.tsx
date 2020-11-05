a9b0eb5d925e742dd95f6cde3b6cd6ec90290999
avernikoz <avernikoz@gmail.com>
2020-11-05 15:54:13 +0300

    update version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -44,7 +44,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `10.8.7`
+const version = `10.8.8`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const currentVersion = localStorage.getItem('version')
 if (currentVersion !== version) {
