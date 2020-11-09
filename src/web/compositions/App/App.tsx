d98855bac39cf41079eb42edf6bd7b93b309cdd1
avernikoz <avernikoz@gmail.com>
2020-11-09 22:45:38 +0300

    upd version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -35,7 +35,7 @@ import { syncStorage } from '@storage'
 import { getSearchParamsObject } from '@sb/compositions/App/App.utils'
 import { useQuery } from 'react-apollo'
 
-const version = `10.7.2`
+const version = `10.7.3`
 const currentVersion = localStorage.getItem('version')
 if (currentVersion !== version) {
   localStorage.clear()
