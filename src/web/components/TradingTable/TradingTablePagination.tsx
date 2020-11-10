3928228c2ca40e1e956ed4df8191317ae5e4bf3f
avernikoz <avernikoz@gmail.com>
2020-11-10 15:56:42 +0300

    add loader for TradingTablePagination

--- src/web/components/TradingTable/TradingTablePagination.tsx
+++ src/web/components/TradingTable/TradingTablePagination.tsx
@@ -1,58 +1,61 @@
-import React from 'react'
-import styled from 'styled-components'
-import Checkbox from '@material-ui/core/Checkbox'
+import React from 'react';
+import styled from 'styled-components';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import { Loading } from '@sb/components'
 
 const StyledLabel = styled.label`
-  color: ${(props) =>
-    (props.theme &&
-      props.theme.palette &&
-      props.theme.palette.grey &&
-      props.theme.palette.grey.light) ||
-    '#7284A0'};
-  font-size: 0.9rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  letter-spacing: 0.1rem;
-`
+	color: ${(props) =>
+		(props.theme && props.theme.palette && props.theme.palette.grey && props.theme.palette.grey.light) ||
+		'#7284A0'};
+	font-size: 0.9rem;
+	font-weight: bold;
+	text-transform: uppercase;
+	letter-spacing: 0.1rem;
+`;
 
 const StyledCheckox = styled(Checkbox)`
   & svg {
     width: 2rem;
     height: 2rem;
   }
-`
+`;
 
-export const PaginationBlock = ({
-  theme,
-  allKeys,
-  specificPair,
-  handleToggleAllKeys,
-  handleToggleSpecificPair,
-}) => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <div>
-        <StyledCheckox
-          id="specPair"
-          checked={!specificPair}
-          onChange={handleToggleSpecificPair}
-          style={{ padding: '0  .4rem 0 1.2rem' }}
-        />
-        <StyledLabel theme={theme} htmlFor="specPair">
-          all pairs
-        </StyledLabel>
-      </div>
-      <div>
-        <StyledCheckox
-          id="allKeys"
-          checked={allKeys}
-          onChange={handleToggleAllKeys}
-          style={{ padding: '0 1.2rem' }}
-        />
-        <StyledLabel theme={theme} htmlFor="allKeys">
-          all accounts
-        </StyledLabel>
-      </div>
-    </div>
-  )
-}
+export const PaginationBlock = ({ theme, allKeys, specificPair, handleToggleAllKeys, handleToggleSpecificPair, loading }) => {
+	return (
+		<div style={{ display: 'flex' }}>
+			<div>
+				<StyledCheckox
+					id="specPair"
+					checked={!specificPair}
+					onChange={handleToggleSpecificPair}
+					style={{ padding: '0  .4rem 0 1.2rem' }}
+				/>
+				<StyledLabel theme={theme} htmlFor="specPair">
+					all pairs
+				</StyledLabel>
+			</div>
+			<div>
+				<StyledCheckox
+					id="allKeys"
+					checked={allKeys}
+					onChange={handleToggleAllKeys}
+					style={{ padding: '0 1.2rem' }}
+				/>
+				<StyledLabel theme={theme} htmlFor="allKeys">
+					all accounts
+				</StyledLabel>
+			</div>
+			<div
+				style={{
+					padding: '0 2rem',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center'
+				}}
+			>
+				{loading && <Loading size={16} style={{ height: '16px' }} />}
+			</div>
+		</div>
+	);
+};
