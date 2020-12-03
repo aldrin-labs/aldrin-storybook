ef320c0a807a35016d57415754ef38f282fb0ef3
avernikoz <avernikoz@gmail.com>
2020-12-03 14:00:55 +0300

    update OB & Spread table --- move styles & remove some of the methods to class methods

--- src/web/compositions/Chart/Tables/OrderBookTable/Tables/Asks/OrderBookTable.tsx
+++ src/web/compositions/Chart/Tables/OrderBookTable/Tables/Asks/OrderBookTable.tsx
@@ -1,115 +1,130 @@
-import React, { Component, PureComponent } from 'react'
-import { withTheme } from '@material-ui/styles'
+import React, { Component, PureComponent, CSSProperties } from 'react';
+import { withTheme } from '@material-ui/styles';
 
-import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
-import { Column, Table } from 'react-virtualized'
-import 'react-virtualized/styles.css'
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import { Column, Table } from 'react-virtualized';
+import 'react-virtualized/styles.css';
 
-import { withErrorFallback } from '@core/hoc/withErrorFallback'
-import { IProps } from './OrderBookTable.types'
+import { withErrorFallback } from '@core/hoc/withErrorFallback';
+import { IProps } from './OrderBookTable.types';
 
-import { getDataFromTree } from '@core/utils/chartPageUtils'
+import { getDataFromTree } from '@core/utils/chartPageUtils';
 
-import defaultRowRenderer from '../../utils'
-import { AsksWrapper } from '../../OrderBookTableContainer.styles'
+import defaultRowRenderer from '../../utils';
+import { AsksWrapper } from '../../OrderBookTableContainer.styles';
+
+const getHeaderStyle = (theme) => ({
+  color: theme.palette.grey.text,
+  paddingLeft: '.5rem',
+  paddingTop: '.25rem',
+  marginLeft: 0,
+  marginRight: 0,
+  letterSpacing: '.075rem',
+  borderBottom: theme.palette.border.main,
+  fontSize: '1rem'
+})
+
+const gridStyle = {
+	// overflow: mode !== 'asks' ? 'hidden' : 'hidden auto',
+	overflow: 'hidden'
+};
+
+const columnPriceHeaderStyle = { paddingLeft: 'calc(.5rem + 10px)' };
+
+const columnSizeHeaderStyle = { textAlign: 'right', paddingRight: '.9rem' };
+
+const columnTotalHeaderStyle = {
+	paddingRight: 'calc(.5rem + 10px)',
+	textAlign: 'right'
+};
+const getTotalStyles = (theme): CSSProperties => ({ textAlign: 'right', color: theme.palette.dark.main });
+const getSizeStyles = (theme): CSSProperties => ({
+	textAlign: 'right',
+	color: theme.palette.dark.main
+});
+const getPriceStyles = (theme): CSSProperties => ({ color: theme.palette.red.main });
 
 @withTheme()
-class OrderBookTable extends Component<IProps> {
-  render() {
-    const {
-      data,
-      theme,
-      mode,
-      aggregation,
-      openOrderHistory,
-      marketType,
-      arrayOfMarketIds,
-      //amountForBackground,
-      updateTerminalPriceFromOrderbook,
-      currencyPair,
-    } = this.props
-    const tableData = getDataFromTree(data.asks, 'asks').reverse()
-    const amountForBackground =
-      tableData.reduce((acc, curr) => acc + +curr.size, 0) / tableData.length
+class OrderBookTable extends PureComponent<IProps> {
+	render() {
+		const {
+			data,
+			theme,
+			mode,
+			aggregation,
+			openOrderHistory,
+			marketType,
+			arrayOfMarketIds,
+			updateTerminalPriceFromOrderbook,
+			base,
+			quote
+		} = this.props;
+		const tableData = getDataFromTree(data.asks, 'asks').reverse();
+		const amountForBackground = tableData.reduce((acc, curr) => acc + +curr.size, 0) / tableData.length;
 
-    const [base, quote] = currencyPair.split('_')
+		// Add memo
+		const headerStyle = getHeaderStyle(theme);
+		const totalStyles = getTotalStyles(theme);
+		const sizeStyles = getSizeStyles(theme);
+		const priceStyles = getPriceStyles(theme);
 
-    return (
-      <AsksWrapper mode={mode} isFullHeight={mode === 'asks'}>
-        <AutoSizer>
-          {({ width, height }: { width: number; height: number }) => (
-            <Table
-              width={width}
-              height={height}
-              rowCount={tableData.length}
-              onRowClick={({ event, index, rowData }) => {
-                updateTerminalPriceFromOrderbook(+rowData.price)
-              }}
-              headerHeight={window.outerHeight / 50}
-              headerStyle={{
-                color: theme.palette.grey.text,
-                paddingLeft: '.5rem',
-                paddingTop: '.25rem',
-                marginLeft: 0,
-                marginRight: 0,
-                letterSpacing: '.075rem',
-                borderBottom: theme.palette.border.main,
-                fontSize: '1rem',
-              }}
-              gridStyle={{
-                // overflow: mode !== 'asks' ? 'hidden' : 'hidden auto',
-                overflow: 'hidden',
-              }}
-              rowHeight={window.outerHeight / 60}
-              overscanRowCount={0}
-              scrollToIndex={tableData.length - 1}
-              rowGetter={({ index }) => tableData[index]}
-              rowRenderer={(...rest) =>
-                defaultRowRenderer({
-                  theme,
-                  ...rest[0],
-                  side: 'asks',
-                  marketType,
-                  aggregation,
-                  arrayOfMarketIds,
-                  openOrderHistory,
-                  amountForBackground,
-                })
-              }
-            >
-              <Column
-                label="Price"
-                dataKey="price"
-                headerStyle={{ paddingLeft: 'calc(.5rem + 10px)' }}
-                width={width - width / 6}
-                style={{ color: theme.palette.red.main }}
-              />
-              <Column
-                label={`Size (${base})`}
-                dataKey="size"
-                headerStyle={{ textAlign: 'right', paddingRight: '6px' }}
-                width={width + width / 6}
-                style={{
-                  textAlign: 'right',
-                  color: theme.palette.dark.main,
-                }}
-              />
-              <Column
-                label={`Total (${quote})`}
-                dataKey="total"
-                headerStyle={{
-                  paddingRight: 'calc(10px)',
-                  textAlign: 'right',
-                }}
-                width={width}
-                style={{ textAlign: 'right', color: theme.palette.dark.main }}
-              />
-            </Table>
-          )}
-        </AutoSizer>
-      </AsksWrapper>
-    )
-  }
+		return (
+			<AsksWrapper mode={mode} isFullHeight={mode === 'asks'}>
+				<AutoSizer>
+					{({ width, height }: { width: number; height: number }) => (
+						<Table
+							width={width}
+							height={height}
+							rowCount={tableData.length}
+							onRowClick={({ event, index, rowData }) => {
+								updateTerminalPriceFromOrderbook(+rowData.price);
+							}}
+							headerHeight={window.outerHeight / 50}
+							headerStyle={headerStyle}
+							gridStyle={gridStyle}
+							rowHeight={window.outerHeight / 60}
+							overscanRowCount={0}
+							scrollToIndex={tableData.length - 1}
+							rowGetter={({ index }) => tableData[index]}
+							rowRenderer={(...rest) =>
+								defaultRowRenderer({
+									theme,
+									...rest[0],
+									side: 'asks',
+									marketType,
+									aggregation,
+									arrayOfMarketIds,
+									openOrderHistory,
+									amountForBackground
+								})}
+						>
+							<Column
+								label="Price"
+								dataKey="price"
+								headerStyle={columnPriceHeaderStyle}
+								width={width - width / 6}
+								style={priceStyles}
+							/>
+							<Column
+								label={`Size (${base})`}
+								dataKey="size"
+								headerStyle={columnSizeHeaderStyle}
+								width={width + width / 6}
+								style={sizeStyles}
+							/>
+							<Column
+								label={`Total (${quote})`}
+								dataKey="total"
+								headerStyle={columnTotalHeaderStyle}
+								width={width}
+								style={totalStyles}
+							/>
+						</Table>
+					)}
+				</AutoSizer>
+			</AsksWrapper>
+		);
+	}
 }
 
-export default withErrorFallback(OrderBookTable)
+export default withErrorFallback(OrderBookTable);
--- src/web/compositions/Chart/Tables/OrderBookTable/Tables/Bids/SpreadTable.tsx
+++ src/web/compositions/Chart/Tables/OrderBookTable/Tables/Bids/SpreadTable.tsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from 'react'
+import React, { Component, PureComponent, CSSProperties } from 'react'
 import styled from 'styled-components'
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
 import { Column, Table } from 'react-virtualized'
@@ -17,8 +17,60 @@ import {
 import defaultRowRenderer from '../../utils'
 import { BidsWrapper } from '../../OrderBookTableContainer.styles'
 
+const getHeaderStyle = (theme): CSSProperties => ({
+  color: '#7284A0',
+  paddingLeft: '.5rem',
+  marginLeft: 0,
+  marginRight: 0,
+  paddingTop: '.25rem',
+  letterSpacing: '.075rem',
+  borderBottom: theme.palette.border.main,
+  fontSize: '1rem',
+})
+
+const gridStyle = {
+  // overflow: mode !== 'bids' ? 'hidden' : 'hidden auto',
+  overflow: 'hidden',
+}
+
+const columnPriceHeaderStyle = { paddingLeft: 'calc(.5rem + 10px)' }
+
+const columnSizeHeaderStyle = { textAlign: 'right', paddingRight: '.9rem' }
+
+const columnTotalHeaderStyle = {
+  paddingRight: 'calc(.5rem + 10px)',
+  textAlign: 'right',
+}
+const getTotalStyles = (theme): CSSProperties => ({ textAlign: 'right', color: theme.palette.dark.main })
+const getSizeStyles = (theme): CSSProperties => ({
+  textAlign: 'right',
+  color: theme.palette.dark.main,
+})
+const getPriceStyles = (theme): CSSProperties => ({ color: theme.palette.green.main })
 @withTheme()
-class SpreadTable extends Component<IProps> {
+class SpreadTable extends PureComponent<IProps> {
+  onRowClick = ({ event, index, rowData }) => {
+		const { updateTerminalPriceFromOrderbook } = this.props;
+
+    updateTerminalPriceFromOrderbook(+rowData.price);
+	};
+
+	// rowRenderer = (...rest) => {
+	// 	const { amountForBackground, theme } = this.props;
+	// 	return defaultRowRenderer({
+	// 		...rest[0],
+	// 		theme,
+	// 		amountForBackground,
+	// 		openOrderHistory: []
+	// 	});
+	// };
+
+	// rowGetter = ({ index }) => {
+	// 	const { data } = this.props;
+
+	// 	return data[index];
+	// };
+
   render() {
     const {
       theme,
@@ -28,16 +80,20 @@ class SpreadTable extends Component<IProps> {
       mode,
       marketType,
       arrayOfMarketIds,
-      // amountForBackground,
       updateTerminalPriceFromOrderbook,
-      currencyPair,
+      base,
+      quote,
     } = this.props
 
     const tableData = getDataFromTree(data.bids, 'bids').reverse()
     const amountForBackground =
       tableData.reduce((acc, curr) => acc + +curr.size, 0) / tableData.length
 
-    const [base, quote] = currencyPair.split('_')
+    // Add memo  
+    const headerStyle = getHeaderStyle(theme)  
+    const totalStyles = getTotalStyles(theme)
+    const sizeStyles = getSizeStyles(theme)
+    const priceStyles = getPriceStyles(theme)
 
     return (
       <BidsWrapper mode={mode} isFullHeight={mode === 'bids'}>
@@ -48,23 +104,9 @@ class SpreadTable extends Component<IProps> {
               width={width}
               height={height}
               headerHeight={window.outerHeight / 50}
-              onRowClick={({ event, index, rowData }) => {
-                updateTerminalPriceFromOrderbook(+rowData.price)
-              }}
-              headerStyle={{
-                color: '#7284A0',
-                paddingLeft: '.5rem',
-                marginLeft: 0,
-                marginRight: 0,
-                paddingTop: '.25rem',
-                letterSpacing: '.075rem',
-                borderBottom: theme.palette.border.main,
-                fontSize: '1rem',
-              }}
-              gridStyle={{
-                // overflow: mode !== 'bids' ? 'hidden' : 'hidden auto',
-                overflow: 'hidden',
-              }}
+              onRowClick={this.onRowClick}
+              headerStyle={headerStyle}
+              gridStyle={gridStyle}
               rowCount={tableData.length}
               rowHeight={window.outerHeight / 60}
               overscanRowCount={0}
@@ -85,29 +127,23 @@ class SpreadTable extends Component<IProps> {
               <Column
                 label={mode === 'bids' ? `price` : ''}
                 dataKey="price"
-                headerStyle={{ paddingLeft: 'calc(.5rem + 10px)' }}
+                headerStyle={columnPriceHeaderStyle}
                 width={width - width / 6}
-                style={{ color: theme.palette.green.main }}
+                style={priceStyles}
               />
               <Column
                 label={mode === 'bids' ? `size (${base})` : ''}
                 dataKey="size"
                 width={width + width / 6}
-                headerStyle={{ textAlign: 'right', paddingRight: '.9rem' }}
-                style={{
-                  textAlign: 'right',
-                  color: theme.palette.dark.main,
-                }}
+                headerStyle={columnSizeHeaderStyle}
+                style={sizeStyles}
               />
               <Column
                 label={mode === 'bids' ? `total (${quote})` : ''}
                 dataKey="total"
-                headerStyle={{
-                  paddingRight: 'calc(.5rem + 10px)',
-                  textAlign: 'right',
-                }}
+                headerStyle={columnTotalHeaderStyle}
                 width={width}
-                style={{ textAlign: 'right', color: theme.palette.dark.main }}
+                style={totalStyles}
               />
             </Table>
           )}
