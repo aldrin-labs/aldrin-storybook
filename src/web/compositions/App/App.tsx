f7c651c7fd2fe1f83f93dbcf1179842ad1cda512
avernikoz <avernikoz@gmail.com>
2021-01-22 17:06:44 +0300

    update version --- 2 fixes for PositionTable

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -36,7 +36,7 @@ import { GET_VIEW_MODE } from '@core/graphql/queries/chart/getViewMode'
 import { SnackbarUtilsConfigurator } from '@sb/utils/SnackbarUtils'
 import { useQuery } from 'react-apollo'
 
-const version = `11.0.31`
+const version = `11.0.32`
 const currentVersion = localStorage.getItem('version')
 if (currentVersion !== version) {
   localStorage.clear()
