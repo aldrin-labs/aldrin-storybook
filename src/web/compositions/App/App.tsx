7c9cbbef68f3cf7e23285b14a82ebacb9efeaeb5
avernikoz <avernikoz@gmail.com>
2021-02-10 12:42:52 +0300

    update version --- switch-endpoint-to-ob-instance

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -36,7 +36,7 @@ import { GET_VIEW_MODE } from '@core/graphql/queries/chart/getViewMode'
 import { SnackbarUtilsConfigurator } from '@sb/utils/SnackbarUtils'
 import { useQuery } from 'react-apollo'
 
-const version = `11.0.43`
+const version = `11.0.44-switch-endpoint-to-ob-instance`
 const currentVersion = localStorage.getItem('version')
 if (currentVersion !== version) {
   localStorage.clear()
