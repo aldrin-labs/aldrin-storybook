f07b6247e3f35ad52451ce5a97f5298a598150f2
avernikoz <avernikoz@gmail.com>
2021-02-23 00:42:57 +0300

    update version of sb --- fix buttons

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -36,7 +36,7 @@ import { GET_VIEW_MODE } from '@core/graphql/queries/chart/getViewMode'
 import { SnackbarUtilsConfigurator } from '@sb/utils/SnackbarUtils'
 import { useQuery } from 'react-apollo'
 
-const version = `11.0.50`
+const version = `11.0.51`
 const currentVersion = localStorage.getItem('version')
 if (currentVersion !== version) {
   localStorage.clear()
