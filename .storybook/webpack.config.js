fa0f43c207b99866d6f2ba79f7ba791449a3abc4
Vika Stadnichenko <flosssolis.wk@gmail.com>
2021-02-23 09:37:45 +0000

    Update webpack.config.js

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -27,30 +27,6 @@ module.exports = (baseConfig, env, ...rest) => {
         '@nodemodules': path.resolve(__dirname, '..', 'node_modules'),
       },
     },
-    optimization: {
-      splitChunks: {
-        chunks: 'async',
-        minSize: 20000,
-        minRemainingSize: 0,
-        maxSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-        },
-      },
-    },
     module: {
       rules: [
         {
