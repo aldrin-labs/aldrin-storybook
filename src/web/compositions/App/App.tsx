854d89134a60a34a07fc890bb88b615c1c6663ac
avernikoz <avernikoz@gmail.com>
2021-03-09 14:25:52 +0300

    update version of App

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -48,7 +48,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `10.9.44-remove-farming`
+const version = `10.9.45-update-trading-terminal`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const localPassword = localStorage.getItem('localPassword')
 const currentVersion = localStorage.getItem('version')
