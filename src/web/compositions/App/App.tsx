aa26d0e474d8e407a02463f02c0f908c7cfafe0e
avernikoz <avernikoz@gmail.com>
2021-03-17 22:20:31 +0300

    remove homepage --- bump version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -49,7 +49,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `10.9.51-mathwallet-under-maintenance`
+const version = `10.9.52-remove-homepage`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const localPassword = localStorage.getItem('localPassword')
 const currentVersion = localStorage.getItem('version')
