6889bc2a560df060366d662d72b989357a60624f
Likbes <likbes.js@gmail.com>
2021-03-30 23:20:46 +0300

    bump version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -50,7 +50,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `10.9.65-audit`
+const version = `10.9.66-audit`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const isNotificationDone = localStorage.getItem('isNotificationDone')
 const localPassword = localStorage.getItem('localPassword')
