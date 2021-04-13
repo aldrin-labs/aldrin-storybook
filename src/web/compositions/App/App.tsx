4b64b64e9cf638acb6a11f956efe17e7461b87a8
Likbes <likbes.js@gmail.com>
2021-04-13 12:25:03 +0300

    bump version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -48,7 +48,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `1.0.3-fix-reload`
+const version = `1.0.4-fix-reload`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const localPassword = localStorage.getItem('localPassword')
 const currentVersion = localStorage.getItem('version')
