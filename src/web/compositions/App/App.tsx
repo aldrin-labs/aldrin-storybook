06bd8a475443e611f7052cb29656e5e00e0c61a2
avernikoz <avernikoz@gmail.com>
2021-05-03 14:17:37 +0300

    bump version

--- src/web/compositions/App/App.tsx
+++ src/web/compositions/App/App.tsx
@@ -50,7 +50,7 @@ import { WalletProvider } from '@sb/dexUtils/wallet'
 import { MarketProvider } from '@sb/dexUtils/markets'
 import { PreferencesProvider } from '@sb/dexUtils/preferences'
 
-const version = `10.9.92-change-mainnnet-api-url`
+const version = `10.9.93-change-mainnnet-api-url-to-rpc-pool`
 const isOnboardingDone = localStorage.getItem('isOnboardingDone')
 const isNotificationDone = localStorage.getItem('isNotificationDone')
 const localPassword = localStorage.getItem('localPassword')
