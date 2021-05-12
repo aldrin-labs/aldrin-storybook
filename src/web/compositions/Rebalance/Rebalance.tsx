a8df5a8e720f3a4efec917c1be2692d1a978d4eb
avernikoz <avernikoz@gmail.com>
2021-05-12 14:38:59 +0300

    add row container

--- src/web/compositions/Rebalance/Rebalance.tsx
+++ src/web/compositions/Rebalance/Rebalance.tsx
@@ -26,52 +26,50 @@ const RebalanceComposition = ({
   const { wallet } = useWallet()
 
   return (
-    <>
-      <RowContainer
+    <RowContainer
+      theme={theme}
+      style={{ height: '100%', background: theme.palette.grey.additional }}
+    >
+      <Card
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          width: '75%',
+          height: '80%',
+          padding: '0',
+        }}
         theme={theme}
-        style={{ height: '100%', background: theme.palette.grey.additional }}
       >
-        <Card
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            width: '75%',
-            height: '80%',
-            padding: '0',
-          }}
-          theme={theme}
-        >
-          {!publicKey && (
-            <>
-              <Text style={{ color: theme.palette.text.light }}>
-                Connect your wallet to rebalance your coins
-              </Text>
-              {/* connect wallet */}
-              <BtnCustom
-                theme={theme}
-                onClick={wallet.connect}
-                needMinWidth={false}
-                btnWidth="auto"
-                height="auto"
-                fontSize="1.4rem"
-                padding="1rem 2rem"
-                borderRadius=".8rem"
-                borderColor={theme.palette.blue.serum}
-                btnColor={'#fff'}
-                backgroundColor={theme.palette.blue.serum}
-                textTransform={'none'}
-                margin={'4rem 0 0 0'}
-                transition={'all .4s ease-out'}
-                style={{ whiteSpace: 'nowrap' }}
-              >
-                Connect wallet
-              </BtnCustom>
-            </>
-          )}
-        </Card>
-      </RowContainer>
-    </>
+        {!publicKey && (
+          <>
+            <Text style={{ color: theme.palette.text.light }}>
+              Connect your wallet to rebalance your coins
+            </Text>
+            {/* connect wallet */}
+            <BtnCustom
+              theme={theme}
+              onClick={wallet.connect}
+              needMinWidth={false}
+              btnWidth="auto"
+              height="auto"
+              fontSize="1.4rem"
+              padding="1rem 2rem"
+              borderRadius=".8rem"
+              borderColor={theme.palette.blue.serum}
+              btnColor={'#fff'}
+              backgroundColor={theme.palette.blue.serum}
+              textTransform={'none'}
+              margin={'4rem 0 0 0'}
+              transition={'all .4s ease-out'}
+              style={{ whiteSpace: 'nowrap' }}
+            >
+              Connect wallet
+            </BtnCustom>
+          </>
+        )}
+      </Card>
+    </RowContainer>
   )
 }
 
