ff7e8bb5b0e396e693bb72c879f3ed7bd868a91c
avernikoz <avernikoz@gmail.com>
2021-05-17 13:36:08 +0300

    update utils index

--- src/web/compositions/Rebalance/utils/index.ts
+++ src/web/compositions/Rebalance/utils/index.ts
@@ -1,5 +1,7 @@
 import { getPricesForTokens } from './getPricesForTokens'
 import { getTokenValuesForTokens } from './getTokenValuesForTokens'
 import { getSortedTokensByValue } from './getSortedTokensByValue'
+import { getTotalTokenValue } from './getTotalTokenValue'
+import { getPercentageAllocationForTokens } from './getPercentageAllocationForTokens'
 
-export { getTokenValuesForTokens, getPricesForTokens, getSortedTokensByValue }
+export { getTokenValuesForTokens, getPricesForTokens, getSortedTokensByValue, getTotalTokenValue, getPercentageAllocationForTokens }
