3c6d3d0b71a16f4d3c1b308ab884ebdd05cca8a2
avernikoz <avernikoz@gmail.com>
2021-05-28 11:39:57 +0300

    upd cardspanel --- remove rebalance

--- src/web/compositions/Chart/components/CardsPanel.tsx
+++ src/web/compositions/Chart/components/CardsPanel.tsx
@@ -196,15 +196,6 @@ export const CardsPanel = ({ theme }) => {
             </NavLinkButton>
             <NavLinkButton
               theme={theme}
-              pathname={location.pathname}
-              to="/rebalance"
-              page={'rebalance'}
-              component={(props) => <Link to={`/rebalance`} {...props} />}
-            >
-              Rebalance
-            </NavLinkButton>
-            <NavLinkButton
-              theme={theme}
               data-tut="analytics"
               page={'analytics'}
               pathname={location.pathname}
