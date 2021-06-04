ae8604cff0d0c6f208b4e472f95ce256107b2eb6
avernikoz <avernikoz@gmail.com>
2021-06-04 23:25:27 +0300

    update getSliderStepForTokens

--- src/web/compositions/Rebalance/utils/getSliderStepForTokens.ts
+++ src/web/compositions/Rebalance/utils/getSliderStepForTokens.ts
@@ -4,28 +4,16 @@ import { stripDigitPlaces } from '@core/utils/PortfolioTableUtils'
 
 
 
-export const getSliderStepForTokens = (tokens: any[], totalTokenValue) => {
+export const getSliderStepForTokens = (tokens: any[], totalTokenValue: number) => {
     const tokensWithSliderStep = tokens.map(el => {
 
         let decimalCount, stepInAmountToken, stepInValueToken, stepInPercentageToken
-        
-        // with if else we are handling case with zero amount
-        if (el.amount === 0) {
-            decimalCount = 4
-            stepInAmountToken = stripDigitPlaces(1 / Math.pow(10, decimalCount), decimalCount)
 
-            // with if else we are handling case with zero amount
-            stepInValueToken = stepInAmountToken * el.price 
-            stepInPercentageToken = stepInValueToken
-
-        } else {
+            // TODO: Fetch decimal count from blockchain 
             decimalCount = getDecimalCount(el.amount)
-            // const stepInAmountToken = stripDigitPlaces(el.amount * 0.1 / el.tokenValue, decimalCount)
             stepInAmountToken = stripDigitPlaces(1 / Math.pow(10, decimalCount), decimalCount)
-            stepInValueToken = stepInAmountToken * el.tokenValue / el.amount
-            stepInPercentageToken = el.percentage * stepInAmountToken / el.amount
-        }
-
+            stepInValueToken = stepInAmountToken * el.price
+            stepInPercentageToken = stepInAmountToken * 100 / (totalTokenValue / el.price)
 
         return { ...el, stepInAmountToken, stepInValueToken, stepInPercentageToken, decimalCount }
     })
