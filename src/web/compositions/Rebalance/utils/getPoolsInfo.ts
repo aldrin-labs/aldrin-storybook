7782fb49d2025992cb8f5856420ed2d27fbe862b
avernikoz <avernikoz@gmail.com>
2021-06-05 14:41:28 +0300

    update getPoolsInfo --- remove dupes & reverted dupes

--- src/web/compositions/Rebalance/utils/getPoolsInfo.ts
+++ src/web/compositions/Rebalance/utils/getPoolsInfo.ts
@@ -15,5 +15,25 @@ export const getPoolsInfo = async (): Promise<PoolInfo[]> => {
       },
     }
 
-  return getPoolsInfo
+  // TODO: Replace it with handling dupes in:
+  // 1. Pools with the same base_quote and picking the most by tvl (tokenA & tokenB)
+  // 2. Pools with reverted (SRM_SOL & SOL_SRM)
+  const poolsWithoutDuplicatesMap: {[key: string]: PoolInfo} = getPoolsInfo.reduce((acc: {[key: string]: PoolInfo}, el: PoolInfo) => {
+      acc[el.name] = el
+
+      return acc
+    }, {})
+
+  for (const el in poolsWithoutDuplicatesMap) {
+    const revertedPoolName: string = poolsWithoutDuplicatesMap[el].name.split('_').reverse().join('_')
+
+    if (poolsWithoutDuplicatesMap[revertedPoolName]) {
+      delete poolsWithoutDuplicatesMap[revertedPoolName]
+    }
+  }
+
+  const poolsWithoutRevertedDupes = Object.values(poolsWithoutDuplicatesMap)
+  console.log('poolsWithoutRevertedDupes: ', poolsWithoutRevertedDupes)
+
+  return poolsWithoutRevertedDupes
 }
