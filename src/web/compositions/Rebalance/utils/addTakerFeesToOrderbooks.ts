1f0f288f5dd6185dbcd05afac7d62546aa62262d
Likbes <likbes.js@gmail.com>
2021-07-27 17:57:41 +0300

    handle taker fee

---
+++ src/web/compositions/Rebalance/utils/addTakerFeesToOrderbooks.ts
@@ -0,0 +1,29 @@
+import { TAKER_FEE } from '@sb/dexUtils/config'
+import { OrderbooksMap } from './getOrderbookForMarkets'
+
+export const addTakerFeesToPricesInOrderbooks = ({
+  orderbooksMap,
+}: {
+  orderbooksMap: OrderbooksMap
+}) => {
+  const orderbooksWithFees = Object.entries(orderbooksMap).map(
+    ([name, orderbook]) => {
+      const asksWithTakerFees = orderbook.asks.map(([price, amount]) => {
+        const priceWithTakerFee = price * (1 + TAKER_FEE)
+        return [priceWithTakerFee, amount]
+      })
+
+      const bidsWithTakerFees = orderbook.bids.map(([price, amount]) => {
+        const priceWithTakerFee = price * (1 + TAKER_FEE)
+        return [priceWithTakerFee, amount]
+      })
+
+      return [name, { asks: asksWithTakerFees, bids: bidsWithTakerFees }]
+    }
+  )
+
+  const orderbooksMapWithFees = Object.fromEntries(orderbooksWithFees)
+  console.log('orderbooksMapWithFees', orderbooksMapWithFees)
+
+  return orderbooksMapWithFees
+}
