d2a59b1eac6da7e0bddd16cc459bf003bbd944f8
Likbes <likbes.js@gmail.com>
2021-07-31 14:40:27 +0300

    added auto-approve disable warning

--- src/web/compositions/Vesting/index.tsx
+++ src/web/compositions/Vesting/index.tsx
@@ -3,9 +3,10 @@ import { BtnCustom } from '@sb/components/BtnCustom/BtnCustom.styles'
 import { useWallet } from '@sb/dexUtils/wallet'
 import { withTheme } from '@material-ui/core/styles'
 import { compose } from 'recompose'
-import { RowContainer } from '../AnalyticsRoute/index.styles'
+import { Row, RowContainer } from '../AnalyticsRoute/index.styles'
 import { withPublicKey } from '@core/hoc/withPublicKey'
 import { ClaimBlock } from './ClaimBlock'
+import AttentionComponent from '@sb/components/AttentionComponent/AttentionComponent'
 
 const VestingPage = ({
   theme,
@@ -19,25 +20,37 @@ const VestingPage = ({
   return (
     <RowContainer height={'100%'}>
       {!wallet.connected ? (
-        <BtnCustom
-          theme={theme}
-          onClick={wallet.connect}
-          needMinWidth={false}
-          btnWidth="auto"
-          height="auto"
-          fontSize="1.4rem"
-          padding="2rem 8rem"
-          borderRadius="1.1rem"
-          borderColor={theme.palette.blue.serum}
-          btnColor={'#fff'}
-          backgroundColor={theme.palette.blue.serum}
-          textTransform={'none'}
-          margin={'4rem 0 0 0'}
-          transition={'all .4s ease-out'}
-          style={{ whiteSpace: 'nowrap' }}
-        >
-          Connect wallet
-        </BtnCustom>
+        <RowContainer direction="column">
+          <BtnCustom
+            theme={theme}
+            onClick={wallet.connect}
+            needMinWidth={false}
+            btnWidth="auto"
+            height="auto"
+            fontSize="1.4rem"
+            padding="2rem 8rem"
+            borderRadius="1.1rem"
+            borderColor={theme.palette.blue.serum}
+            btnColor={'#fff'}
+            backgroundColor={theme.palette.blue.serum}
+            textTransform={'none'}
+            margin={'4rem 0 0 0'}
+            transition={'all .4s ease-out'}
+            style={{ whiteSpace: 'nowrap' }}
+          >
+            Connect wallet
+          </BtnCustom>
+
+          <Row width={'50rem'} margin={'3rem 0 0 0'}>
+            <AttentionComponent
+              text={
+                'Please make sure that auto-confirmation of transactions is disabled when connecting the wallet. You can find this option as a checkbox on the wallet connection popup.'
+              }
+              textStyle={{ fontSize: '1.5rem' }}
+              iconStyle={{ margin: '0 2rem' }}
+            />
+          </Row>
+        </RowContainer>
       ) : (
         <ClaimBlock theme={theme} />
       )}
