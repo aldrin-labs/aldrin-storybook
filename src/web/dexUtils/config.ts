17079ef874c6a7c6b4330814e6ea2f18e656433a
Likbes <likbes.js@gmail.com>
2021-08-03 17:15:26 +0300

    added config

---
+++ src/web/dexUtils/config.ts
@@ -0,0 +1,17 @@
+import { clusterApiUrl, PublicKey } from '@solana/web3.js'
+import { MAINNET_BETA_ENDPOINT } from './connection';
+
+export const TAKER_FEE = 0.0022
+export const DEX_PID = new PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin");
+export const DEVNET_DEX_PID = new PublicKey("DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY")
+
+const DEX_PIDS = {
+  [MAINNET_BETA_ENDPOINT]: DEX_PID,
+  [clusterApiUrl('testnet')]: DEX_PID,
+  'https://api.devnet.solana.com': DEVNET_DEX_PID,
+  'http://127.0.0.1:8899': DEX_PID,
+}
+
+export const getDexProgramIdByEndpoint = (endpoint: string) => {
+  return DEX_PIDS[endpoint]
+}
