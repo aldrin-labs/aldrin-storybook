ca355868e8788f17501c96cf36cfb1864580c1ff
Likbes <likbes.js@gmail.com>
2021-09-04 18:24:38 +0300

    removed using of getProgramAccounts more than one time and trade history from mark price

--- src/web/dexUtils/connection.tsx
+++ src/web/dexUtils/connection.tsx
@@ -99,56 +99,57 @@ export function useAccountInfo(
   publicKey: PublicKey | undefined | null
 ): [AccountInfo<Buffer> | null | undefined, boolean] {
   const connection = useConnection()
-  const cacheKey = tuple(connection, publicKey?.toBase58())
+  const cacheKey = tuple('useAccountInfo', publicKey?.toBase58())
   const [accountInfo, loaded] = useAsyncData<AccountInfo<Buffer> | null>(
     async () => (publicKey ? connection.getAccountInfo(publicKey) : null),
     cacheKey,
-    { refreshInterval: 60_000 }
+    { refreshInterval: 3_000 }
   )
 
-  useEffect(() => {
-    if (!publicKey) {
-      return
-    }
-    if (accountListenerCount.has(cacheKey)) {
-      let currentItem = accountListenerCount.get(cacheKey)
-      ++currentItem.count
-    } else {
-      let previousInfo: AccountInfo<Buffer> | null = null
-      const subscriptionId = connection.onAccountChange(publicKey, (info) => {
-        if (
-          !previousInfo ||
-          !previousInfo.data.equals(info.data) ||
-          previousInfo.lamports !== info.lamports
-        ) {
-          // probably here is memory leak, sometimes this code executes realy frequently and block whole page
-          // console.log('connection', connection, info)
-          // console.log(
-          //   'setCache useAccountInfo',
-          //   connection,
-          //   info,
-          //   previousInfo,
-          //   previousInfo?.data.equals(info.data),
-          //   previousInfo?.lamports === info.lamports
-          // )
-          previousInfo = info
-          setCache(cacheKey, info)
-        }
-      })
-      accountListenerCount.set(cacheKey, { count: 1, subscriptionId })
-    }
-    return () => {
-      let currentItem = accountListenerCount.get(cacheKey)
-      let nextCount = currentItem.count - 1
-      if (nextCount <= 0) {
-        connection.removeAccountChangeListener(currentItem.subscriptionId)
-        accountListenerCount.delete(cacheKey)
-      } else {
-        --currentItem.count
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cacheKey])
+  // useEffect(() => {
+  //   if (!publicKey) {
+  //     return
+  //   }
+  //   if (accountListenerCount.has(cacheKey)) {
+  //     let currentItem = accountListenerCount.get(cacheKey)
+  //     ++currentItem.count
+  //   } else {
+  //     let previousInfo: AccountInfo<Buffer> | null = null
+  //     const subscriptionId = connection.onAccountChange(publicKey, (info) => {
+  //       if (
+  //         !previousInfo ||
+  //         !previousInfo.data.equals(info.data) ||
+  //         previousInfo.lamports !== info.lamports
+  //       ) {
+  //         // probably here is memory leak, sometimes this code executes realy frequently and block whole page
+  //         // console.log('connection', connection, info)
+  //         // console.log(
+  //         //   'setCache useAccountInfo',
+  //         //   connection,
+  //         //   info,
+  //         //   previousInfo,
+  //         //   previousInfo?.data.equals(info.data),
+  //         //   previousInfo?.lamports === info.lamports
+  //         // )
+  //         previousInfo = info
+  //         setCache(cacheKey, info)
+  //       }
+  //     })
+  //     accountListenerCount.set(cacheKey, { count: 1, subscriptionId })
+  //   }
+  //   return () => {
+  //     let currentItem = accountListenerCount.get(cacheKey)
+  //     let nextCount = currentItem.count - 1
+  //     if (nextCount <= 0) {
+  //       connection.removeAccountChangeListener(currentItem.subscriptionId)
+  //       accountListenerCount.delete(cacheKey)
+  //     } else {
+  //       --currentItem.count
+  //     }
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [cacheKey])
+  
   const previousInfoRef = useRef<AccountInfo<Buffer> | null | undefined>(null)
   if (
     !accountInfo ||
--- src/web/dexUtils/wallet.tsx
+++ src/web/dexUtils/wallet.tsx
@@ -29,12 +29,13 @@ import { TokenListProvider } from '@solana/spl-token-registry'
 import { TokenInstructions } from '@project-serum/serum'
 import { useAsyncData } from './fetch-loop'
 import { getMaxWithdrawAmount } from './pools'
-import { MINT_LAYOUT, parseTokenAccountData } from './tokens'
+import { getTokenAccountInfo, MINT_LAYOUT, parseTokenAccountData } from './tokens'
 import Sollet from '@icons/sollet.svg'
 import Mathwallet from '@icons/mathwallet.svg'
 import Solong from '@icons/solong.svg'
 import WalletAldrin from '@icons/RINLogo.svg'
 import { WalletAdapter } from './adapters'
+import { _VERY_SLOW_REFRESH_INTERVAL } from './markets'
 
 export const WALLET_PROVIDERS = [
   // { name: 'solflare.com', url: 'https://solflare.com/access-wallet' },
@@ -279,7 +280,8 @@ export function useWalletPublicKeys() {
 
   const [tokenAccountInfo, loaded] = useAsyncData(
     () => getTokenAccountInfo(connection, wallet.publicKey),
-    'getTokenAccountInfo'
+    'getTokenAccountInfo',
+    { refreshInterval: _VERY_SLOW_REFRESH_INTERVAL }
   )
 
   let publicKeys = [
