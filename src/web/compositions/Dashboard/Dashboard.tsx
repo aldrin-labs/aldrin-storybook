8e3c73d2c1334399faf9951486ee5890b0e220d8
flosssolis <flosssolis.wk@gmail.com>
2021-09-15 12:32:57 +0300

    logic for getting open orders for all markets

--- src/web/compositions/Dashboard/Dashboard.tsx
+++ src/web/compositions/Dashboard/Dashboard.tsx
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react'
 
 import { Theme, withTheme } from '@material-ui/core'
 import { useWallet } from '@sb/dexUtils/wallet'
-import { useTokenAccounts } from '@sb/dexUtils/markets'
+import {
+  useAllMarketsList,
+  useAllMarketsMapById,
+  useTokenAccounts,
+} from '@sb/dexUtils/markets'
 import { useConnection } from '@sb/dexUtils/connection'
 import { DEX_PID } from '@core/config/dex'
-import { OpenOrders } from '@project-serum/serum'
+import { Market, OpenOrders } from '@project-serum/serum'
 
 import ConnectWallet from './components/ConnectWallet/ConnectWallet'
 import { LoadingWithHint } from '@sb/compositions/Rebalance/components/RebalancePopup/LoadingWithHint'
 import { Row, RowContainer } from '../AnalyticsRoute/index.styles'
 import UnsettledBalancesTable from './components/UnsettledBalancesTable/UnsettledBalancesTable'
+import { onlyUnique } from '@sb/dexUtils/utils'
+import { loadMarketsByNames } from './loadMarketsByNames'
+import { getOrderbookForMarkets } from '../Rebalance/utils/getOrderbookForMarkets'
+import { LoadedMarket } from './loadMarketsByNames'
 
 /* dashboard shows all open orders and all unsettled balances by using open orders accounts
 it gives you ability to settle your funds and cancel orders */
@@ -22,6 +30,9 @@ const Dashboard = ({ theme }: { theme: Theme }) => {
   const connection = useConnection()
   const [userTokenAccounts, userTokenAccountsLoaded] = useTokenAccounts()
 
+  const allMarketsMap = useAllMarketsList()
+  const allMarketsMapById = useAllMarketsMapById()
+
   useEffect(() => {
     const getOpenOrdersAccounts = async () => {
       // 1. load all OOA by using users publicKey
@@ -30,10 +41,54 @@ const Dashboard = ({ theme }: { theme: Theme }) => {
         wallet.publicKey,
         DEX_PID
       )
-      console.log(
-        'openOrdersAccounts',
-        openOrdersAccounts.map((el) => el.market.toString())
+
+      // map of ooa by marketid
+      const openOrdersAccountsMapByMarketId = openOrdersAccounts.reduce(
+        (acc, current) => {
+          const marketId = current.market.toString()
+          if (acc.has(marketId)) {
+            acc.set(marketId, [...acc.get(marketId), current])
+          } else {
+            acc.set(marketId, current)
+          }
+          return acc
+        },
+        new Map()
       )
+
+      const uniqueMarketsIds = openOrdersAccounts
+        .map((el) => el.market.toString())
+        .filter(onlyUnique)
+
+      const uniqueMarketsNames = uniqueMarketsIds.map(
+        (marketId) => allMarketsMapById.get(marketId)?.name || 'Unknown Market'
+      )
+
+      const loadedMarketsMap = await loadMarketsByNames({
+        wallet,
+        connection,
+        marketsNames: uniqueMarketsNames,
+        allMarketsMap,
+      })
+
+      const orderbooks = await getOrderbookForMarkets({
+        connection,
+        loadedMarketsMap,
+        allMarketsMap,
+      })
+
+      // filterForOpenOrders - after load asks + bids
+      const openOrders = loadedMarketsMap
+        .values()
+        .map((marketData: LoadedMarket) => {
+          const { market, marketName } = marketData
+          market.filterForOpenOrders(
+            orderbooks[marketName].bids,
+            orderbooks[marketName].asks,
+            openOrdersAccountsMapByMarketId.get(market.address.toString())
+          )
+        })
+      // go through every loaded market => Market.filterForOpenOrders(ask, bid by name, openOrdersAccounts -> )
     }
     if (connected) getOpenOrdersAccounts()
   }, [connected])
---
+++ src/web/compositions/Dashboard/loadMarketsByNames.ts
@@ -0,0 +1,53 @@
+import { sleep } from '@core/utils/helpers'
+import { Market } from '@project-serum/serum'
+import { WalletAdapter } from '@sb/dexUtils/adapters'
+import { MarketsMap } from '@sb/dexUtils/markets'
+import { Connection } from '@solana/web3.js'
+
+export interface LoadedMarket {
+  market: Market
+  marketName: string
+}
+export interface LoadedMarketsMap {
+  [key: string]: LoadedMarket
+}
+
+export const loadMarketsByNames = async ({
+  wallet,
+  connection,
+  marketsNames,
+  allMarketsMap,
+}: {
+  wallet: WalletAdapter
+  connection: Connection
+  marketsNames: string[]
+  allMarketsMap: MarketsMap
+}): Promise<LoadedMarketsMap> => {
+  const marketsMap: LoadedMarketsMap = {}
+  let i = 0
+
+  const filteredMarketNames = [...new Set(marketsNames)]
+
+  console.time('markets')
+
+  for (let name of filteredMarketNames) {
+    const marketInfo = allMarketsMap.get(name)
+
+    const market = await Market.load(
+      connection,
+      marketInfo.address,
+      {},
+      marketInfo.programId
+    )
+
+    marketsMap[name] = { market, marketName: name }
+
+    if (i % 2 === 0) await sleep(1 * 1000)
+
+    i++
+  }
+
+  console.timeEnd('markets')
+
+  return marketsMap
+}
