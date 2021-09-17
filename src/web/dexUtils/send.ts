4dd66ab78eac6211d2e4448d7b6122de895ee549
avernikoz <avernikoz@gmail.com>
2021-09-17 14:48:44 +0300

    update settleFunds and placeOrder methods --- handle cases with SystemProgram.programId

--- src/web/dexUtils/send.ts
+++ src/web/dexUtils/send.ts
@@ -168,12 +168,39 @@ export async function settleFunds({
     return
   }
 
+  // handling case when user might settle with 11111111111111111111111111111111 instead of the user's pubkey
+  if ((baseTokenAccount && baseTokenAccount.pubkey && quoteTokenAccount && quoteTokenAccount.pubkey) && (SystemProgram.programId.equals(baseTokenAccount.pubkey) || SystemProgram.programId.equals(quoteTokenAccount.pubkey))) {
+    notify({ message: 'Sorry, your base/quote pubKey related to Solana Program Id.' })
+    return 
+  }
+
+  if (SystemProgram.programId.equals(wallet?.publicKey)) {
+    notify({ message: 'Sorry, your wallet pubKey related to Solana Program Id.' })
+    return 
+  }
+
+  if (baseTokenAccount?.pubkey && quoteTokenAccount?.pubkey) {
+    const baseToken = new Token(connection, new PublicKey(market.baseMintAddress), TOKEN_PROGRAM_ID, new Account())
+    const baseTokenInfo = await baseToken.getAccountInfo(new PublicKey(baseTokenAccount.pubkey))
+    const quoteToken = new Token(connection, new PublicKey(market.quoteMintAddress), TOKEN_PROGRAM_ID, new Account())
+    const quoteTokenInfo = await quoteToken.getAccountInfo(new PublicKey(quoteTokenAccount.pubkey))
+
+    if (!baseTokenInfo.owner.equals(wallet.publicKey) || !quoteTokenInfo.owner.equals(wallet.publicKey)) {
+      notify({ message: `Sorry, your wallet pubKey doesn't related to your base/quote tokenAccount owners.` })
+      console.log('baseTokenInfo.owner', baseTokenInfo.owner.toBase58())
+      console.log('quoteTokenInfo.owner', quoteTokenInfo.owner.toBase58())
+
+      return 
+    }
+  }
+
   const usdcRef = process.env.REACT_APP_USDC_REFERRAL_FEES_ADDRESS
   const usdtRef = process.env.REACT_APP_USDT_REFERRAL_FEES_ADDRESS
 
   let createAccountTransaction: Transaction | undefined
   let baseCurrencyAccountPubkey = baseTokenAccount?.pubkey
   let quoteCurrencyAccountPubkey = quoteTokenAccount?.pubkey
+  
 
   if (!baseCurrencyAccountPubkey) {
     const result = await createTokenAccountTransaction({
@@ -384,6 +411,33 @@ export async function placeOrder({
   )
 
   console.log('openOrdersAccount in placeOrder', openOrdersAccount)
+
+  // handling case when user might settle with 11111111111111111111111111111111 instead of the user's pubkey
+  if ((baseCurrencyAccount && baseCurrencyAccount?.pubkey && quoteCurrencyAccount && quoteCurrencyAccount.pubkey) && (SystemProgram.programId.equals(baseCurrencyAccount.pubkey) || SystemProgram.programId.equals(quoteCurrencyAccount?.pubkey)) ) {
+    notify({ message: 'Sorry, your base/quote pubKey related to Solana Program Id.' })
+    return 
+  }
+
+  if (SystemProgram.programId.equals(wallet?.publicKey)) {
+    notify({ message: 'Sorry, your wallet pubKey related to Solana Program Id.' })
+    return 
+  }
+
+  if (baseCurrencyAccount?.pubkey && quoteCurrencyAccount?.pubkey) {
+    const baseToken = new Token(connection, new PublicKey(market.baseMintAddress), TOKEN_PROGRAM_ID, new Account())
+    const baseTokenInfo = await baseToken.getAccountInfo(new PublicKey(baseCurrencyAccount?.pubkey))
+    const quoteToken = new Token(connection, new PublicKey(market.quoteMintAddress), TOKEN_PROGRAM_ID, new Account())
+    const quoteTokenInfo = await quoteToken.getAccountInfo(new PublicKey(quoteCurrencyAccount?.pubkey))
+
+    if (!baseTokenInfo.owner.equals(wallet.publicKey) || !quoteTokenInfo.owner.equals(wallet.publicKey)) {
+      notify({ message: `Sorry, your wallet pubKey doesn't related to your base/quote tokenAccount owners.` })
+      console.log('baseTokenInfo.owner', baseTokenInfo.owner.toBase58())
+      console.log('quoteTokenInfo.owner', quoteTokenInfo.owner.toBase58())
+
+      return 
+    }
+  }
+
 
   const transaction = new Transaction()
 
