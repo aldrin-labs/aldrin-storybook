bbd7dabb0fd16ee679f393aae998808447b56b3b
avernikoz <avernikoz@gmail.com>
2021-09-20 16:38:17 +0300

    remove DEFAULT_PUBLIC_KEY from WalletAdapters

--- src/web/dexUtils/adapters/CommonWallet/index.ts
+++ src/web/dexUtils/adapters/CommonWallet/index.ts
@@ -1,7 +1,7 @@
 import EventEmitter from 'eventemitter3'
 import { PublicKey, Transaction } from '@solana/web3.js'
 import bs58 from 'bs58'
-import { WalletAdapter } from '../types'
+import { WalletAdapter } from '@sb/dexUtils/types'
 
 type InjectedProvider = { postMessage: (params: unknown) => void }
 
