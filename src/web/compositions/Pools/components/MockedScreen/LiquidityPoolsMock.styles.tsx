45ffbea647abddbd363b0834902acddec6362ae6
flosssolis <flosssolis.wk@gmail.com>
2021-10-05 14:37:23 +0300

    liquidity pools mocked page

---
+++ src/web/compositions/Pools/components/MockedScreen/LiquidityPoolsMock.styles.tsx
@@ -0,0 +1,133 @@
+import styled from 'styled-components'
+import Grid from '@icons/grid.svg'
+import { Row } from '@sb/compositions/AnalyticsRoute/index.styles'
+
+export const TransparentText = styled.div`
+  font-family: Avenir Next Bold;
+  font-size: ${(props) => props.fontSize || '10rem'};
+  letter-spacing: -1.37308px;
+  color: transparent;
+  -webkit-text-stroke: 0.1rem #fff;
+  white-space: nowrap;
+`
+export const RotatedContainer = styled.div`
+  left: 0;
+  transform: translate(-40%, 0) rotate(270deg);
+  position: absolute;
+  z-index: 1;
+  overflow: hidden;
+  pointer-events: none;
+
+  @media only screen and (max-width: 700px) {
+    display: none;
+  }
+`
+export const Header = styled.span`
+  font-family: Avenir Next Medium;
+  font-size: 4rem;
+  line-height: 7rem;
+  color: #ffffff;
+  text-align: left;
+`
+
+export const LinkContainer = styled.a`
+  width: 4rem;
+  height: 4rem;
+  cursor: pointer;
+  text-decoration: none;
+  @media (max-width: 600px) {
+    width: 6rem;
+    height: 6rem;
+  }
+`
+export const VioletButton = styled.a`
+  display: flex;
+  align-items: center;
+  border-radius: 1.5rem;
+  white-space: nowrap;
+  font-size: ${(props) => props.fontSize || '2.3rem'};
+  background-color: #621ae1;
+  color: #f8faff;
+  font-family: Avenir Next Medium;
+  letter-spacing: 0.5px;
+  cursor: pointer;
+  padding: 2rem 5rem;
+  justify-content: center;
+  text-transform: uppercase;
+
+  &:hover {
+    color: #f8faff;
+    background-color: #651ce4;
+    transition: 0.3s;
+  }
+
+  @media (max-width: 600px) {
+    padding: 3rem 7rem;
+    font-size: 3rem;
+  }
+`
+export const SpinAnimatedImage = styled.img`
+  width: 35%;
+  height: auto;
+  animation-name: spin;
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @media (max-width: 600px) {
+    width: 90%;
+  }
+`
+export const AnimatedImage = styled.img`
+  width: 70%;
+  height: auto;
+  animation-name: rotate;
+  animation-duration: 5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(60deg);
+    }
+
+    50% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(60deg);
+    }
+  }
+  @media (max-width: 600px) {
+    width: 30%;
+  }
+`
+export const GridContainer = styled(Row)`
+  background-image: url(${Grid});
+  background-repeat: repeat-y;
+  background-position-x: left;
+  background-size: cover;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
+
+export const MobileImageContainer = styled.div`
+  top: ${(props) => props.top || 'auto'};
+  bottom: ${(props) => props.bottom || 'auto'};
+  right: ${(props) => props.right || 'auto'};
+  left: ${(props) => props.left || 'auto'};
+  position: absolute;
+  @media (min-width: 600px) {
+    display: none;
+  }
+`
---
+++ src/web/compositions/Pools/components/MockedScreen/LiquidityPoolsMock.tsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import { SvgIcon } from '@sb/components'
+
+import { RowContainer, Row } from '@sb/compositions/AnalyticsRoute/index.styles'
+import {
+  RotatedContainer,
+  TransparentText,
+  Header,
+  LinkContainer,
+  VioletButton,
+  SpinAnimatedImage,
+  AnimatedImage,
+  GridContainer,
+  MobileImageContainer,
+} from './LiquidityPoolsMock.styles'
+import { Text } from '@sb/compositions/Addressbook/index'
+
+import MediumIcon from '@icons/violetMedium.svg'
+import TelegramIcon from '@icons/violetTelegram.svg'
+import MarketcapIcon from '@icons/violetMarketcap.svg'
+import DiscordIcon from '@icons/violetDiscord.svg'
+import TwitterIcon from '@icons/violetTwitter.svg'
+import KudelskiLogo from '@icons/kudelski.svg'
+import ComingSoonCircle from '@icons/comingSoonCircle.svg'
+import AldrinLogo from '@icons/aldrinLogoWithShadow.svg'
+import useMobileSize from '@webhooks/useMobileSize'
+
+export const LiquidityPoolsMock = ({}) => {
+  const isMobile = useMobileSize()
+  return (
+    <RowContainer
+      style={{
+        background: 'rgb(14, 16, 22)',
+        position: 'relative',
+      }}
+      height={'100%'}
+    >
+      <RotatedContainer>
+        <TransparentText>AMM AMM AMM</TransparentText>
+      </RotatedContainer>
+      <Row
+        width={isMobile ? '90%' : '50%'}
+        align="flex-start"
+        direction="column"
+        style={{
+          padding: isMobile ? '0' : '0 10rem',
+          margin: isMobile ? '0' : '0 0 0 10%',
+        }}
+      >
+        <Row width={isMobile ? '90%' : '60%'} justify="space-between">
+          <LinkContainer>
+            <SvgIcon width="100%" height="100%" src={TwitterIcon} />
+          </LinkContainer>
+          <LinkContainer>
+            <SvgIcon width="100%" height="100%" src={TelegramIcon} />
+          </LinkContainer>
+          <LinkContainer>
+            <SvgIcon width="100%" height="100%" src={DiscordIcon} />
+          </LinkContainer>
+          <LinkContainer>
+            <SvgIcon width="100%" height="100%" src={MediumIcon} />
+          </LinkContainer>
+          <LinkContainer>
+            <SvgIcon width="100%" height="100%" src={MarketcapIcon} />
+          </LinkContainer>
+        </Row>
+        <Header style={{ margin: '4rem 0' }}>
+          Aldrin will be the first full 3rd party audited Automated Market Maker
+          (AMM) on Solana.
+        </Header>
+        <VioletButton>VIEW AUDIT REPORT</VioletButton>
+        <Row
+          width="40%"
+          direction="column"
+          align="flex-start"
+          margin="5rem 0 0 0"
+        >
+          <Text fontSize={isMobile ? '2.5rem' : '1.5rem'}>Audited by</Text>
+          <SvgIcon
+            width={isMobile ? '70%' : '40%'}
+            height="auto"
+            style={{ marginTop: '1rem' }}
+            src={KudelskiLogo}
+          />
+        </Row>
+        <MobileImageContainer top="4rem" left="4rem">
+          <AnimatedImage src={AldrinLogo} />
+        </MobileImageContainer>
+        <MobileImageContainer bottom="5rem" right="5rem">
+          <SpinAnimatedImage src={ComingSoonCircle} />
+        </MobileImageContainer>
+      </Row>
+      <GridContainer height={'100%'} width="40%" direction={'column'}>
+        <Row width="100%" height="65%" justify="flex-start">
+          <AnimatedImage src={AldrinLogo} />
+        </Row>
+        <Row
+          width="100%"
+          height="35%"
+          justify="flex-end"
+          style={{ padding: '0 10rem' }}
+        >
+          <SpinAnimatedImage src={ComingSoonCircle} />
+        </Row>
+      </GridContainer>
+    </RowContainer>
+  )
+}
