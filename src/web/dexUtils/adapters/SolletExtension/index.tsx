ac4f09020478ce01afae1cda59ef3aad978322f2
Dmitry Modin <xzwiex@lembrand.com>
2021-10-12 12:35:55 +0300

    Revert Sollet extension constructor

--- src/web/dexUtils/adapters/SolletExtension/index.tsx
+++ src/web/dexUtils/adapters/SolletExtension/index.tsx
@@ -1,21 +1,23 @@
 import { CommonWalletAdapter } from '../CommonWallet/index'
 import { notify } from '../../notifications'
 
-const { sollet } = window as any
-
-export class SolletExtensionAdapter extends CommonWalletAdapter {
-  constructor(provider: unknown, protected _network: string) {
-    super(sollet, _network)
+export function SolletExtensionAdapter(_: unknown, network: string) {
+  const { sollet } = window as any
+  if (sollet) {
+    return new CommonWalletAdapter(sollet, network)
   }
 
-  connect = async (): Promise<void> => {
-    window.open(
-      'https://chrome.google.com/webstore/detail/sollet/fhmfendgdocmcbmfikdcogofphimnkno',
-      '_blank'
-    )
-    notify({
-      message: 'Sollet Extension Error',
-      description: 'Please install the Sollet Extension for Chrome',
-    })
+  return {
+    on: () => {},
+    connect: () => {
+      window.open(
+        'https://chrome.google.com/webstore/detail/sollet/fhmfendgdocmcbmfikdcogofphimnkno',
+        '_blank'
+      )
+      notify({
+        message: 'Sollet Extension Error',
+        description: 'Please install the Sollet Extension for Chrome',
+      })
+    },
   }
 }
