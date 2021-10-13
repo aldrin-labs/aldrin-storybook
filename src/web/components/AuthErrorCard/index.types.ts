1cfef359ac71760332d57b4086e404ae648c44d3
Dmitry Modin <xzwiex@lembrand.com>
2021-10-13 16:27:34 +0300

    Remove unused code

--- src/web/components/AuthErrorCard/index.types.ts
+++
@@ -1,5 +0,0 @@
-import { WithTheme } from '@material-ui/core/styles'
-
-export interface IProps extends WithTheme {
-  open: boolean
-}
--- src/web/components/GitTransactionCalendar/ChooseYear.types.ts
+++
@@ -1,4 +0,0 @@
-export type IProps = {
-  activeDateButton: any
-  onDateButtonClick: any
-}
