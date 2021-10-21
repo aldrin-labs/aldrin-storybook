081577d39121ccde146f7f3aa6c1dd01e98aff4b
Dmitry Modin <xzwiex@lembrand.com>
2021-10-21 22:22:52 +0300

    UP

--- src/web/compositions/Staking/components/StakingComponent.tsx
+++ src/web/compositions/Staking/components/StakingComponent.tsx
@@ -47,108 +47,14 @@ import {
 import { Row, Cell, StretchedBlock } from '../../../components/Layout'
 import { Button } from '../../../components/Button'
 import { SuccessText } from '../../../components/Typography'
-
-interface UserBalanceProps {
-  value: number
-  visible: boolean
-}
-const UserBalance: React.FC<UserBalanceProps> = (props) => (
-  <BalanceRow>
-    <Digit>{props.visible ? stripByAmount(props.value) : '＊＊＊'}</Digit>&nbsp;RIN
-  </BalanceRow>
-)
+import { UserStakingInfo } from './UserStakingInfo'
 
 export const StakingComponent: React.FC = () => {
-  const [isBalancesShowing, setIsBalancesShowing] = useState(true)
-  const [allTokensData, setAllTokensData] = useState<TokenInfo[]>([])
-
-  const { wallet } = useWallet()
-  const connection = useConnection()
-
-  const walletAddress = wallet?.publicKey?.toString() || ''
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const allTokensData = await getAllTokensData(wallet.publicKey, connection)
-
-      setAllTokensData(allTokensData)
-    }
-    fetchData()
-  }, [])
-
-  const tokenData = allTokensData?.find((token) => token.mint === CCAI_MINT)
-
   return (
     <>
       <RootRow>
         <Cell col={12} colLg={6} >
-          <Block>
-            <BlockContent border>
-              <WalletRow>
-                <div>
-                  <StretchedBlock align="center">
-                    <BlockTitle>
-                      Your RIN Staking
-                    </BlockTitle>
-                    <SvgIcon
-                      src={isBalancesShowing ? ImagesPath.eye : ImagesPath.closedEye}
-                      width={'1.5em'}
-                      height={'auto'}
-                      onClick={() => {
-                        setIsBalancesShowing(!isBalancesShowing)
-                      }}
-                    />
-                  </StretchedBlock>
-                  <StyledTextDiv>
-                    {isBalancesShowing ? walletAddress : '＊＊＊'}
-                  </StyledTextDiv>
-                </div>
-                <WalletBalanceBlock>
-                  <BlockSubtitle>
-                    Available in wallet:
-                  </BlockSubtitle>
-                  <BalanceWrap>
-                    <UserBalance visible={isBalancesShowing} value={tokenData?.amount || 0} />
-                  </BalanceWrap>
-                </WalletBalanceBlock>
-              </WalletRow>
-            </BlockContent>
-            <BlockContent>
-              <Row>
-                <Cell colMd={4} colLg={12} colXl={4}>
-                  <TotalStakedBlock inner>
-                    <BlockContent>
-                      <BlockSubtitle>Total staked:</BlockSubtitle>
-                      <UserBalance visible={isBalancesShowing} value={1} />
-                    </BlockContent>
-                  </TotalStakedBlock>
-                </Cell>
-                <Cell colMd={8} colLg={12} colXl={8}>
-                  <RewardsBlock inner>
-                    <BlockContent>
-                      <StretchedBlock>
-                        <div>
-                          <BlockSubtitle>Rewards:</BlockSubtitle>
-                          <UserBalance visible={isBalancesShowing} value={2} />
-                        </div>
-                        <div>
-                          <BlockSubtitle>Available to claim:</BlockSubtitle>
-                          <UserBalance visible={isBalancesShowing} value={33} />
-                        </div>
-                        <div>
-                          <Button backgroundImage={StakeBtn} fontSize="xs" padding="lg" borderRadis="xxl">Claim</Button>
-                        </div>
-                      </StretchedBlock>
-
-                    </BlockContent>
-                  </RewardsBlock>
-                </Cell>
-              </Row>
-
-              <Button backgroundImage={StakeBtn} fontSize="xs" padding="lg" borderRadis="xxl">Stake</Button>
-              <Button backgroundImage={StakeBtn} disabled fontSize="xs" padding="lg" borderRadis="xxl">Stake</Button>
-            </BlockContent>
-          </Block>
+          <UserStakingInfo />
         </Cell>
         <Cell col={12} colLg={6}>
           <Row>
