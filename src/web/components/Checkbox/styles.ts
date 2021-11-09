775a90c5346bea9922dc5257b5747ca01cf47d44
Dmitry Modin <xzwiex@lembrand.com>
2021-11-09 11:46:13 +0200

    Fix banner

--- src/web/components/Checkbox/styles.ts
+++ src/web/components/Checkbox/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { Text } from '../Typography'
-import { COLORS, BORDER_RADIUS } from '@variables/variables'
+import { COLORS, BORDER_RADIUS, TRANSITION } from '@variables/variables'
 
 export const CheckboxContainer = styled.div`
   display: flex;
@@ -21,17 +21,22 @@ interface CheckMarkProps {
 export const CheckMark = styled.div<CheckMarkProps>`
   width: 16px;
   height: 16px;
-  background: ${COLORS.primary};
+  background: ${COLORS.black};
+  border: 2px solid ${COLORS.primary};
   border-radius: ${BORDER_RADIUS.sm};
   position: relative;
   cursor: pointer;
+  transition: ${TRANSITION};
 
   ${(props: CheckMarkProps) => props.checked ? `
+    & {
+      background: ${COLORS.primary};
+    }
     &:after {
       content: "";
       position: absolute;
-      left: 6px;
-      top: 3px;
+      left: 4px;
+      top: 1px;
       width: 5px;
       height: 8px;
       border-right: 1px solid ${COLORS.black};
