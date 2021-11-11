1982864ade12f4ff11f2320e9df63cf4c22146ac
ccaideveloper <flosssolis.wk@gmail.com>
2021-11-11 09:47:04 +0200

    popups about chart issue

--- src/web/components/WarningPopup/desktopPopup.tsx
+++ src/web/components/WarningPopup/desktopPopup.tsx
@@ -14,10 +14,10 @@ import { Row, RowContainer } from '@sb/compositions/AnalyticsRoute/index.styles'
 import DarkLogo from '@icons/DarkLogo.svg'
 import SvgIcon from '../SvgIcon'
 
-export const RpcCapacityWarningPopup = ({ theme }: { theme: Theme }) => {
+export const ChartsIssuePopup = ({ theme }: { theme: Theme }) => {
   const [isPopupTemporaryHidden, setIsPopupTemporaryHidden] = useState(false)
   const [isPopupOpen, setIsPopupOpen] = useLocalStorageState(
-    'isRpcProblemWarningPopupOpen',
+    'isChartsIssueBannerOpen',
     true
   )
   const isMobile = useMobileSize()
@@ -45,7 +45,7 @@ export const RpcCapacityWarningPopup = ({ theme }: { theme: Theme }) => {
             fontFamily: 'Avenir Next Bold',
           }}
         >
-          Solana Cluster Unstable{' '}
+          Important note!
         </DemiText>
       </RowContainer>
       <RowContainer height="40%" align="flex-start" justify="space-between">
@@ -64,50 +64,9 @@ export const RpcCapacityWarningPopup = ({ theme }: { theme: Theme }) => {
             }}
             theme={theme}
           >
-            The mainnet-beta cluster is experiencing some instability at the
-            moment.
-          </Text>
-          <Text
-            style={{
-              lineHeight: '3rem',
-              marginBottom: '1rem',
-              fontSize: '1.8rem',
-            }}
-            theme={theme}
-          >
-            This can be the reason for the failure of trading, settling and
-            transaction processing.
-          </Text>
-          <Text
-            style={{
-              lineHeight: '3rem',
-              marginBottom: '1rem',
-              fontSize: '1.8rem',
-            }}
-            theme={theme}
-          >
-            Team are aware of the problem and currently looking into it.{' '}
-          </Text>
-          <Text
-            style={{
-              lineHeight: '3rem',
-              fontSize: '1.8rem',
-            }}
-            theme={theme}
-          >
-            Learn More:{' '}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href={'https://status.solana.com/'}
-              style={{
-                fontFamily: 'Avenir Next Bold',
-                textDecoration: 'none',
-                color: '#000',
-              }}
-            >
-              https://status.solana.com/
-            </a>
+            We’re doing an upgrade which will impact our charts temporarily.
+            Trading is not impacted by this but we encourage to trade once
+            charts are fully functional.
           </Text>
         </Row>
         <Row width={'40%'} height={'100%'}>
@@ -115,7 +74,6 @@ export const RpcCapacityWarningPopup = ({ theme }: { theme: Theme }) => {
         </Row>
       </RowContainer>
       <Row
-        // width={'25%'}
         justify="space-between"
         style={{ flexWrap: 'nowrap' }}
       >
