39e4643df16451cc44003f1f3a214ed90a101cb1
Dmitry Modin <xzwiex@lembrand.com>
2021-12-06 12:32:20 +0200

    Fix

--- src/web/dexUtils/pools/getAvailableToClaimFarmingTokens.ts
+++ src/web/dexUtils/pools/getAvailableToClaimFarmingTokens.ts
@@ -1,13 +1,16 @@
 import { getTotalFarmingAmountToClaim } from '../common/getTotalFarmingAmountToClaim'
 import { FarmingTicket } from '../common/types'
+import { MIN_POOL_TOKEN_AMOUNT_TO_STAKE } from '../common/config'
 
 export const getAvailableToClaimFarmingTokens = (
   farmingTickets: FarmingTicket[]
 ) => {
   return (
-    farmingTickets.reduce(
-      (acc, ticket) => acc + getTotalFarmingAmountToClaim(ticket),
-      0
-    ) || 0
+    farmingTickets
+      .filter((ft) => ft.tokensFrozen > MIN_POOL_TOKEN_AMOUNT_TO_STAKE)
+      .reduce(
+        (acc, ticket) => acc + getTotalFarmingAmountToClaim(ticket),
+        0
+      ) || 0
   )
 }
