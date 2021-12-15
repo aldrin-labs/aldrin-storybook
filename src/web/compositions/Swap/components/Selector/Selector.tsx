424b3e0901b13a9bdc17a7f248325ae1b080519c
Likbes <likbes.js@gmail.com>
2021-12-15 17:30:27 +0200

    replaced styles

--- src/web/compositions/Swap/components/Selector/Selector.tsx
+++ src/web/compositions/Swap/components/Selector/Selector.tsx
@@ -5,41 +5,14 @@ import {
     TokenIconsContainer,
 } from '@sb/compositions/Pools/components/Tables/components'
 
-import { SelectorRow, StyledText } from '../SelectCoinPopup'
-import styled from 'styled-components'
+import { StyledText } from '../SelectCoinPopup'
 import { COLORS } from '@variables/variables'
 import { PoolInfo } from '@sb/compositions/Pools/index.types'
 import { filterDataBySymbolForDifferentDeviders } from '@sb/compositions/Chart/Inputs/SelectWrapper/SelectWrapper.utils'
 import { getTokenNameByMintAddress } from '@sb/dexUtils/markets'
+import { SelectorRowsContainer, StyledSelectorRow } from './Selector.styles'
 
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-]
 
-const StyledSelectorRow = styled(SelectorRow)`
-  cursor: pointer;
-  justify-content: space-between;
-  height: 6rem;
-`
-const SelectorRowsContainer = styled(RowContainer)`
-  position: absolute;
-  background: #222429;
-  border: 0.1rem solid #383b45;
-  z-index: 1000;
-  padding: 0 2rem;
-  border-radius: 1.5rem;
-  top: 5rem;
-  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.55);
-  max-height: 65rem;
-  overflow: scroll;
-  div {
-    &:last-child {
-      border: none;
-    }
-  }
-`
 
 const textStyles: React.CSSProperties = {
     color: COLORS.primary,
