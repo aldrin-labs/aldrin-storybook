3ba08132dcbfb7ea3a99d1ad5b833c3733688276
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-01-10 14:54:42 +0200

    added pools icon on mobile

--- src/web/compositions/Chart/components/MobileFooter/MobileFooter.tsx
+++ src/web/compositions/Chart/components/MobileFooter/MobileFooter.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
+
 import { FeedbackPopup } from '../UsersFeedbackPopup'
 import {
-  FeedbackBtn,
   TradeLink,
-  WalletLink,
   DashboardLink,
   StakingLink,
   SwapsLink,
+  PoolsLink,
 } from './NavIconsComponents'
 import { FooterComponent } from './styles'
 
@@ -17,18 +17,18 @@ export const MobileFooter = ({ pathname }) => {
     <FooterComponent height="11em" justify="space-around">
       <TradeLink isActive={pathname.includes('chart')} />
       <DashboardLink isActive={pathname.includes('dashboard')} />
-      {/* <AnalyticsLink isActive={pathname.includes('analytics')} />
-      <PoolsLink isActive={pathname.includes('pools')} /> */}
+      {/* <AnalyticsLink isActive={pathname.includes('analytics')} /> */}
+      <PoolsLink isActive={pathname.includes('pools')} />
       <SwapsLink isActive={pathname.includes('swap')} />
       {/* <RebalanceLink isActive={pathname.includes('rebalance')} /> */}
       <StakingLink isActive={pathname.includes('staking')} />
-      <WalletLink isActive={pathname.includes('wallet')} />
+      {/* <WalletLink isActive={pathname.includes('wallet')} />
       <FeedbackBtn
         onClick={() => {
           setIsFeedBackPopupOpen(true)
         }}
         isActive={isFeedBackPopupOpen}
-      />
+      /> */}
       <FeedbackPopup
         open={isFeedBackPopupOpen}
         onClose={() => {
