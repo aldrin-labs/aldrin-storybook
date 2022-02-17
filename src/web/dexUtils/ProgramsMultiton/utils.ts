50967235f9c3317df316232dd8313112e9d442ec
Dmitry Modin <xzwiex@lembrand.com>
2022-02-17 16:14:12 +0200

    Switch to prod API

--- src/web/dexUtils/ProgramsMultiton/utils.ts
+++ src/web/dexUtils/ProgramsMultiton/utils.ts
@@ -13,9 +13,12 @@ export const POOLS_V2_PROGRAM_ADDRESS = MASTER_BUILD
 export const MARKET_ORDER_PROGRAM_ADDRESS =
   'EVAsnnEkPuDXDnGG2AtHNunXBNqK44Nd3bZauH7zKndP'
 
+// export const STAKING_PROGRAM_ADDRESS = MASTER_BUILD
+//   ? 'rinajRPUgiiW2rG6uieXvcNNQNaWr9ZcMmqo28VvXfa'
+//   : 'FhcygzxWnn782qsSryBeZn2kXZ3sAfCJ8NEoFjBeZsVX'
 export const STAKING_PROGRAM_ADDRESS = MASTER_BUILD
   ? 'rinajRPUgiiW2rG6uieXvcNNQNaWr9ZcMmqo28VvXfa'
-  : 'FhcygzxWnn782qsSryBeZn2kXZ3sAfCJ8NEoFjBeZsVX'
+  : 'rinajRPUgiiW2rG6uieXvcNNQNaWr9ZcMmqo28VvXfa'
 
 export const VESTING_PROGRAM_ADDRESS =
   '77WyS2Q4QHRgnLJZyduowV9dGVjHFqfHc2F3fVvuAdB8'
