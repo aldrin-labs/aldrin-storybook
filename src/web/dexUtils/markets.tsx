14f7301f309b423a5b317a1f3ed7e0358e833df7
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-05-30 17:26:32 +0300

    fixed all_token_mints

--- src/web/dexUtils/markets.tsx
+++ src/web/dexUtils/markets.tsx
@@ -5,7 +5,6 @@ import {
   OpenOrders,
   Orderbook,
   TokenInstructions,
-  TOKEN_MINTS,
 } from '@project-serum/serum'
 import { TokenInfo } from '@solana/spl-token-registry'
 import { Account, AccountInfo, PublicKey, SystemProgram } from '@solana/web3.js'
@@ -16,10 +15,7 @@ import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { OrderWithMarket } from '@sb/dexUtils/send'
 
 import { DEX_PID, getDexProgramIdByEndpoint } from '@core/config/dex'
-import {
-  AWESOME_TOKENS,
-  useAwesomeMarkets,
-} from '@core/utils/awesomeMarkets/serum'
+import { useAwesomeMarkets } from '@core/utils/awesomeMarkets/serum'
 import { Metrics } from '@core/utils/metrics'
 
 import {
@@ -37,17 +33,20 @@ import {
   TOKEN_PROGRAM_ID,
 } from './token/token'
 import { getTokenAccountInfo } from './tokens'
-import { getUniqueListBy, useLocalStorageState } from './utils'
+import { useLocalStorageState } from './utils'
 import { useWallet } from './wallet'
 
-export const ALL_TOKENS_MINTS = getUniqueListBy(
-  [...TOKEN_MINTS, ...AWESOME_TOKENS],
-  'name'
-)
+export const ALL_TOKENS_MINTS = tokensList.map((el) => {
+  return { ...el, address: new PublicKey(el.address) }
+})
 
-export const ALL_TOKENS_MINTS_MAP = ALL_TOKENS_MINTS.reduce((acc, el) => {
-  acc[el.address] = el.name
-  acc[el.name] = el.address
+export const ALL_TOKENS_MINTS_MAP = ALL_TOKENS_MINTS.reduce<{
+  [key: string]: string
+}>((acc, el) => {
+  if (el.symbol) {
+    acc[el.address.toString()] = el.symbol
+    acc[el.symbol] = el.address.toString()
+  }
 
   return acc
 }, {})
