732c4d6d12d46eef088f7cb8b3e3c4176ab13c03
Dmitry Modin <dmitry@aldrin.com>
2022-06-10 10:04:16 +0300

    Add public nodes

--- src/web/dexUtils/connection.tsx
+++ src/web/dexUtils/connection.tsx
@@ -23,7 +23,8 @@ export const ENDPOINTS = [
 const connection = new MultiEndpointsConnection(
   [
     { url: 'https://solana-api.projectserum.com', weight: 2 },
-    // { url: 'https://api-cryptocurrencies-ai.rpcpool.com', weight: 20 },
+    { url: 'https://api.mainnet-beta.solana.com', weight: 2 },
+    { url: 'https://api-cryptocurrencies-ai.rpcpool.com', weight: 1 },
     // { url: 'https://aldrinexchange.genesysgo.net', weight: 3 },
   ],
   'confirmed'
