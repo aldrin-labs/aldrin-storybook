fbb555350b783a9722672ebc8676963c7c5a1f63
Aleksei D <derushev.aleksei@gmail.com>
2022-06-15 00:29:47 +0300

    RIN-15 token icon component

--- src/web/components/TokenIcon/TokenIcon.tsx
+++ src/web/components/TokenIcon/TokenIcon.tsx
@@ -1,27 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { TokenListProvider, TokenInfo } from '@solana/spl-token-registry'
-import CoinPlaceholder from '@icons/coinPlaceholder.svg'
-import { SvgIcon } from '@sb/components'
+import React from 'react'
 
-import GreenCheckmark from '@icons/successIcon.svg'
-import Warning from '@icons/warningPairSel.png'
-import ThinkingFace from '@icons/thinkingFace.png'
-import MangoLogo from '@icons/mango_logo.png'
-import RinLogo from '@icons/rin_logo.png'
-import SunnyLogo from '@icons/sunny_logo.jpg'
+import { SvgIcon } from '@sb/components'
+import { ALL_TOKENS_MINTS_MAP } from '@sb/dexUtils/markets'
+import { useTokenInfos } from '@sb/dexUtils/tokenRegistry'
+
+import AlmLogo from '@icons/alm_logo.png'
+import CobanLogo from '@icons/coban_logo.svg'
+import CoinPlaceholder from '@icons/coinPlaceholder.svg'
+import FriesLogo from '@icons/fries_logo.png'
 import GMCoinLogo from '@icons/gmcoinIcon.jpg'
 import OTRLogo from '@icons/otrIcon.jpg'
 import PTRLogo from '@icons/ptr_logo.png'
-import SYPLogo from '@icons/syp_logo.png'
-import CobanLogo from '@icons/coban_logo.svg'
 import SfcnLogo from '@icons/sfcn_logo.png'
-import FriesLogo from '@icons/fries_logo.png'
-import AlmLogo from '@icons/alm_logo.png'
 import SobLogo from '@icons/sob_logo.png'
+import GreenCheckmark from '@icons/successIcon.svg'
+import SunnyLogo from '@icons/sunny_logo.jpg'
+import SYPLogo from '@icons/syp_logo.png'
+import ThinkingFace from '@icons/thinkingFace.png'
+import Warning from '@icons/warningPairSel.png'
 
-import { useTokenInfos } from '@sb/dexUtils/tokenRegistry'
-import { RIN_MINT } from '@sb/dexUtils/utils'
-import { ALL_TOKENS_MINTS_MAP } from '@sb/dexUtils/markets'
+import spriteJson from './sprite/token-icons.json'
+import spriteImage from './sprite/token-icons.webp'
+
+const CUSTOM_TOKEN_IMAGES = {
+  SUNNY: SunnyLogo,
+  PRT: PTRLogo,
+  SYP: SYPLogo,
+  SFCN: SfcnLogo,
+  ALM: AlmLogo,
+  FRIES: FriesLogo,
+  SOB: SobLogo,
+  GMCOIN: GMCoinLogo,
+  OTR: OTRLogo,
+}
 
 export const TokenIcon = ({
   mint,
@@ -42,10 +53,12 @@ export const TokenIcon = ({
   isAdditionalCustomUserMarket?: boolean
   className?: string
 }) => {
-  const tokenMap = useTokenInfos()
+  // console.log('debug tokenMap', tokenMap)
 
   if (isAdditionalCustomUserMarket) {
-    return <SvgIcon className={className} width={'50%'} height={'auto'} src={Warning} />
+    return (
+      <SvgIcon className={className} width="50%" height="auto" src={Warning} />
+    )
   }
 
   if (!mint) {
@@ -54,96 +67,39 @@ export const TokenIcon = ({
         src={CoinPlaceholder}
         height={height}
         width={width}
-        style={{ margin: margin }}
+        style={{ margin }}
         className={className}
       />
     )
   }
 
-  let token = tokenMap.get(mint)
-
-  // mango has no logo for now
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'MNGO') {
-    token = {
-      logoURI: MangoLogo,
-    }
+  if (Object.keys(CUSTOM_TOKEN_IMAGES).includes(ALL_TOKENS_MINTS_MAP[mint])) {
+    return (
+      <div style={{ width, height, margin }} className={className}>
+        <img src={CUSTOM_TOKEN_IMAGES[mint]} />
+      </div>
+    )
   }
 
-  // rin has no logo for now
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'RIN') {
-    token = {
-      logoURI: RinLogo,
-    }
-  }
+  const spriteToken = spriteJson.icons.find((item) => item.address === mint)
 
-  // sunny has no logo for now
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'SUNNY') {
-    token = {
-      logoURI: SunnyLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'PRT') {
-    token = {
-      logoURI: PTRLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'SYP') {
-    token = {
-      logoURI: SYPLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'COBAN') {
-    token = {
-      logoURI: CobanLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'SFCN') {
-    token = {
-      logoURI: SfcnLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'ALM') {
-    token = {
-      logoURI: AlmLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'FRIES') {
-    token = {
-      logoURI: FriesLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'SOB') {
-    token = {
-      logoURI: SobLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'GMCOIN') {
-    token = {
-      logoURI: GMCoinLogo,
-    }
-  }
-
-  if (ALL_TOKENS_MINTS_MAP[mint] === 'OTR') {
-    token = {
-      logoURI: OTRLogo,
-    }
-  }
-
-  if (!token || !token.logoURI) {
+  if (!spriteToken) {
     if (emojiIfNoLogo) {
       return isAwesomeMarket ? (
-        <SvgIcon className={className} width={'50%'} height={'auto'} src={ThinkingFace} />
+        <SvgIcon
+          className={className}
+          width="50%"
+          height="auto"
+          src={ThinkingFace}
+        />
       ) : (
-          <SvgIcon className={className} width={'50%'} height={'auto'} src={GreenCheckmark} />
-        )
+        <SvgIcon
+          className={className}
+          width="50%"
+          height="auto"
+          src={GreenCheckmark}
+        />
+      )
     }
 
     return (
@@ -151,22 +107,22 @@ export const TokenIcon = ({
         src={CoinPlaceholder}
         height={height}
         width={width}
-        style={{ margin: margin }}
+        style={{ margin }}
         className={className}
       />
     )
   }
 
   return (
-    <img
-      src={token.logoURI}
-      style={{
-        height,
-        width,
-        margin,
-        borderRadius: mint === RIN_MINT ? '0' : '50%',
-      }}
-      className={className}
-    />
+    <div style={{ width, height, margin }} className={className}>
+      <div
+        style={{
+          ...spriteToken.styles,
+          backgroundImage: `url(${spriteImage})`,
+          zoom: `calc(24/64)`,
+          borderRadius: 100,
+        }}
+      />
+    </div>
   )
 }
