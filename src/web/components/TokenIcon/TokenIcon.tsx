2f4cae0c731d5b05023ac2d97807dad3a9982bee
Aleksei D <derushev.aleksei@gmail.com>
2022-06-17 13:01:05 +0300

    RIN-15 custom tokens to sprite

--- src/web/components/TokenIcon/TokenIcon.tsx
+++ src/web/components/TokenIcon/TokenIcon.tsx
@@ -2,35 +2,13 @@ import React from 'react'
 
 import { SvgIcon } from '@sb/components'
 import { TokenIconContainer } from '@sb/components/TokenIcon/TokenIcon.styles'
-import { ALL_TOKENS_MINTS_MAP } from '@sb/dexUtils/markets'
 
-import AlmLogo from '@icons/alm_logo.png'
 import CoinPlaceholder from '@icons/coinPlaceholder.svg'
-import FriesLogo from '@icons/fries_logo.png'
-import GMCoinLogo from '@icons/gmcoinIcon.jpg'
-import OTRLogo from '@icons/otrIcon.jpg'
-import PTRLogo from '@icons/ptr_logo.png'
-import SfcnLogo from '@icons/sfcn_logo.png'
-import SobLogo from '@icons/sob_logo.png'
-import SunnyLogo from '@icons/sunny_logo.jpg'
-import SYPLogo from '@icons/syp_logo.png'
 import ThinkingFace from '@icons/thinkingFace.png'
 import Warning from '@icons/warningPairSel.png'
 
 import spriteJson from './sprite/token-icons.json'
 import spriteImage from './sprite/token-icons.webp'
-
-const CUSTOM_TOKEN_IMAGES = {
-  SUNNY: SunnyLogo,
-  PRT: PTRLogo,
-  SYP: SYPLogo,
-  SFCN: SfcnLogo,
-  ALM: AlmLogo,
-  FRIES: FriesLogo,
-  SOB: SobLogo,
-  GMCOIN: GMCoinLogo,
-  OTR: OTRLogo,
-}
 
 export const TokenIcon = ({
   mint,
@@ -57,18 +35,6 @@ export const TokenIcon = ({
 
   if (!mint) {
     IconComponent = <SvgIcon src={CoinPlaceholder} />
-  } else if (
-    Object.keys(CUSTOM_TOKEN_IMAGES).includes(ALL_TOKENS_MINTS_MAP[mint])
-  ) {
-    IconComponent = (
-      <img
-        src={CUSTOM_TOKEN_IMAGES[mint]}
-        alt={ALL_TOKENS_MINTS_MAP[mint]}
-        width="100%"
-        height="100%"
-        style={{ borderRadius: 100 }}
-      />
-    )
   }
 
   const spriteToken = spriteJson.icons.find((item) => item.address === mint)
