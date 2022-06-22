afb30e921f9385abca5f0b11356fd5f96e81c8d9
Dmitry Modin <dmitry@aldrin.com>
2022-06-22 13:20:05 +0300

    Fix pools loading

--- src/web/dexUtils/staking/hooks/useSrinStakingPools.ts
+++ src/web/dexUtils/staking/hooks/useSrinStakingPools.ts
@@ -3,7 +3,6 @@ import useSWR from 'swr'
 import { fetchSrinStakingPools } from '@core/solana'
 import { COMMON_REFRESH_INTERVAL } from '@core/utils/config'
 
-import { walletAdapterToWallet } from '../../common'
 import { useConnection } from '../../connection'
 import { useWallet } from '../../wallet'
 
@@ -11,11 +10,7 @@ export const useSrinStakingPools = () => {
   const { wallet } = useWallet()
   const connection = useConnection()
   const fetcher = async () => {
-    if (!wallet.publicKey) {
-      return []
-    }
-    const w = walletAdapterToWallet(wallet)
-    return fetchSrinStakingPools(w, connection)
+    return fetchSrinStakingPools(wallet, connection)
   }
   return useSWR(`srin-staking-pools`, fetcher, {
     refreshInterval: COMMON_REFRESH_INTERVAL,
