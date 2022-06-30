b548afe9e55977b5bd9c8154d73c953379946b80
Aleksei D <derushev.aleksei@gmail.com>
2022-06-30 16:06:21 +0300

    RIN-230 cleanup

--- src/web/components/DeleteKeyDialog/DeleteKeyDialog.tsx
+++ src/web/components/DeleteKeyDialog/DeleteKeyDialog.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
 import { Grid, FormControl } from '@material-ui/core'
 import Dialog from '@material-ui/core/Dialog'
-
 import Clear from '@material-ui/icons/Clear'
+import React, { useState } from 'react'
+
+import { OpenDeleteButton } from '@sb/components/DeleteKeyDialog/DeleteKeyDialog.styles'
 import {
   ClearButton,
   StyledPaper,
@@ -14,10 +15,7 @@ import {
   StyledInput,
   StyledButton,
 } from '@sb/components/SharePortfolioDialog/SharePortfolioDialog.styles'
-
 import { ErrorText } from '@sb/components/SignalPreferencesDialog/SignalPreferencesDialog.styles'
-
-import { OpenDeleteButton } from '@sb/components/DeleteKeyDialog/DeleteKeyDialog.styles'
 
 const DeleteAccountDialogComponent = ({
   data,
@@ -43,20 +41,27 @@ const DeleteAccountDialogComponent = ({
     closeMainPopup()
   }
 
-  const showDeleteKeyStatus = ({ status = 'ERR', errorMessage = 'Something went wrong with deleting your key' }: { status: "ERR" | "OK", errorMessage: string }) => {
+  const showDeleteKeyStatus = ({
+    status = 'ERR',
+    errorMessage = 'Something went wrong with deleting your key',
+  }: {
+    status: 'ERR' | 'OK'
+    errorMessage: string
+  }) => {
     if (status === 'OK') {
-      enqueueSnackbar(`Your key was successfully deleted`, { variant: 'success' })
+      enqueueSnackbar(`Your key was successfully deleted`, {
+        variant: 'success',
+      })
     } else {
       enqueueSnackbar(`Error: ${errorMessage}`, { variant: 'error' })
     }
   }
 
-
   const handleSubmit = async () => {
     if (checkName.toLowerCase() === name.toLowerCase()) {
       try {
         const response = await deleteMutation({
-          variables: { name: name, removeTrades: false },
+          variables: { name, removeTrades: false },
         })
         const { status, errorMessage } = response.data.deleteExchangeKey
 
@@ -67,8 +72,7 @@ const DeleteAccountDialogComponent = ({
           return
         }
         closeDialog()
-
-      } catch(e) {
+      } catch (e) {
         setError('Something went wrong')
         showDeleteKeyStatus({ status: 'ERR', errorMessage: e.message })
       }
@@ -84,10 +88,9 @@ const DeleteAccountDialogComponent = ({
       </OpenDeleteButton>
       <Dialog
         PaperComponent={StyledPaper}
-        style={{ width: '75rem', margin: 'auto' }}
         open={isOpen}
         onClose={closeDialog}
-        maxWidth={'md'}
+        maxWidth="md"
         style={{ transition: 'opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms' }}
         aria-labelledby="form-dialog-title"
       >
