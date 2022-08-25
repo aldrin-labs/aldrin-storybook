a8799a78c6dd8b258915bae49c44637c6eb05cf2
Viktoriia Stadnichenko <viktoriiastadnichenko@MacBook-Air-Viktoriia.local>
2022-08-25 16:17:21 +0300

    banner

---
+++ src/web/compositions/StakingV2/components/MaintenancePage/index.styles.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const WideContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span {
+    text-align: center;
+  }
+  .maintenance-header {
+    margin-bottom: 0.5em;
+    margin-top: 1em;
+  }
+`
+export const TextColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span {
+    text-align: center;
+  }
+`
