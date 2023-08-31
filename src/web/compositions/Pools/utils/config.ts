0db5bae5b37f31aa44218c90c3544db94228ba73
avernikoz <avernikoz@gmail.com>
2023-08-31 15:56:53 +0300

    remove idl usage in getPools

--- src/web/compositions/Pools/utils/config.ts
+++ src/web/compositions/Pools/utils/config.ts
@@ -1,36 +1,8 @@
-import { Idl, Program } from '@project-serum/anchor'
-
 import { POOLS_PROGRAM_ADDRESS, POOLS_V2_PROGRAM_ADDRESS } from '@core/solana'
-// eslint-disable-next-line camelcase
-import MM_FARMING_POOL_v1_IDL_JSON from '@core/solana/programsMultiton/idls/pools.json'
-// eslint-disable-next-line camelcase
-import MM_FARMING_POOL_v2_IDL_JSON from '@core/solana/programsMultiton/idls/poolsV2.json'
 
 export const TOKEN_SWAP_PROGRAM_ID = POOLS_PROGRAM_ADDRESS
 export const FARMING_POOLS_V2_PROGRAM_ID = POOLS_V2_PROGRAM_ADDRESS
 export const MAX_TRANSACTION_BATCH_SIZE = 10
-
-// eslint-disable-next-line camelcase
-const mm_farming_pool_idl: Idl = JSON.parse(
-  JSON.stringify(MM_FARMING_POOL_v1_IDL_JSON)
-)
-// eslint-disable-next-line camelcase
-const mm_farming_pool_v2_idl: Idl = JSON.parse(
-  JSON.stringify(MM_FARMING_POOL_v2_IDL_JSON)
-)
-
-export const PROGRAMS_MAP_BY_PROGRAM_ID = {
-  [TOKEN_SWAP_PROGRAM_ID.toString()]: new Program(
-    mm_farming_pool_idl,
-    TOKEN_SWAP_PROGRAM_ID,
-    {} as any
-  ),
-  [FARMING_POOLS_V2_PROGRAM_ID.toString()]: new Program(
-    mm_farming_pool_v2_idl,
-    FARMING_POOLS_V2_PROGRAM_ID,
-    {} as any
-  ),
-}
 
 export const ACCOUNT_DATA_SIZE_MAP_BY_PROGRAM_ID = {
   [TOKEN_SWAP_PROGRAM_ID.toString()]: {
--- src/web/compositions/Pools/utils/getPools.ts
+++ src/web/compositions/Pools/utils/getPools.ts
@@ -1,17 +1,20 @@
-import { Connection, PublicKey } from '@solana/web3.js'
+import { PublicKey } from '@solana/web3.js'
 
-import {
-  ACCOUNT_DATA_SIZE_MAP_BY_PROGRAM_ID,
-  PROGRAMS_MAP_BY_PROGRAM_ID,
-} from './config'
+import { AldrinConnection, ProgramsMultiton } from '@core/solana'
+
+import { ACCOUNT_DATA_SIZE_MAP_BY_PROGRAM_ID } from './config'
 import { filterCorruptedPools } from './filterCorruptedPools'
 import { RawPool } from './types'
 
 export const getPools = async (
   programId: PublicKey,
-  connection: Connection
+  connection: AldrinConnection
 ): Promise<RawPool[]> => {
-  const program = PROGRAMS_MAP_BY_PROGRAM_ID[programId.toString()]
+  const program = ProgramsMultiton.getProgramByAddress({
+    connection,
+    programAddress: programId.toString(),
+  })
+
   const dataSizes = ACCOUNT_DATA_SIZE_MAP_BY_PROGRAM_ID[programId.toString()]
 
   const tokenSwapOwnedAccounts = await connection.getProgramAccounts(
